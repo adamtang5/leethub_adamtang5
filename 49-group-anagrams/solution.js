c2103f062361da59d704fa397d761c2f0895f2ab
Adam Tang <adamtang@gmail.com>
2022-08-25 13:19:54 -0700

    simplified data structure

--- 49-group-anagrams/solution.js
+++ 49-group-anagrams/solution.js
@@ -3,29 +3,22 @@
  * @return {string[][]}
  */
 
-/*
-iterate through each s in strs, and tally counts for each of 26 letters in alphabet
-keep track of all anagram groups in group object, with key being stringified counts,
-and value being list of anagrams in that group
-*/
-
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
     const groups = {};
-    const aCode = 'a'.charCodeAt(0);
-    // console.log(aCode);
     let keyString = "";
-    
+
     strs.forEach(s => {
-        let tally = new Array(26).fill(0);
+        let codes = [];
         for (let i = 0; i < s.length; i++) {
-            tally[s.charCodeAt(i) - aCode]++;
+            codes.push(s.charCodeAt(i));
         }
-        
-        keyString = JSON.stringify(tally);
+        codes.sort((a, b) => a - b);
+
+        keyString = JSON.stringify(codes);
         // console.log(keyString);
         groups[keyString] = groups[keyString] || [];
         groups[keyString].push(s);
     });
-    
+
     return Object.values(groups);
 };
