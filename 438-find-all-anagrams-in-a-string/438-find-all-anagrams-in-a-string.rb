a74a83d59a370f6ea2b106a41a53187f87f9478f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-26 15:54:46 -0700

    Time: 321 ms (60.71%), Space: 212.1 MB (92.86%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rb
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rb
@@ -4,26 +4,24 @@
 
 def find_anagrams(s, p)
     return [] if p.length > s.length
-    ans = []
-    pCount = Array.new(26, 0)
-    p.each_char { |ch| pCount[ch.ord - 'a'.ord] += 1 }
-    # p pCount
-    sCount = Array.new(26, 0)
-    l, r = 0, p.length
-    (l...r).each do |i|
-        sCount[s[i].ord - 'a'.ord] += 1
+    pCount, sCount = Hash.new(0), Hash.new(0)
+    (0...p.length).each do |i|
+        pCount[p[i]] += 1
+        sCount[s[i]] += 1
     end
-    # p sCount
-    while r <= s.length
+    ans = pCount == sCount ? [0] : []
+    l = 0
+    (p.length...s.length).each do |r|
+        sCount[s[r]] += 1
+        sCount[s[l]] -= 1
+        if sCount[s[l]] == 0
+            sCount.delete(s[l])
+        end
+        l += 1
         if pCount == sCount
             ans << l
         end
-        sCount[s[l].ord - 'a'.ord] -= 1
-        if r < s.length
-            sCount[s[r].ord - 'a'.ord] += 1
-        end
-        l += 1
-        r += 1
     end
+    
     return ans
 end
