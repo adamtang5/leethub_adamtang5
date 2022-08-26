6efab2822f8f2bf7a6496558b0c250d68a6ea389
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-26 13:39:35 -0700

    Time: 4035 ms (6.49%), Space: 52.5 MB (5.30%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -10,11 +10,25 @@ data structure: arrays of length 26
 
 var findAnagrams = function(s, p) {
     let pCount = new Array(26).fill(0);
-    let sCount = new Array(26).fill(0);
+    let pSet = new Set(p.split(''));
+    const skipIdx = [];
+    for (let i = 0; i < s.length; i++) {
+        if (!pSet.has(s[i])) skipIdx.push(i);
+    }
+    // console.log(skipIdx);
+    
+    let sCount;
     const aCode = 'a'.charCodeAt(0);
     const pLen = p.length;
     let [start, end] = [0, pLen];
     const ans = [];
+    
+    const _updateSCount = () => {
+        sCount = new Array(26).fill(0);
+        for (let i = start; i < end; i++) {
+            sCount[s.charCodeAt(i) - aCode]++;
+        }
+    };
     
     for (let i = 0; i < pLen; i++) {
         pCount[p.charCodeAt(i) - aCode]++;
@@ -22,21 +36,33 @@ var findAnagrams = function(s, p) {
     const pStr = JSON.stringify(pCount);
     // console.log(pStr);
     
-    for (let i = start; i < end; i++) {
-        sCount[s.charCodeAt(i) - aCode]++;
-    }
-    // console.log(sCount);
-    
     while (end <= s.length) {
-        if (JSON.stringify(sCount) === pStr) ans.push(start);
-        sCount[s.charCodeAt(start) - aCode]--;
-        if (end === s.length) {
-            break;
+        let i2Skip;
+        if (skipIdx.some(i => {
+            if (i >= start && i < end) {
+                i2Skip = i;
+                return true;
+            } else {
+                return false;
+            }            
+        })) {
+            start = i2Skip + 1;
+            end = start + pLen;
+            // console.log(start, end, ans);
+            _updateSCount();
         } else {
-            sCount[s.charCodeAt(end) - aCode]++;
+            _updateSCount();
+            if (JSON.stringify(sCount) === pStr) ans.push(start);
+            sCount[s.charCodeAt(start) - aCode]--;
+            if (end === s.length) {
+                break;
+            } else {
+                sCount[s.charCodeAt(end) - aCode]++;
+            }
+            start++;
+            end++;
+            // console.log(start, end, ans);
         }
-        start++;
-        end++;
     }
     
     return ans;
