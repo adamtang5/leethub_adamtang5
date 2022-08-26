95fe4c2bccdd2a6f33c0dcc8a9e4274671312aec
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-26 15:33:52 -0700

    Time: 598 ms (16.31%), Space: 48.3 MB (55.84%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -9,32 +9,21 @@ var findAnagrams = function(s, p) {
     let pCount = new Array(26).fill(0);
     let sCount = new Array(26).fill(0);
     const aCode = 'a'.charCodeAt(0);
-    const pLen = p.length;
-    let [start, end] = [0, pLen];
-    const ans = [];
+    let start = 0;
     
-    for (let i = 0; i < pLen; i++) {
+    for (let i = 0; i < p.length; i++) {
         pCount[p.charCodeAt(i) - aCode]++;
-    }
-    const pStr = JSON.stringify(pCount);
-    // console.log(pStr);
-    
-    for (let i = start; i < end; i++) {
         sCount[s.charCodeAt(i) - aCode]++;
     }
-    // console.log(sCount);
+    const pStr = JSON.stringify(pCount);
+    let ans = JSON.stringify(sCount) === pStr ? [0] : []
     
-    while (end <= s.length) {
-        if (JSON.stringify(sCount) === pStr) ans.push(start);
+    for (let end = p.length; end < s.length; end++) {
+        sCount[s.charCodeAt(end) - aCode]++;
         sCount[s.charCodeAt(start) - aCode]--;
-        if (end === s.length) {
-            break;
-        } else {
-            sCount[s.charCodeAt(end) - aCode]++;
-        }
         start++;
-        end++;
+        // console.log(sCount, start);
+        if (JSON.stringify(sCount) === pStr) ans.push(start);
     }
-    
     return ans;
 };
