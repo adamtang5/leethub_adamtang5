215aba26f04204f4b5ac655285797c891e319e1e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-27 21:47:51 -0700

    Time: 161 ms (77.28%), Space: 211.7 MB (71.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.rb
+++ 242-valid-anagram/242-valid-anagram.rb
@@ -3,10 +3,10 @@
 # @return {Boolean}
 def is_anagram(s, t)
     return false if s.length != t.length
-    counts = Array.new(26, 0)
+    counts = Hash.new(0)
     (0...s.length).each do |i|
-        counts[s[i].ord - 'a'.ord] += 1
-        counts[t[i].ord - 'a'.ord] -= 1
+        counts[s[i]] += 1
+        counts[t[i]] -= 1
     end
-    return counts.all? { |n| n == 0 }
+    return counts.values.all? { |n| n == 0 }
 end
