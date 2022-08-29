ca7db71ee482aba5b532512b979b00d091ad1216
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-29 11:29:13 -0700

    Time: 271 ms (13.89%), Space: 219 MB (80.56%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def subarray_sum(nums, k)
+    ans, currSum = 0, 0
+    prefixSums = Hash.new(0)
+    prefixSums[0] = 1
+    nums.each do |n|
+        currSum += n
+        diff = currSum - k
+        ans += prefixSums[diff]
+        prefixSums[currSum] += 1
+    end
+    return ans
+end
