1512260c6f5c5681e94606393480308d204cf7ef
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-28 22:49:18 -0700

    Time: 1144 ms (100.00%), Space: 223.8 MB (100.00%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.rb
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.rb
@@ -2,8 +2,8 @@
 # @param {String} t
 # @return {Integer}
 def min_steps(s, t)
-    counts = Hash.new(0)
-    s.each_char { |ch| counts[ch] += 1}
-    t.each_char { |ch| counts[ch] -= 1}
-    return counts.values.sum { |n| n.abs }
+    counts = Array.new(26, 0)
+    s.each_char { |ch| counts[ch.ord - 'a'.ord] += 1}
+    t.each_char { |ch| counts[ch.ord - 'a'.ord] -= 1}
+    return counts.sum { |n| n.abs }
 end
