0ed43181def20fae444eaeaddf213904ce2a436d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-08-29 00:14:41 -0700

    Time: 1282 ms (54.26%), Space: 18.1 MB (71.65%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,23 @@
+import json
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        i = 0
+        while i < len(nums) - 2:
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                if nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    while l+1 < len(nums) and nums[l] == nums[l+1]:
+                        l += 1
+                    l += 1
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
+            i += 1
+        return ans
