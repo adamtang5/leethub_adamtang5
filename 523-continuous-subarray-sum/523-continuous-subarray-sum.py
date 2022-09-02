6dfa0aa149f49827736275dd0e1627dff7e6810f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-01 23:59:46 -0700

    Time: 1470 ms (51.38%), Space: 33.1 MB (70.11%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -3,14 +3,13 @@ class Solution:
         if len(nums) < 2:
             return False
         prefixMods = {}
-        prefixMods[0] = prefixMods.get(0, [])
-        prefixMods[0].append(-1)
+        prefixMods[0] = -1
         currMod = 0
         for i in range(len(nums)):
             currMod += nums[i]
             currMod %= k
-            if currMod in prefixMods and any([idx < (i-1) for idx in prefixMods[currMod]]):
+            if currMod in prefixMods and prefixMods[currMod] < i-1:
                 return True
-            prefixMods[currMod] = prefixMods.get(currMod, [])
-            prefixMods[currMod].append(i)
+            if currMod not in prefixMods:
+                prefixMods[currMod] = i
         return False
