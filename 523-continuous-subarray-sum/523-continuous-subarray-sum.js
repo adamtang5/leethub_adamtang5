954edb3d231d518a271a2a1d168b02dfcb550d96
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-01 23:57:35 -0700

    Time: 153 ms (63.32%), Space: 66.7 MB (25.33%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.js
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.js
@@ -3,19 +3,17 @@
  * @param {number} k
  * @return {boolean}
  */
+
 var checkSubarraySum = function(nums, k) {
     if (nums.length < 2) return false;
     const prefixMods = {};
-    prefixMods[0] = prefixMods[0] || [];
-    prefixMods[0].push(-1);
+    prefixMods[0] = -1;
     let currMod = 0;
     for (let i = 0; i < nums.length; i++) {
         currMod += nums[i];
         currMod %= k;
-        if (prefixMods[currMod] !== undefined && prefixMods[currMod].some(idx => idx < i - 1)) return true;
-        prefixMods[currMod] = prefixMods[currMod] || [];
-        prefixMods[currMod].push(i);
-        // console.log(prefixMods);
+        if (prefixMods[currMod] !== undefined && prefixMods[currMod] < i - 1) return true;
+        if (prefixMods[currMod] === undefined) prefixMods[currMod] = i;
     }
     return false;
 };
