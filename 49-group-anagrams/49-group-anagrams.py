978e75881e82bded745ea023f11415dcd12c2892
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-04 21:59:47 -0700

    Time: 268 ms (6.85%), Space: 18.6 MB (36.22%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = {}
+        groups = collections.defaultdict(list)
         for s in strs:
             key = [0] * 26
             for ch in s:
                 key[ord(ch) - ord('a')] += 1
             key = str(key)
 
-            groups[key] = groups.get(key, [])
             groups[key].append(s)
         return groups.values()
