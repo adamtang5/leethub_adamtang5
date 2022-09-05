fee38033196ff10f28a4d4c666ded585b9541214
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-04 21:52:32 -0700

    Time: 234 ms (30.50%), Space: 54.9 MB (21.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,15 +5,12 @@
 
 var groupAnagrams = function(strs) {
     const groups = {};
-    const aCode = 'a'.charCodeAt(0);
     let keyString = "";
 
     strs.forEach(s => {
-        const counts = new Array(26).fill(0);
-        for (let i = 0; i < s.length; i++) {
-            counts[s.charCodeAt(i) - aCode]++;
-        }
-        keyString = JSON.stringify(counts);
+        let chs = s.split('');
+        chs.sort();
+        keyString = JSON.stringify(chs);
         groups[keyString] = groups[keyString] || [];
         groups[keyString].push(s);
     });
