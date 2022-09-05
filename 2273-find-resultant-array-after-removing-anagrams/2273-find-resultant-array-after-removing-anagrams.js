8020d2fc633daebbb4a9dd79f15b7a6794608f02
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-04 21:39:17 -0700

    Time: 128 ms (55.97%), Space: 47.8 MB (85.45%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
@@ -4,14 +4,11 @@
  */
 var removeAnagrams = function(words) {
     const ans = [];
-    const aCode = 'a'.charCodeAt(0);
     let currKeyStr = '';
     words.forEach((word) => {
-        const counts = new Array(26).fill(0);
-        for (let i = 0; i < word.length; i++) {
-            counts[word.charCodeAt(i) - aCode]++;
-        }
-        const keyStr = JSON.stringify(counts);
+        let chs = word.split('');
+        chs.sort();
+        const keyStr = JSON.stringify(chs);
         if (keyStr !== currKeyStr) {
             ans.push(word);
             currKeyStr = keyStr;
