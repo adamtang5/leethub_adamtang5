102fdd4142882d84fa1404ad72056a2d4cc96bb0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 00:20:59 -0700

    Time: 65 ms (91.55%), Space: 13.9 MB (59.66%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,17 +1,3 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        if x < 10:
-            return True
-        digits = int(math.log10(x))
-        while digits > 0:
-            firstDigit = int(x / pow(10, digits))
-            lastDigit = x % 10
-            if firstDigit != lastDigit:
-                return False
-            else:
-                x %= pow(10, digits)
-                x //= 10
-                digits -= 2
-        return True
+        return str(x) == str(x)[::-1]
