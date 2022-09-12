3db79831ca1d145e03c79045c9103af35de0101c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-11 21:56:57 -0700

    Time: 189 ms (5.28%), Space: 211.1 MB (46.04%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.rb
+++ 7-reverse-integer/7-reverse-integer.rb
@@ -1,25 +1,13 @@
 # @param {Integer} x
 # @return {Integer}
 def reverse(x)
-    min_value = -2147483648     # -2^31
-    max_value = 2147483647      #  2^31 - 1
-    max_sans_ones = (max_value / 10.0).truncate(0)
-    min_sans_ones = (min_value / 10.0).truncate(0)
-    max_ones = max_value.remainder(10)
-    min_ones = min_value.remainder(10)
-    
-    ans = 0
-    while x != 0
-        digit = x.remainder(10)
-        x = (x / 10.0).truncate(0)
-        
-        if ans > max_sans_ones || (ans == max_sans_ones && digit > max_ones)
-            return 0
-        elsif ans < min_sans_ones || (ans == min_sans_ones && digit < min_ones)
-            return 0
-        end
-        ans = (ans * 10) + digit
+    sign = x >= 0 ? 1 : -1
+    x = x.abs
+    s = ''
+    while x > 0
+        s += (x % 10).to_s
+        x /= 10
     end
-    
-    return ans
+    ans = sign * s.to_i
+    return ans < -1 * (2 ** 31) || ans >= 2 ** 31 ? 0 : ans
 end
