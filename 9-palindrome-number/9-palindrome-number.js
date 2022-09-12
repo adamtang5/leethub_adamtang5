ed4c876a98f410c2575ad39e86bed93a86300209
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 00:24:00 -0700

    Time: 201 ms (85.99%), Space: 51.1 MB (51.57%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,19 +4,7 @@
  */
 
 var isPalindrome = function(x) {
-    if (x < 0) return false;
-    if (x < 10) return true;
-    
-    x = x.toString(10);
-    let [l, r] = [0, x.length - 1];
-    
-    while (l < r) {
-        if (x[l] !== x[r]) {
-            return false;
-        } else {
-            l++;
-            r--;
-        }
-    }
-    return true;
+    s = x.toString(10);
+    rev = s.split('').reverse().join('');
+    return s === rev;
 };
