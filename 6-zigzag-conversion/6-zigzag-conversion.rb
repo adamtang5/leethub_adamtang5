89535ef604d9325e0c34f48cd017051d2cc51b89
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-11 20:03:23 -0700

    Time: 172 ms (72.79%), Space: 211 MB (69.39%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.rb
+++ 6-zigzag-conversion/6-zigzag-conversion.rb
@@ -7,14 +7,10 @@ def convert(s, num_rows)
     rows = Array.new(num_rows, '')
     new_base = (num_rows - 1) * 2
     
-    new_idx_mods = Array.new(new_base, 0)
-    (0...new_base).each do |i|
-        new_idx_mods[i] = i <= new_base / 2 ? i : new_base - i
-    end
-    
     s.each_char.with_index do |ch, i|
         i_mod = i % new_base
-        rows[new_idx_mods[i_mod]] += ch
+        new_i_mod = i_mod <= new_base / 2 ? i_mod : new_base - i_mod
+        rows[new_i_mod] += ch
     end
     return rows.join('')
 end
