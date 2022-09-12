2644f97dd56b159ac7f4860c246b0c4916d52a27
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-11 22:38:48 -0700

    Time: 69 ms (21.84%), Space: 14 MB (28.77%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -3,11 +3,11 @@ import re
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.strip()
-        sign = 1
-        
-        digitsRe = "^[-+]?\d+"
-        if not re.match(digitsRe, s):
+        if s == '':
             return 0
+
+        sign = 1
+        ans = ''
         
         if s[0] == '-':
             sign = -1
@@ -15,7 +15,17 @@ class Solution:
         elif s[0] == '+':
             s = s[1:]
             
-        ans = sign * int(re.findall(digitsRe, s)[0])
+        digitRe = "\d"
+        for i in range(len(s)):
+            if re.search(digitRe, s[i]):
+                ans += s[i]
+                i += 1
+            else:
+                break
+        
+        if len(ans) == 0:
+            return len(ans)
+        ans = sign * int(ans)
         
         if ans < -1 * pow(2, 31):
             return -1 * pow(2, 31)
