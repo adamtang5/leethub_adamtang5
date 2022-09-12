b2f39c94edc4b0b6397eae349981c6bba16634cc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 00:19:02 -0700

    Time: 161 ms (63.77%), Space: 211.2 MB (16.49%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.rb
+++ 9-palindrome-number/9-palindrome-number.rb
@@ -1,9 +1,5 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-    return false if x < 0
-    return true if x < 10
-    s = x.to_s
-    rev_s = s.reverse
-    return s == rev_s
+    return x.to_s == x.to_s.reverse
 end
