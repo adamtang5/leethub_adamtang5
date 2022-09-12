d3ae6c4a54faa4d72142d84e75093033c6e71879
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-11 23:42:20 -0700

    Time: 163 ms (98.06%), Space: 50.6 MB (82.86%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 
-/*
-data structure: POJO with Math.floor(log x / log 10) as key, x % 10 ** n as value
-*/
-
 var isPalindrome = function(x) {
     if (x < 0) return false;
     if (x < 10) return true;
