517c32f52fafd2bb2dbdcf4beacbe88cdcc8e9ad
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 22:30:19 -0700

    Time: 307 ms (6.80%), Space: 63.9 MB (8.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -22,7 +22,8 @@ var numIslands = function(grid) {
         visited.add(`${row}-${col}`);
         const fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
         for (let [rowDiff, colDiff] of fundDirs) {
-            dfs(row + rowDiff, col + colDiff);
+            const [newRow, newCol] = [row + rowDiff, col + colDiff];
+            dfs(newRow, newCol);
         }
         return 1;
     };
