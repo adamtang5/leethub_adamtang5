72c0e0254020e3cea47b5ad7df0785331eb8410a
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 22:57:12 -0700

    Time: 258 ms (9.96%), Space: 64.5 MB (6.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    if (!grid.length) return 0;
     let ans = 0;
     let visited = new Set();
     
@@ -22,28 +21,12 @@ var numIslands = function(grid) {
         
         visited.add(`${row}-${col}`);
         const fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-        fundDirs.forEach(([rowDiff, colDiff]) => {
+        for (let [rowDiff, colDiff] of fundDirs) {
             const [newRow, newCol] = [row + rowDiff, col + colDiff];
             dfs(newRow, newCol);
-        });
+        }
         return 1;
     };
-
-    // const bfs = (row, col) => {
-    //     const queue = [[row, col]];
-    //     visited.add(`${row}-${col}`);
-    //     while (queue.length) {
-    //         const [currRow, currCol] = queue.shift();
-    //         const fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    //         fundDirs.forEach(([rowDiff, colDiff]) => {
-    //         const [newRow, newCol] = [currRow + rowDiff, currCol + colDiff];
-    //             if (valid(newRow, newCol)) {
-    //                 queue.push([newRow, newCol]);
-    //                 visited.add(`${newRow}-${newCol}`);
-    //             }
-    //         });
-    //     }
-    // };
     
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
