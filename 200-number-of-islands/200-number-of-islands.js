6a64013daf597777305213ea28fd3e0b80588358
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 22:22:36 -0700

    Time: 210 ms (15.33%), Space: 57.5 MB (15.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -10,20 +10,20 @@ var numIslands = function(grid) {
         return row >= 0 && row < nRows && col >= 0 && col < nCols;
     };
     
-    const valid = (row, col, grid, visited) => {
+    const valid = (row, col) => {
         return inBounds(row, col) &&
             !visited.has(`${row}-${col}`) &&
             grid[row][col] === "1";
     };
     
-    const dfs = (row, col, grid, visited) => {
-        if (!valid(row, col, grid, visited)) return 0;
+    const dfs = (row, col) => {
+        if (!valid(row, col)) return 0;
         
         visited.add(`${row}-${col}`);
         const fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
         fundDirs.forEach(([rowDiff, colDiff]) => {
             const [newRow, newCol] = [row + rowDiff, col + colDiff];
-            dfs(newRow, newCol, grid, visited);
+            dfs(newRow, newCol);
         });
         return 1;
     };
@@ -31,7 +31,7 @@ var numIslands = function(grid) {
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[row].length; col++) {
             if (!visited.has(`${row}-${col}`)) {
-                ans += dfs(row, col, grid, visited);
+                ans += dfs(row, col);
             }
         }
     }
