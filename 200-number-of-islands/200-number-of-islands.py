d7883610c6248d58ff68386ceda014f52c10c25b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-12 23:18:27 -0700

    Time: 1003 ms (5.01%), Space: 21.9 MB (25.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -13,20 +13,24 @@ class Solution:
         def valid(row, col):
             return inBounds(row, col) and (row, col) not in visited and grid[row][col] == '1'
         
-        def dfs(row, col):
-            if not valid(row, col):
-                return 0
-            else:
-                visited.add((row, col))
+        def bfs(row, col):
+            q = collections.deque()
+            visited.add((row, col))
+            q.append((row, col))
+
+            while q:
+                currRow, currCol = q.popleft()
                 fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
                 for rowDiff, colDiff in fundDirs:
-                    newRow, newCol = row + rowDiff, col + colDiff
-                    dfs(newRow, newCol)
-                return 1
+                    newRow, newCol = currRow + rowDiff, currCol + colDiff
+                    if valid(newRow, newCol):
+                        q.append((newRow, newCol))
+                        visited.add((newRow, newCol))
             
         for row in range(ROWS):
             for col in range(COLS):
-                if (row, col) not in visited:
-                    ans += dfs(row, col)
+                if grid[row][col] == '1' and (row, col) not in visited:
+                    bfs(row, col)
+                    ans += 1
                     
         return ans
