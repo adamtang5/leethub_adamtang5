fae582ea14ce94d88c2184540dbf2f2345e6a96b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-15 23:12:21 -0700

    Time: 228 ms (80.00%), Space: 217.3 MB (53.33%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rb
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rb
@@ -2,23 +2,17 @@
 # @param {Integer} k
 # @return {Integer}
 def num_subarray_product_less_than_k(nums, k)
-    l, r, ans, prod = 0, 0, 0, nums[0]
-    while r < nums.length && l <= r
-        if prod < k
-            ans += r - l + 1
-            r += 1
-            if r < nums.length
-                prod *= nums[r]
-            end
-        elsif l < r
+    l, r, ans, prod = 0, 0, 0, 1
+    while r < nums.length
+        prod *= nums[r]
+        while prod >= k && l < r
             prod /= nums[l]
             l += 1
-        elsif l == r
-            r += 1
-            if r < nums.length
-                prod *= nums[r]
-            end
         end
+        if prod < k
+            ans += r - l + 1
+        end
+        r += 1
     end
     return ans
 end
