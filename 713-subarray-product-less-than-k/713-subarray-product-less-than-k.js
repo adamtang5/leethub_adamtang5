b400f20121c915c6c0be4861fb10af98fa24408f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-15 23:06:55 -0700

    Time: 157 ms (30.71%), Space: 48.4 MB (8.08%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.js
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) {
-    let [l, r, ans, prod] = [0, 0, 0, nums[0]];
-    while (r < nums.length && l <= r) {
-        // console.log(prod);
-        if (prod < k) {
-            ans += r - l + 1;
-            r++;
-            prod *= nums[r];
-        } else if (l < r) {
+    let [l, r, ans, prod] = [0, 0, 0, 1];
+    while (r < nums.length) {
+        prod *= nums[r];
+        while (prod >= k && l < r) {
             prod /= nums[l];
             l++;
-        } else if (l === r) {
-            r++;
-            prod *= nums[r];
         }
+        if (prod < k) {
+            ans += r - l + 1;
+        }
+        r++;
     }
     return ans;
 };
