30d76f5a0a2dd73e4739e0854e2e3a3166ae0609
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-17 23:11:51 -0700

    Time: 2450 ms (7.79%), Space: 24.6 MB (88.04%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        currStreak = ans = 0
+        for n in nums:
+            if n == 0:
+                currStreak += 1
+            else:
+                ans += currStreak * (currStreak + 1) // 2
+                currStreak = 0
+        ans += currStreak * (currStreak + 1) // 2
+        return ans
