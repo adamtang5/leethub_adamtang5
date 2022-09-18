a1361130f05d2c999722093f5b97c56fbae61290
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-17 22:35:51 -0700

    Time: 741 ms (13.18%), Space: 17.8 MB (27.42%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         ans, prefixSum = 0, 0
-        prefixSums = { 0: 1 }
+        prefixSums = collections.defaultdict(int)
+        prefixSums[0] += 1
         for n in nums:
             prefixSum += n
             diff = prefixSum - goal
             ans += prefixSums.get(diff, 0)
-            prefixSums[prefixSum] = prefixSums.get(prefixSum, 0) + 1
+            prefixSums[prefixSum] += 1
         return ans
     
