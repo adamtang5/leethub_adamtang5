435d25cf5d9c2cdb3e85db2fb7838b7f53d1d396
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-20 16:56:58 -0700

    Time: 133 ms (10.55%), Space: 43.4 MB (36.46%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+/*
+Pattern:
+Base case: n = 1 --> ()
+Initial open parens: n, n - 1, ... 1
+*/
+
+var generateParenthesis = function(n) {
+    const dp = {};
+    dp['0-0'] = [''];
+    dp['0-1'] = [')'];
+
+    const valid = (nOpen, nClose) => {
+        return nClose >= nOpen && nOpen >= 0 && nClose >= 0;
+    };
+    
+    const dfs = (nOpen, nClose) => {
+        if (dp[`${nOpen}-${nClose}`]) return dp[`${nOpen}-${nClose}`];
+        
+        let ans = [];
+        
+        if (valid(nOpen - 1, nClose)) {
+            ans = [...ans, ...dfs(nOpen - 1, nClose).map(seq => '(' + seq)];
+        }
+        if (valid(nOpen, nClose - 1)) {
+            ans = [...ans, ...dfs(nOpen, nClose - 1).map(seq => ')' + seq)];
+        }
+        dp[`${nOpen}-${nClose}`] = ans;
+        return ans;
+    };
+    
+    return dfs(n, n);
+};
