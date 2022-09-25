adb9d2c5759ba9af1f939a322e13f9a0dbfab2bd
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-24 23:25:00 -0700

    Time: 140 ms (22.07%), Space: 44.8 MB (43.12%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    // trim leading ')'
+    let l = 0;
+    while (s[l] === ')') {
+        l++;
+    }
+    
+    // trim trailing '('
+    let r = s.length;
+    while (s[r - 1] === '(') {
+        r--;
+    }
+    s = s.slice(l, r);
+    
+    let mirrored = s.split('').map(paren => paren === ')' ? '(' : ')').reverse().join('');
+    console.log(s, mirrored);
+    
+    const parse = s => {
+        let [ans, stackHeight, validLen] = [0, 0, 0];
+
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === '(') {
+                stackHeight++;
+                validLen++;
+            } else {
+                stackHeight--;
+                if (stackHeight < 0) {
+                    validLen = 0;
+                    stackHeight = 0;
+                } else {
+                    validLen++;
+                }
+                if (stackHeight === 0) ans = Math.max(ans, validLen);
+            }
+        }
+        
+        return ans;
+    };
+    
+    return Math.max(parse(s), parse(mirrored));
+};
