77c802b932bc6618eb9a8852d0f838750bec8ed0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-25 22:39:33 -0700

    Time: 73 ms (92.31%), Space: 44.5 MB (66.67%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
@@ -7,6 +7,7 @@ var isValid = function(s) {
     if (s === '') return true;
     if (s.length % 3) return false;
     if (!s.includes('abc')) return false;
+    if (s[0] !== 'a' || s[s.length - 1] !== 'c') return false;
     
     // recursive case
     const idx = s.indexOf('abc');
