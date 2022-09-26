25d2698442a0a0d8c4104dc74484d54497301019
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-25 23:20:40 -0700

    Time: 303 ms (33.33%), Space: 212.1 MB (66.67%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.rb
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.rb
@@ -1,11 +1,21 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
-    # base case
+    # edge cases
     return true if s == ''
     return false if s.length % 3 != 0 || !s.include?('abc') || s[0] != 'a' || s[-1] != 'c'
     
-    # recursive case
-    idx = s.index('abc')
-    return is_valid(s[0...idx] + s[idx+3..-1])
+    stack = []
+    (0...s.length).each do |i|
+        if s[i] != 'c'
+            stack << s[i]
+        elsif stack.length < 2
+            return false
+        elsif stack[-1] == 'b' && stack[-2] == 'a'
+            stack = stack[0...-2]
+        else
+            return false
+        end
+    end
+    return stack.length == 0
 end
