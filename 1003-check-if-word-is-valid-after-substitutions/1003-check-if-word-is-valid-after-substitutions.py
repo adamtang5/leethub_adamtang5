5d8436f54ba4d9947afbf19b5d91b7157cdfdc50
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-25 23:11:14 -0700

    Time: 109 ms (47.26%), Space: 14.1 MB (55.91%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -1,11 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # base case
+        # edge cases
         if s == '':
             return True
-        elif len(s) % 3 or 'abc' not in s or s[0] != 'a' or s[-1] != 'c':
+        if len(s) % 3 or 'abc' not in s or s[0] != 'a' or s[-1] != 'c':
             return False
         
-        # recursive case
-        idx = s.find('abc')
-        return self.isValid(s[0:idx] + s[idx+3:])
+        stack = []
+        for i in range(len(s)):
+            if s[i] != 'c':
+                stack.append(s[i])
+            elif len(stack) < 2:
+                return False
+            elif stack[-1] == 'b' and stack[-2] == 'a':
+                del stack[-2:]
+            else:
+                return False
+            
+        return len(stack) == 0
