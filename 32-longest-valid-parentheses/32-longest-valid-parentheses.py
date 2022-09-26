65ca9f029b6e22da48e2eb009988bab5a1c34ade
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-25 22:19:25 -0700

    Time: 72 ms (59.24%), Space: 14.2 MB (77.94%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,26 +1,11 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        # trim leading ')' and trailing '('
-        s = s.lstrip(')').rstrip('(')
-        if s == '':
-            return len(s)
-        
-        mirrored = ''.join(['(' if paren == ')' else ')' for paren in s[::-1]])
-        
-        def parse(s):
-            ans = stackHeight = validLen = 0
-            for paren in s:
-                if paren == '(':
-                    stackHeight += 1
-                    validLen += 1
-                else:
-                    stackHeight -= 1
-                    if stackHeight < 0:
-                        validLen = stackHeight = 0
-                    else:
-                        validLen += 1
-                    if stackHeight == 0:
-                        ans = max(ans, validLen)
-            return ans
-        
-        return max(parse(s), parse(mirrored))
+        if len(s) < 2:
+            return 0
+        dp = [0] * len(s)
+        for i in range(len(s)):
+            if s[i] == ')' and i-1 >= 0 and s[i-1] == '(':
+                dp[i] = dp[i-2] + 2
+            elif s[i] == ')' and i-1 - dp[i-1] >= 0 and s[i-1] == ')' and s[i-1 - dp[i-1]] == '(':
+                dp[i] = dp[i-1] + dp[i-2 - dp[i-1]] + 2
+        return max(dp)
