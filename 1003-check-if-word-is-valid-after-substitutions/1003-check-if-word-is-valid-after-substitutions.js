c2a5e8e8740aaaeb5286b2665b03b25db6023f8d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-25 22:38:01 -0700

    Time: 532 ms (5.13%), Space: 74.5 MB (5.13%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    // base case
+    if (s === '') return true;
+    if (s.length % 3) return false;
+    if (!s.includes('abc')) return false;
+    
+    // recursive case
+    const idx = s.indexOf('abc');
+    return isValid(s.slice(0, idx) + s.slice(idx + 3));
+};
