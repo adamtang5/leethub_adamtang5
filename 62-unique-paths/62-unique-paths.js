d131ef2c4cbc65dfe0b749d2c0caf9aa2d70806b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-26 11:59:59 -0700

    Time: 101 ms (34.49%), Space: 41.9 MB (82.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,11 +4,20 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    // base cases
-    if (m === 1 || n === 1) return 1;
-    if (m === 2) return n;
-    if (n === 2) return m;
+    const dp = new Array(m).fill().map(() => new Array(n));
+    for (let i = 0; i < m; i++) {
+        dp[i][n-1] = 1;
+    }
     
-    // recursive case
-    return uniquePaths(m, n-1) + uniquePaths(m-1, n);
+    for (let i = 0; i < n; i++) {
+        dp[m-1][i] = 1;
+    }
+    
+    for (let r = m-2; r >= 0; r--) {
+        for (let c = n-2; c >= 0; c--) {
+            dp[r][c] = dp[r+1][c] + dp[r][c+1];
+        }
+    }
+    
+    return dp[0][0];
 };
