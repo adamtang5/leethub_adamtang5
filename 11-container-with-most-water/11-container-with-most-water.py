9ccc65fadab603ecc38d6262d3b1de33bd6a8690
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-27 15:55:34 -0700

    Time: 1666 ms (26.89%), Space: 27.5 MB (11.25%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -3,8 +3,11 @@ class Solution:
         l, r, ans = 0, len(height)-1, 0
         while l < r:
             ans = max(ans, (r-l) * min(height[r], height[l]))
+            currLeft, currRight = height[l], height[r]
             if height[l] <= height[r]:
-                l += 1
+                while l < r and height[l] <= currLeft:
+                    l += 1
             else:
-                r -= 1
+                while l < r and height[r] <= currRight:
+                    r -= 1
         return ans
