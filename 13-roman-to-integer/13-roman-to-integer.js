98bf51d894a5d8ffb20287e021e05ebec9f03aa1
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-28 15:03:00 -0700

    Time: 258 ms (19.79%), Space: 47.9 MB (37.27%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const lookup = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    
+    let [ans, i] = [0, 0];
+    
+    while (i < s.length) {
+        if (lookup[s[i]] < lookup[s[i + 1]]) {
+            ans += lookup[s[i + 1]] - lookup[s[i]];
+            i += 2;
+        } else {
+            ans += lookup[s[i]];
+            i++;
+        }
+    }
+    
+    return ans;
+};
