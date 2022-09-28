d5f22b0fb6f7c83fd36ba4596f44782d30d390d8
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-09-28 15:57:53 -0700

    Time: 92 ms (90.49%), Space: 211.1 MB (23.88%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.rb
+++ 14-longest-common-prefix/14-longest-common-prefix.rb
@@ -1,8 +1,12 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
-    ans = ''
-    minLength = strs.map{ |s| s.length }.min
+    ans, minLength = '', 1/0.0
+    strs.each do |str|
+        if str.length < minLength
+            minLength = str.length
+        end
+    end
     
     (0...minLength).each do |i| 
         ch = strs[0][i]
