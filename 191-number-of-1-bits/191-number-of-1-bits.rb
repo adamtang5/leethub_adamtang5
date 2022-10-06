ba8994be99df93d1fbbca5c62d1175b2b0576eb5
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-06 16:39:34 -0700

    Time: 147 ms (37.62%), Space: 210.9 MB (56.44%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.rb
+++ 191-number-of-1-bits/191-number-of-1-bits.rb
@@ -1,5 +1,12 @@
 # @param {Integer} n, a positive integer
 # @return {Integer}
 def hamming_weight(n)
-    return n.to_s(2).chars.select { |d| d == '1' }.length
+    count = 0
+    while n > 0
+        if n % 2 > 0
+            count += 1
+        end
+        n = (n - (n % 2)) / 2
+    end
+    return count
 end
