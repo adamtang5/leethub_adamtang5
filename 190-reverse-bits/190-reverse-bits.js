505d0dc8a30d108bc8d79ee1bf64719aa0d6d3c6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-06 17:24:06 -0700

    Time: 81 ms (83.59%), Space: 43.9 MB (75.31%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -3,7 +3,7 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    let reversedArr = n.toString(2).split('').reverse();
-    let right = new Array(32 - reversedArr.length).fill('0');
+    const reversedArr = n.toString(2).split('').reverse();
+    const right = new Array(32 - reversedArr.length).fill('0');
     return parseInt([...reversedArr, ...right].join(''), 2);
 };
