56e7da73e4f595919218a896a99b9939d08c2a63
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-07 21:53:53 -0700

    Time: 59 ms (45.85%), Space: 13.9 MB (50.16%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        revStr = format(n, 'b')[::-1]
+        right = '0' * (32-len(revStr))
+        return int(revStr+right, 2)
+        
