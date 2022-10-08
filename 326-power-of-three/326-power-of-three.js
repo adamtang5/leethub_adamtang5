690a5cbe9eafb46005f3948d75e9600708fa0bd8
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-07 22:27:15 -0700

    Time: 366 ms (41.34%), Space: 51 MB (75.12%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while (n > 1) {
+        if (n % 3) return false;
+        n /= 3;
+    }
+    return n == 1;
+};
