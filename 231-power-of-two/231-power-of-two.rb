24374acf94a2b1b48e1cbb842d1f3d8ad813f44c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-07 23:08:07 -0700

    Time: 158 ms (37.84%), Space: 210.9 MB (86.49%) - LeetHub

--- 231-power-of-two/231-power-of-two.rb
+++ 231-power-of-two/231-power-of-two.rb
@@ -1,11 +1,6 @@
 # @param {Integer} n
 # @return {Boolean}
 def is_power_of_two(n)
-    while n > 1
-        if n % 2 > 0
-            return false
-        end
-        n /= 2
-    end
-    return n == 1
+    return false if n <= 0
+    return n & (-n) == n
 end
