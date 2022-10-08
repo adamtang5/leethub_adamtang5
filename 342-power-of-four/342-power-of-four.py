0963159ec5a1d0a1257620fb0df603433963b50f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-07 23:34:29 -0700

    Time: 51 ms (52.04%), Space: 13.8 MB (94.89%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,7 +1,8 @@
+import math
+
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        while n > 1:
-            if n % 4:
-                return False
-            n /= 4
-        return n == 1
+        if n <= 0:
+            return False
+        power = math.floor(math.log(n) / math.log(4))
+        return n == 4 ** power
