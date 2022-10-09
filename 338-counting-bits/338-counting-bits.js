f549956066c5d6ad33c216fa8b8d0def271c1773
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-09 10:52:39 -0700

    Time: 134 ms (72.47%), Space: 47.4 MB (96.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -3,10 +3,15 @@
  * @return {number[]}
  */
 var countBits = function(n) {
+    if (n === 0) return [0];
     const ans = new Array(n+1).fill(0);
+    let offset = 1;
     
-    for (let i = 0; i <= n; i++) {
-        ans[i] = i.toString(2).split('').filter(d => d === '1').length;
+    for (let i = 1; i <= n; i++) {
+        if (i === offset * 2) {
+            offset = i;
+        }
+        ans[i] = 1 + ans[i - offset];
     }
     
     return ans;
