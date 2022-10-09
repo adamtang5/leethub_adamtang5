5af2a861953c60bfda225cc9169cd9c76dd1a9b4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-09 11:31:43 -0700

    Time: 79 ms (100.00%), Space: 211 MB (88.89%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.rb
+++ 461-hamming-distance/461-hamming-distance.rb
@@ -2,5 +2,5 @@
 # @param {Integer} y
 # @return {Integer}
 def hamming_distance(x, y)
-    return (x ^ y).to_s(2).split('').filter { |d| d == '1' }.length
+    return (x^y).to_s(2).split('').filter{ |d| d == '1' }.length
 end
