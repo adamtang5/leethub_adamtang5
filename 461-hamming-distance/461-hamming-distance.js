8fd65ffc5e57fc51524bb232ec3ca1b2c7d3f700
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-09 11:07:35 -0700

    Time: 83 ms (74.49%), Space: 42.1 MB (53.28%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    return (x ^ y).toString(2).split('').filter(d => d === '1').length;
+};
