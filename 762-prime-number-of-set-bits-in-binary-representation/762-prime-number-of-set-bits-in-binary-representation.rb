d3cbf06f0f9528ff5f3bedf859a1ddd057fcb95a
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-10 00:21:32 -0700

    Time: 961 ms (36.36%), Space: 210.9 MB (90.91%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.rb
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.rb
@@ -1,14 +1,7 @@
 # @param {Integer} left
 # @param {Integer} right
 # @return {Integer}
+require 'prime'
 def count_prime_set_bits(left, right)
-    count = 0
-    primes = Set[2, 3, 5, 7, 11, 13, 17, 19]
-    (left..right).each do |i|
-        bits = i.to_s(2).split('').filter{ |d| d == '1' }.length
-        if primes.include?(bits)
-            count += 1
-        end
-    end
-    return count
+    (left..right).count{|x| x.to_s(2).count(?1).prime?}
 end
