f425102918184f3def87a57ca77cad325fde88b2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-13 23:12:26 -0700

    Time: 3574 ms (15.96%), Space: 13.9 MB (63.00%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,7 +3,8 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         ans = set()
-        for i in range(len(nums)-3):
+        i = 0
+        while i < len(nums)-3:
             j = i+1
             while j < len(nums)-2:
                 l, r = j+1, len(nums)-1
@@ -19,4 +20,8 @@ class Solution:
                 while nums[j+1] == nums[j] and j < len(nums)-2:
                     j += 1
                 j += 1
+
+            while nums[i+1] == nums[i] and i < len(nums)-3:
+                i += 1
+            i += 1
         return [json.loads(s) for s in list(ans)]
