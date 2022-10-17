70c19e8bb3370fabd6d981c95b90ae09075f47e6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-17 12:24:57 -0700

    Time: 180 ms (20.54%), Space: 47.8 MB (6.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,29 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    const isGood = arr => {
-        const firstZeroIdx = arr.indexOf(0);
-        if (firstZeroIdx === -1) return true;
-        for (let i = firstZeroIdx; i < arr.length; i++) {
-            if (arr[i] !== 0) return false;
-        }
-        return true;
-    };
-    
     const start = nums.indexOf(0);
     if (start === -1) return nums;
     let [clean, dirty] = [start, start];
     
     while (dirty < nums.length && clean < nums.length) {
         if (clean > dirty) [nums[clean], nums[dirty]] = [nums[dirty], nums[clean]];
-        // if (isGood(nums)) break;
         while (dirty < nums.length && nums[dirty] !== 0) {
             dirty++;
-            console.log("dirty:", dirty);
         }
         while (clean < nums.length && nums[clean] === 0) {
             clean++;
-            console.log("clean:", clean);
         }
     }
     return nums;
