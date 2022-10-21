1183bfc656896ea9fd0b05cad76cae9bfec35ecc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-20 20:08:42 -0700

    Time: 167 ms (22.06%), Space: 210.8 MB (98.53%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.rb
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.rb
@@ -2,5 +2,27 @@
 # @param {String} needle
 # @return {Integer}
 def str_str(haystack, needle)
-    return !haystack.include?(needle) ? -1 : haystack.index(needle)
+    h_idx = 0
+    while h_idx <= haystack.length - needle.length
+        while h_idx <= haystack.length - needle.length && haystack[h_idx] != needle[0]
+            h_idx += 1
+        end
+        if h_idx > haystack.length-needle.length
+            return -1
+        end
+        aux = 0
+        while aux < needle.length do
+            if haystack[h_idx+aux] != needle[aux]
+                break
+            else
+                aux += 1
+            end
+        end
+        if aux == needle.length
+            return h_idx
+        else
+            h_idx += 1
+        end
+    end
+    return -1
 end
