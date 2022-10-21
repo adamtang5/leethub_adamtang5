bad4cc3c54cacfe1736d6564dad54b4c1fbbb1e4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-20 20:17:24 -0700

    Time: 110 ms (20.53%), Space: 42 MB (54.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,26 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    let hIdx = 0;
+    const lenDiff = haystack.length - needle.length;
+    while (hIdx <= lenDiff) {
+        while (hIdx <= lenDiff && haystack[hIdx] !== needle[0]) {
+            hIdx++;
+        }
+        if (hIdx > lenDiff) return -1;
+        let aux = 0;
+        while (aux < needle.length) {
+            if (haystack[hIdx + aux] != needle[aux]) {
+                break
+            } else {
+                aux++;
+            }
+        }
+        if (aux === needle.length) {
+            return hIdx;
+        } else {
+            hIdx++;
+        }
+    }
+    return -1;
 };
