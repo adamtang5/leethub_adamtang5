840883eff64baf1a163fadeb198068c5cfb4462e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-20 20:41:07 -0700

    Time: 278 ms (48.33%), Space: 14.4 MB (28.49%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,9 +2,9 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         if len(s) < 2:
             return s
-        rev_s = s[::-1]
-        pre, post = s, rev_s
+        revStr = s[::-1]
+        pre, post = s, revStr
         while pre != post:
             pre = pre[0:-1]
             post = post[1:]
-        return rev_s[0:-len(pre)] + s
+        return revStr[0:-len(pre)]+s
