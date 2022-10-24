1898e9aaa84e4a19d7ec856ad8d6d278e03b3485
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 12:05:39 -0700

    Time: 223 ms (49.34%), Space: 212.6 MB (56.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.rb
@@ -0,0 +1,15 @@
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+    s = s.downcase.gsub(/[^a-z0-9]/, '')
+    l, r = 0, s.length - 1
+    while l < r
+        if s[l] != s[r]
+            return false
+        else
+            l += 1
+            r -= 1
+        end
+    end
+    return true
+end
