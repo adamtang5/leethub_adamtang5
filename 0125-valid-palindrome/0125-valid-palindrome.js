555a4e19b5b6180e9048adb7f274e06b299cbeeb
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 11:29:48 -0700

    Time: 124 ms (48.57%), Space: 44.6 MB (68.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,15 +3,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const re = /^[a-z0-9]$/;
-    s = s.toLowerCase().split('').filter(ch => re.test(ch)).join('');
+    const isAlphaNum = ch => {
+        return ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9';
+    };
+    
+    s = s.toLowerCase();
     let [l, r] = [0, s.length - 1];
     while (l < r) {
-        if (s[l] !== s[r]) {
-            return false;
-        } else {
+        if (!isAlphaNum(s[l])) {
             l++;
+        } else if (!isAlphaNum(s[r])) {
             r--;
+        } else {
+            if (s[l] !== s[r]) {
+                return false;
+            } else {
+                l++;
+                r--;
+            }            
         }
     }
     return true;
