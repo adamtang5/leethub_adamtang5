4684b61dd28959c1bc5ceb361069663e4a7d4d42
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 22:25:25 -0700

    Time: 222 ms (46.67%), Space: 211.2 MB (86.67%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.rb
+++ 1260-shift-2d-grid/1260-shift-2d-grid.rb
@@ -3,5 +3,5 @@
 # @return {Integer[][]}
 def shift_grid(grid, k)
     cols = grid[0].length
-    return grid.flatten.rotate!(-k).each_slice(cols).to_a
+    return grid.flatten!.rotate!(-k).each_slice(cols).to_a
 end
