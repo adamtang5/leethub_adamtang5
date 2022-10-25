effa2640e3d3fffe4c8e26fd1e8f14ffc0c3d4a2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 22:38:09 -0700

    Time: 311 ms (72.78%), Space: 14.4 MB (15.83%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,12 +1,6 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         COLS = len(grid[0])
-        grid1D = [n for row in grid for n in row]
-        shifted1D = [0] * len(grid1D)
-        for i in range(len(grid1D)):
-            shifted1D[(i+k) % len(grid1D)] = grid1D[i]
-        for i in range(len(shifted1D)):
-            row = i // COLS
-            col = i % COLS
-            grid[row][col] = shifted1D[i]
-        return grid
+        flattened = [n for row in grid for n in row]
+        shifted = flattened[-(k % len(flattened)):] + flattened[:-(k % len(flattened))]
+        return [shifted[i:i+COLS] for i in range(0, len(shifted), COLS)]
