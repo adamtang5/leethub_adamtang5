b5ecf25cbbeccc24d6b415c62a834d5ad02ef067
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 21:37:18 -0700

    Time: 171 ms (44.07%), Space: 69.5 MB (34.81%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -6,14 +6,16 @@
 var change = function(amount, coins) {
     coins.sort((a, b) => a - b);
     const dp = new Array(coins.length).fill().map(() => new Array(amount + 1).fill(0));
-    dp.forEach(row => row[0] = 1);
+    
+    dp.forEach(row => row[amount] = 1);
+    
     for (let row = coins.length - 1; row >= 0; row--) {
         const val = coins[row];
-        for (let i = 1; i <= amount; i++) {
-            const lookDown = dp[row + 1] ? dp[row + 1][i] : 0;
-            const lookLeft = dp[row][i - val] || 0;
-            dp[row][i] = lookDown + lookLeft;
+        for (let col = amount - 1; col >= 0; col--) {
+            const lookDown = dp[row + 1] ? dp[row + 1][col] : 0;
+            const lookRight = dp[row][col + val] || 0;
+            dp[row][col] = lookDown + lookRight;
         }
     }
-    return dp[0][amount];
+    return dp[0][0];
 };
