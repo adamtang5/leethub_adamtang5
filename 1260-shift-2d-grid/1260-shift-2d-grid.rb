3b8c3adad2ea4f7e43f32aef7fd677011c59dbb7
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-24 22:25:09 -0700

    Time: 253 ms (46.67%), Space: 211.5 MB (40.00%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.rb
+++ 1260-shift-2d-grid/1260-shift-2d-grid.rb
@@ -3,13 +3,5 @@
 # @return {Integer[][]}
 def shift_grid(grid, k)
     cols = grid[0].length
-    grid_1d = grid.flatten
-    shifted_1d = [0] * grid_1d.length
-    (0...grid_1d.length).each { |i| shifted_1d[(i+k) % grid_1d.length] = grid_1d[i] }
-    (0...shifted_1d.length).each do |i|
-        row = i / cols
-        col = i % cols
-        grid[row][col] = shifted_1d[i]
-    end
-    return grid
+    return grid.flatten.rotate!(-k).each_slice(cols).to_a
 end
