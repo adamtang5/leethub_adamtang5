18a0c890c432ff991474ce8a4ca47ecc610154f3
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-25 10:59:07 -0700

    Time: 122 ms (94.06%), Space: 216 MB (31.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.rb
+++ 0205-isomorphic-strings/0205-isomorphic-strings.rb
@@ -2,12 +2,5 @@
 # @param {String} t
 # @return {Boolean}
 def is_isomorphic(s, t)
-    st_map, ts_map = Hash.new, Hash.new
-    (0...s.length).each do |i|
-        return false if st_map[s[i]] && st_map[s[i]] != t[i]
-        st_map[s[i]] = t[i]
-        return false if ts_map[t[i]] && ts_map[t[i]] != s[i]
-        ts_map[t[i]] = s[i]
-    end
-    return true
+    return s.chars.map{ |ch| s.index(ch) } == t.chars.map{ |ch| t.index(ch) }
 end
