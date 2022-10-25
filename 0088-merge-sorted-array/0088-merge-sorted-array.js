2fbf4aaf76f126a6a140bf1153599d6e2ec94b94
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-25 14:35:29 -0700

    Time: 117 ms (13.08%), Space: 42 MB (63.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,19 +5,8 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-
-/*
-use pointer i = m-1, r = m+n-1 in nums1, pointer j = n-1 in nums2
-edge case: m = 0, n > 0
-iterate through nums1, map nums2[i] => nums1[i]
-
-do until j < 0
-if nums1[i] <= nums2[j] then nums1[r] = nums2[j], r--, j--
-else if nums1[i] > nums2[j] then nums1[r] = nums1[i], r--, i--
-*/
-
 var merge = function(nums1, m, nums2, n) {
-    let [i, r, j] = [m - 1, m + n - 1, n - 1];  // 0, 1, 0
+    let [i, r, j] = [m - 1, m + n - 1, n - 1];
     while (j >= 0 && i >= 0) {
         if (nums1[i] <= nums2[j]) {
             nums1[r] = nums2[j];
