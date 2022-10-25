9498ada5292b9a8d5f442e64836efec356d207d0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-25 13:28:00 -0700

    Time: 89 ms (67.99%), Space: 42.2 MB (39.20%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -17,29 +17,22 @@ else if nums1[i] > nums2[j] then nums1[r] = nums1[i], r--, i--
 */
 
 var merge = function(nums1, m, nums2, n) {
-    // edge case: m = 0, n > 0
-    if (m === 0 && n > 0) {
-        for (let i = 0; i < n; i++) {
-            nums1[i] = nums2[i];
-        }
-    } else {
-        let [i, r, j] = [m - 1, m + n - 1, n - 1];  // 0, 1, 0
-        while (j >= 0 && i >= 0) {
-            if (nums1[i] <= nums2[j]) {
-                nums1[r] = nums2[j];
-                r--;
-                j--;
-            } else {
-                nums1[r] = nums1[i];
-                r--;
-                i--;
-            }
-        }
-        if (i < 0) {
-            for (let k = j; k >= 0; k--) {
-                nums1[k] = nums2[k];
-            }
+    let [i, r, j] = [m - 1, m + n - 1, n - 1];  // 0, 1, 0
+    while (j >= 0 && i >= 0) {
+        if (nums1[i] <= nums2[j]) {
+            nums1[r] = nums2[j];
+            r--;
+            j--;
+        } else {
+            nums1[r] = nums1[i];
+            r--;
+            i--;
         }
     }
-    
+    // i = -1
+    if (i < 0) {
+        for (let k = j; k >= 0; k--) {
+            nums1[k] = nums2[k];
+        }
+    }
 };
