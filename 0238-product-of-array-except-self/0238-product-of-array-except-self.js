bcdd1541a78cbfbe7a5d351e3c81354d81d807ff
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-27 16:46:23 -0700

    Time: 118 ms (89.36%), Space: 53.9 MB (68.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -7,7 +7,7 @@ var productExceptSelf = function(nums) {
     let currProd = 1;
     for (let i = 0; i < nums.length - 1; i++) {
         currProd *= nums[i];
-        ans[i + 1] *= currProd;
+        ans[i + 1] = currProd;
     }
     currProd = 1;
     for (let i = nums.length - 1; i >= 1; i--) {
