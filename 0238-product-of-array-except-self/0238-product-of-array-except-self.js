2f7705289dc9ee4ea10207db5042427f30143daa
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-27 16:42:36 -0700

    Time: 178 ms (61.57%), Space: 53.6 MB (75.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,22 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const n = nums.length;
-    const prefix = Array(n);
-    prefix[0] = 1;
-    for (let i = 1; i < n; i++) {
-        prefix[i] = prefix[i - 1] * nums[i - 1];
+    const ans = Array(nums.length).fill(1);
+    let currProd = 1;
+    for (let i = 0; i < nums.length - 1; i++) {
+        currProd *= nums[i];
+        ans[i + 1] *= currProd;
     }
-    
-    const suffix = Array(n);
-    suffix[suffix.length - 1] = 1;
-    for (let i = n - 2; i >= 0; i--) {
-        suffix[i] = suffix[i + 1] * nums[i + 1];
-    }
-
-    const ans = Array(n);
-    for (let i = 0; i < n; i++) {
-        ans[i] = prefix[i] * suffix[i];
+    currProd = 1;
+    for (let i = nums.length - 1; i >= 1; i--) {
+        currProd *= nums[i];
+        ans[i - 1] *= currProd;
     }
 
     return ans;
