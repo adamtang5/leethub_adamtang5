cfec207f84dff542f93016731ff4a52ef63db8e9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-28 10:58:44 -0700

    Time: 155 ms (74.69%), Space: 48.2 MB (39.37%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = Array(amount + 1).fill(Infinity)
+    dp[0] = 0;
+    for (i = 1; i <= amount; i++) {
+        for (let coin of coins) {
+            if (i >= coin) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+    }
+    return (dp[amount] === Infinity) ? -1 : dp[amount];
+};
