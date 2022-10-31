eef6c41881c7072138445646a34563ff50c71482
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-10-30 23:21:37 -0700

    Time: 2809 ms (100.00%), Space: 213.5 MB (100.00%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.rb
@@ -0,0 +1,46 @@
+# @param {Character[][]} maze
+# @param {Integer[]} entrance
+# @return {Integer}
+def distance(coord1, coord2)
+    r1, c1 = coord1
+    r2, c2 = coord2
+    return (r1-r2).abs + (c1-c2).abs
+end
+
+def valid(r, c, maze, visited)
+    return r >= 0 && r < maze.length && c >= 0 && c < maze[0].length && !visited.include?([r, c]) && maze[r][c] != '+'
+end
+
+def is_exit(r, c, maze, entrance)
+    return (r == 0 || r == maze.length-1 || c == 0 || c == maze[0].length-1) && [r, c] != entrance && maze[r][c] == '.'
+end
+
+def nearest_exit(maze, entrance)
+    steps = Array.new(maze.length) { [1 / 0.0] * maze[0].length }
+    steps[entrance[0]][entrance[1]] = 0
+    visited = Set.new
+    visited.add(entrance)
+    exits = Set.new
+    maze.each_with_index do |row, r|
+        row.each_with_index do |col, c|
+            if is_exit(r, c, maze, entrance)
+                exits.add([r, c])
+            end
+        end
+    end
+    queue = [entrance]
+    dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    while queue.length > 0
+        r, c = queue.shift
+        dirs.each do |rd, cd|
+            nr, nc = r+rd, c+cd
+            if valid(nr, nc, maze, visited)
+                steps[nr][nc] = steps[r][c]+1
+                return steps[nr][nc] if exits.include?([nr, nc])
+                visited.add([nr, nc])
+                queue << [nr, nc]
+            end
+        end
+    end
+    return -1
+end
