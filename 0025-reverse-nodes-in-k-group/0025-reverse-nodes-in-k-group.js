43a7cacab7a2c900013a718b4d0b5157263cb609
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-03 23:53:33 -0700

    Time: 128 ms (60.03%), Space: 45.5 MB (33.08%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    let [curr, prev, rev, len, i] = [head, head, null, 0, 0];
+    let remain, newPrev;
+    while (curr) {
+        curr = curr.next;
+        len++;
+    }
+    curr = head;
+    
+    while (len - i >= k) {
+        let j = 0;
+        rev = null;
+        while (j < k) {
+            remain = curr.next;
+            curr.next = rev;
+            rev = curr;
+            curr = remain;
+            j++;
+        }
+        if (i === 0) {
+            prev.next = remain;
+            head = rev;
+        } else {
+            prev.next.next = remain;
+            newPrev = prev.next;
+            prev.next = rev;
+            prev = newPrev;
+        }
+        i += k;
+    }
+    
+    return head;
+};
