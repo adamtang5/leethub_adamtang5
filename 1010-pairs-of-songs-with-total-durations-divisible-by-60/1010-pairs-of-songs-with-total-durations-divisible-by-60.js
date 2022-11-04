22e9b0c50cbe3076a4ef499f674dffe64dace5a0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-04 00:21:35 -0700

    Time: 97 ms (79.95%), Space: 44.3 MB (98.51%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -4,14 +4,11 @@
  */
 var numPairsDivisibleBy60 = function(time) {
     const counts = new Array(60).fill(0);
-    let numPairs = 0;
-    
     time.forEach(min => counts[min % 60]++);
     
     const rr = n => n * (n - 1) / 2;
     
-    numPairs += rr(counts[0]);
-    numPairs += rr(counts[30]);
+    let numPairs = rr(counts[0]) + rr(counts[30]);
     
     for (let i = 1; i < 30; i++) {
         numPairs += counts[i] * counts[60 - i];
