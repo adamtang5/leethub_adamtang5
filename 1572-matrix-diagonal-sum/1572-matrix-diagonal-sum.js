cedc16d408940df252f5ff059113849e6aacfabc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-04 00:35:08 -0700

    Time: 107 ms (35.86%), Space: 43.4 MB (77.29%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -7,7 +7,7 @@ var diagonalSum = function(mat) {
     
     for (let i = 0; i < n; i++) {
         sum += mat[i][i];
-        sum += mat[n - 1 - i][i];
+        sum += mat[n - i - 1][i];
     }
     if (n % 2) sum -= mat[(n - 1) / 2][(n - 1) / 2];
     
