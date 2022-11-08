1aa9bdc3f04386830108919e02c107a80daaf801
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-08 14:18:59 -0800

    Time: 1511 ms (38.89%), Space: 213 MB (33.33%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.rb
@@ -0,0 +1,37 @@
+# @param {String} s
+# @param {String[]} words
+# @return {Integer[]}
+def find_substring(s, words)
+    ans = []
+    tally = Hash.new(0)
+    words.each { |word| tally[word] += 1 }
+    
+    (0...words[0].length).each do |offset|
+        start = offset
+        copy = tally.dup
+        curr_words = []
+        (offset..s.length-words[0].length).step(words[0].length).each do |i|
+            curr_word, j = '', 0
+            while j < words[0].length
+                curr_word += s[i+j]
+                j += 1
+            end
+            start = [i+j-words.length*words[0].length, start].max
+            curr_words << curr_word
+            if curr_words.length-words.length-1 >= 0
+                popped = curr_words[-words.length-1]
+                if copy[popped]
+                    copy[popped] += 1
+                end
+            end
+            if copy[curr_word]
+                copy[curr_word] -= 1
+                if copy.values.all?{ |val| val == 0 }
+                    ans << start
+                end
+            end
+        end
+    end
+    ans.sort!
+    return ans
+end
