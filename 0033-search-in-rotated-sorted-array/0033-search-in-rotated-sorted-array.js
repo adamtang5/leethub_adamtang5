077a4596612369335c47490c77b1ae0e481405c2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-12 23:09:18 -0800

    Time: 120 ms (10.04%), Space: 44.6 MB (5.08%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let ans = -1;
+    let [l, r] = [0, nums.length - 1];
+    let maxIdx;
+    if (nums[0] < nums[nums.length - 1]) {
+        maxIdx = nums.length - 1;
+    } else {
+        maxIdx = Math.floor((r + l) / 2);
+        while (l <= r && nums[maxIdx + 1] > nums[maxIdx]) {
+            console.log(maxIdx, nums[maxIdx], l, r);
+            if (nums[maxIdx] > nums[0]) {
+                l = maxIdx + 1;
+            } else if (nums[maxIdx] < nums[0]) {
+                r = maxIdx - 1;
+            } else {
+                l++;
+            }
+            maxIdx = Math.floor((r + l) / 2);
+        }
+    }
+    console.log(maxIdx, nums[maxIdx]);
+    
+    if (maxIdx < nums.length - 1) {
+        if (target > nums[0]) {
+            [l, r] = [0, maxIdx];
+        } else if (target < nums[0]) {
+            [l, r] = [maxIdx + 1, nums.length - 1];
+        } else {
+            return 0;
+        }
+    }
+    let pivot = Math.floor((r + l) / 2);
+    while (l <= r && pivot >= 0 && pivot < nums.length && nums[pivot] !== target) {
+        if (nums[pivot] > target) {
+            r = pivot - 1;
+        } else if (nums[pivot] < target) {
+            l = pivot + 1;
+        }
+        pivot = Math.floor((r + l) / 2);
+    }
+    if (l > r) return ans;
+    if (nums[pivot] === target) return pivot;
+};
