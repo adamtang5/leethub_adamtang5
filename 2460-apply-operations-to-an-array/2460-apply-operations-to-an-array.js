0131d2763297ed64877b4aad465c2b8017c9f7aa
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-14 16:45:57 -0800

    Time: 174 ms (5.29%), Space: 44.9 MB (5.73%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+    console.log(nums);
+
+    let [l, r] = [0, 1];
+    while (r < nums.length && l < r) {
+        while (l < nums.length && nums[l] !== 0) {
+            l++;
+        }
+        if (l === nums.length) break;
+        if (l + 1 < nums.length) {
+            r = l + 1;
+            while (nums[r] === 0) {
+                r++;
+            }
+            if (r === nums.length) break;
+        }
+        if (l < r) [nums[l], nums[r]] = [nums[r], nums[l]];
+    }
+    
+    return nums;
+};
