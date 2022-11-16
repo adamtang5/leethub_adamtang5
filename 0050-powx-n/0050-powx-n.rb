5e7f907b720fe9aee328a6a4d6ac27f959d95e1c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-16 01:25:30 -0800

    Time: 156 ms (36.36%), Space: 211.4 MB (14.29%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.rb
@@ -0,0 +1,17 @@
+# @param {Float} x
+# @param {Integer} n
+# @return {Float}
+def my_pow(x, n)
+    return 1 if n == 0
+    if n > 0
+        while n > 0
+            return x * my_pow(x, n-1) if n % 2 > 0
+            return my_pow(x*x, n/2) if n % 2 == 0
+        end
+    else
+        while n < 0
+            return my_pow(x, n+1) / x if n % 2 > 0
+            return my_pow(x*x, n/2) if n % 2 == 0
+        end
+    end
+end
