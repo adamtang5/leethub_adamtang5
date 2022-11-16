82ec74472a6ffec3472d7d8e54a9b0e61d95324d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-15 21:47:39 -0800

    Time: 105 ms (25.56%), Space: 42.7 MB (7.22%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (!p && !q) return true;
+    if (!p && q || p && !q) return false;
+    if (p.left ^ q.left || p.right ^ q.right) return false;
+    if (!p.left && !p.right && !q.left && !q.right && p.val === q.val) return true;
+    
+    if (p.val !== q.val) {
+        return false;
+    } else {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+};
