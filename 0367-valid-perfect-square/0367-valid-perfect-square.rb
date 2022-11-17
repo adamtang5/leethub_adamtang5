1e8995f7c8162b585e71ff928742f083b5609872
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-17 09:03:54 -0800

    Time: 178 ms (6.25%), Space: 211.1 MB (68.75%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.rb
@@ -0,0 +1,16 @@
+# @param {Integer} num
+# @return {Boolean}
+def is_perfect_square(num)
+    l, r = 1, num
+    pivot = (l+r) / 2
+    while l <= r
+        return true if pivot*pivot == num
+        if pivot*pivot < num
+            l = pivot+1
+        else
+            r = pivot-1
+        end
+        pivot = (l+r) / 2
+    end
+    return false
+end
