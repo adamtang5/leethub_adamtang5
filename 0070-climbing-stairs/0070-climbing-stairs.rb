6db5af9b0850a733564a228ba6828b8864f7bafc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-19 20:52:57 -0800

    Time: 67 ms (99.17%), Space: 211.1 MB (31.30%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.rb
+++ 0070-climbing-stairs/0070-climbing-stairs.rb
@@ -1,9 +1,8 @@
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
-    dp = Hash.new
-    dp[1] = 1
-    dp[2] = 2
-    (3..n).each { |x| dp[x] = dp[x-1]+dp[x-2] }
-    return dp[n]
+    return n if n <= 2
+    mem = [1, 2]
+    (n-2).times { mem << mem[-1]+mem[-2] }
+    return mem[-1]
 end
