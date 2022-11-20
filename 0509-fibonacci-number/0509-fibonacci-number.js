f8ac5daf3c96fd31fd69adb9f475f3c34cc33f42
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-19 20:36:11 -0800

    Time: 66 ms (92.99%), Space: 41.7 MB (77.85%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -6,8 +6,7 @@ var fib = function(n) {
     if (n < 2) return n;
     const mem = [0, 1];
     while (n > 2) {
-        mem.push(mem[0] + mem[1]);
-        mem.shift();
+        mem.push(mem[1] + mem.shift());
         n--;
     }
     return mem[0] + mem[1];
