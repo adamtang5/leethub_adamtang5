98a157bb0daa742fc44b7615ab7ece0526ae2cf9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-11-19 20:23:54 -0800

    Time: 139 ms (73.71%), Space: 211 MB (29.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.rb
+++ 0509-fibonacci-number/0509-fibonacci-number.rb
@@ -4,8 +4,7 @@ def fib(n)
     return n if n < 2
     mem = [0, 1]
     while n > 2
-        mem << mem[0]+mem[1]
-        mem.shift
+        mem << mem[-1]+mem.shift
         n -= 1
     end
     return mem[0]+mem[1]
