5ea16a55fc8443e600ada54da21c799dfb87039f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-10 22:56:34 -0800

    Time: 408 ms (5.62%), Space: 52.9 MB (5.11%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(num, k) {
+    const len = num.length;
+    if (num[len - 1] + k < 10) {
+        num[len - 1] += k;
+        return num;
+    } else {
+        num.reverse();
+        k.toString().split('').reverse().forEach((s, i) => {
+            num[i] = num[i] || 0;
+            num[i] += (+s)
+        });
+        console.log(num);
+        let [i, carry] = [0, 0];
+        while (i < num.length) {
+            num[i] += carry;
+            if (num[i] > 9) {
+                carry = Math.floor(num[i] / 10);
+                num[i] %= 10;
+                console.log(carry, num[i]);
+            } else {
+                carry = 0;
+            }
+            i++;
+        }
+        if (carry) num.push(carry);
+        return num.reverse();
+    }
+};
