13892fe89169fb3b86350831d6ef074c087062e4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-20 00:31:24 -0800

    Time: 122 ms (79.17%), Space: 213.9 MB (81.25%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
@@ -2,25 +2,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_sub_array_len(target, nums)
-    min_len, curr_sum, l, r = 1 / 0.0, 0, 0, 0
-    while r < nums.length
-        if min_len < 1 / 0.0
+    min_len, curr_sum, l = 1 / 0.0, 0, 0
+    (0...nums.length).each do |r|
+        curr_sum += nums[r]
+        while curr_sum >= target
+            min_len = [min_len, r-l+1].min
             curr_sum -= nums[l]
             l += 1
-        end
-        while r < nums.length && curr_sum < target
-            curr_sum += nums[r]
-            r += 1
-        end
-        if r < nums.length || curr_sum >= target
-            min_len = [min_len, r-l].min
-        end
-    end
-    while l < r && curr_sum >= target
-        curr_sum -= nums[l]
-        l += 1
-        if curr_sum >= target
-            min_len = [min_len, r-l].min
         end
     end
     return min_len == 1 / 0.0 ? 0 : min_len
