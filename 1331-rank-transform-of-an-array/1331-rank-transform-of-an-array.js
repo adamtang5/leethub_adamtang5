604b545bcaa7712de6f7dd52a680585f0067a0f6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-20 23:34:11 -0800

    Time: 8029 ms (5.45%), Space: 64.8 MB (70.79%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
-    const copy = Array.from(new Set(arr.slice()));
+    const copy = [...new Set(arr.slice())];
     copy.sort((a, b) => a - b);
     return arr.map(n => copy.indexOf(n) + 1);
 };
