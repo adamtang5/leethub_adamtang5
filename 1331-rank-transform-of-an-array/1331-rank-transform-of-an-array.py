0cede5998ae92424c5bd645b7e1b62f7d6f02e54
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-21 00:55:30 -0800

    Time: 339 ms (98.50%), Space: 33.5 MB (45.13%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,7 +1,4 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        copy = sorted(set(arr))
-        iMap = dict();
-        for i, n in enumerate(copy):
-            iMap[str(n)] = iMap.get(str(n), i+1)
-        return [iMap.get(str(n)) for n in arr]
+        ranks = {n:r for r, n in enumerate(sorted(set(arr)), start=1)}
+        return [ranks[n] for n in arr]
