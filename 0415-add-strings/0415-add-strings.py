c2c42089d787556ad21fcb80aa32a2d79b3d73dd
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-22 01:50:52 -0800

    Time: 53 ms (73.75%), Space: 14.4 MB (5.54%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        rev1 = [int(d) for d in num1[::-1]]
+        rev2 = [int(d) for d in num2[::-1]]
+
+        if len(rev1) < len(rev2):
+            rev1, rev2 = rev2, rev1
+        
+        digits = [rev1[i] + (rev2[i] if i < len(rev2) else 0) for i in range(len(rev1))]
+        i, carry = 0, 0
+        while i < len(digits):
+            digits[i] += carry
+            carry = digits[i] // 10
+            digits[i] %= 10
+            i += 1
+        if carry:
+            digits.append(carry)
+        return ''.join([str(d) for d in digits[::-1]])
