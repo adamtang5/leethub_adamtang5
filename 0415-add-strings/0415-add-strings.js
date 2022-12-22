0eb2c0028150e2224bfb5208325488d9fa9350c6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-22 01:34:39 -0800

    Time: 90 ms (74.57%), Space: 45.4 MB (11.26%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -5,16 +5,16 @@
  */
 var addStrings = function(num1, num2) {
     const digits = [];
-    let [rev1, rev2] = ['', ''];
-    for (let i = num1.length - 1; i >= 0; i--) {
-        rev1 += num1[i];
+    let [rev1, rev2] = [[], []];
+    for (let i = 0; i < num1.length; i++) {
+        rev1.unshift(+num1[i]);
     }
-    for (let i = num2.length - 1; i >= 0; i--) {
-        rev2 += num2[i];
+    for (let i = 0; i < num2.length; i++) {
+        rev2.unshift(+num2[i]);
     }
     
     for (let i = 0; i < Math.max(rev1.length, rev2.length); i++) {
-        digits.push((rev1[i] ? +rev1[i] : 0) + (rev2[i] ? +rev2[i] : 0));
+        digits.push((rev1[i] || 0) + (rev2[i] || 0));
     }
     
     let [i, carry, ans] = [0, 0, ''];
