fa05e3d7114cc8568e09f7daa5919558a41c46ac
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2022-12-23 23:05:09 -0800

    Time: 10805 ms (12.75%), Space: 14.9 MB (97.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        steps = [float("inf")] * len(nums)
+        steps = [len(nums)] * len(nums)
         steps[0] = 0
         for i in range(len(nums)-1):
-            j = 1
-            while i+j < len(steps) and j <= nums[i]:
-                steps[i+j] = min(steps[i+j], steps[i]+1)
-                j += 1
+            for j in range(1, nums[i]+1):
+                if i+j < len(steps):
+                    steps[i+j] = min(steps[i+j], steps[i]+1)
         return steps[-1]
