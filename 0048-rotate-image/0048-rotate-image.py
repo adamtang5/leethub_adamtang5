c99c8064e4caef9a2f22659bebfc1a2ed7b5e3f4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-01-03 21:40:16 -0800

    Time: 64 ms (45.75%), Space: 13.9 MB (73.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,17 +6,8 @@ class Solution:
         def reassign(matrix, width, corner):
             lb, ub = corner, corner+width-1
             for i in range(width-1):
-                [
-                    matrix[lb][lb+i],
-                    matrix[lb+i][ub],
-                    matrix[ub][ub-i],
-                    matrix[ub-i][lb]
-                ] = [
-                    matrix[ub-i][lb],
-                    matrix[lb][lb+i],
-                    matrix[lb+i][ub],
-                    matrix[ub][ub-i]
-                ]
+                matrix[lb][lb+i], matrix[lb+i][ub], matrix[ub][ub-i], matrix[ub-i][lb] = \
+                matrix[ub-i][lb], matrix[lb][lb+i], matrix[lb+i][ub], matrix[ub][ub-i]
         
         for w in range(len(matrix), 1, -2):
             reassign(matrix, w, (len(matrix)-w)//2)
