458febc7dac58270318d0807a4e65e568615033a
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-01-20 23:49:22 -0800

    Time: 487 ms (13.04%), Space: 41.8 MB (82.61%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} tx
+ * @param {number} ty
+ * @return {boolean}
+ */
+var reachingPoints = function(sx, sy, tx, ty) {
+    while (tx >= sx && ty >= sy) {
+        if (tx === ty) {
+            break;
+        } else if (tx > ty) {
+            if (ty === sy) {
+                return (tx - sx) % sy === 0;
+            } else {
+                tx -= ty;
+            }            
+        } else if (ty > tx) {
+            if (tx === sx) {
+                return (ty - sy) % sx === 0;
+            } else {
+                ty -= tx;
+            }
+        }        
+    }
+    
+    return (sx === tx && sy === ty);
+};
