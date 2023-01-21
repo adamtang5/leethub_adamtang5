e45424000068733e43c05b5cdce9eb5047930115
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-01-21 00:08:26 -0800

    Time: 36 ms (58.06%), Space: 13.9 MB (79.68%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.py
+++ 0780-reaching-points/0780-reaching-points.py
@@ -1,9 +1,7 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
         while tx >= sx and ty >= sy:
-            if tx == ty:
-                break
-            elif tx > ty:
+            if tx > ty:
                 if ty == sy:
                     return (tx-sx) % sy == 0
                 else:
@@ -13,5 +11,7 @@ class Solution:
                     return (ty-sy) % sx == 0
                 else:
                     ty %= tx
+            else:
+                break
         
         return sx == tx and sy == ty
