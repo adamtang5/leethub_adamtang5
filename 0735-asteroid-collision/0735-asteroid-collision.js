df7fb48768a17aa7c8474c39ffe3506b10302681
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-01-24 16:18:45 -0800

    Time: 141 ms (9.77%), Space: 46.2 MB (8.14%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    const stack = [];
+    
+    for (let i = 0; i < asteroids.length; i++) {
+        if (!stack.length || stack.at(-1) < 0) {
+            stack.push(asteroids[i]);
+        } else {
+            while (true) {
+                const [top, curr] = [stack.at(-1), asteroids[i]];
+                if (top * curr > 0) {
+                    stack.push(curr);
+                    break;
+                } else if (top + curr > 0) {
+                    break;
+                } else if (top + curr === 0) {
+                    stack.pop();
+                    break;
+                } else if (top + curr < 0) {
+                    stack.pop();
+                    if (!stack.length) {
+                        stack.push(curr);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return stack;
+};
