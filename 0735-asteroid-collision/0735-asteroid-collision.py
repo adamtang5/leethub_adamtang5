1a9b6757749387053bdbf2f6ab47cd24cf12b326
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-01-24 16:58:34 -0800

    Time: 99 ms (83.56%), Space: 15.2 MB (21.50%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(asteroids)):
+            if stack == [] or stack[-1] < 0:
+                stack.append(asteroids[i])
+            else:
+                while True:
+                    if stack[-1] * asteroids[i] > 0:
+                        stack.append(asteroids[i])
+                        break
+                    elif stack[-1] + asteroids[i] > 0:
+                        break
+                    elif stack[-1] + asteroids[i] == 0:
+                        stack.pop(-1)
+                        break
+                    elif stack[-1] + asteroids[i] < 0:
+                        stack.pop(-1)
+                        if stack == []:
+                            stack.append(asteroids[i])
+                            break
+        return stack
+                    
