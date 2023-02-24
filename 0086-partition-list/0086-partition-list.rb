1f87eee5bf22a46fd6be7dae38d830da3933c8be
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-02-23 16:47:11 -0800

    Time: 87 ms (33.33%), Space: 211 MB (66.67%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.rb
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode
+#   attr_accessor :val, :next
+#   def initialize(val = 0, _next = nil)
+#     @val = val
+#     @next = _next
+#   end
+# end
+# @param {ListNode} head
+# @param {Integer} x
+# @return {ListNode}
+def partition(head, x)
+  return head if !head
+  left, right = ListNode.new, ListNode.new
+  curr, left_tail, right_tail = head, left, right
+  next_node = nil
+  while curr
+    if curr.val < x
+      left_tail.next = curr
+      left_tail = left_tail.next
+    else
+      right_tail.next = curr
+      right_tail = right_tail.next
+    end
+    next_node = curr.next
+    curr.next = nil
+    curr = next_node
+  end
+  left_tail.next = right.next
+  return left.next
+end
