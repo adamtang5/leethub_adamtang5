4dcca0b29abc093880780b212e424b6357129b2b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-02-24 22:53:01 -0800

    Time: 108 ms (58.49%), Space: 51 MB (98.11%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+  const bitSeq = n => {
+    if (n === 1) return new Array(2).fill(0);
+    const lastSeq = bitSeq(n - 1);
+    lastSeq[lastSeq.length - 1] = n - 1;
+    return [...lastSeq, ...lastSeq];
+  };
+  const seq = bitSeq(n);
+  
+  const ans = new Array(2 ** n).fill(0);
+  
+  for (let i = 0; i < ans.length - 1; i++) {
+    ans[i + 1] = ans[i] ^ (1 << (n - 1 - seq[i]));
+  }
+  return ans;
+};
