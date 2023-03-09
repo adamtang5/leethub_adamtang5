88dcbb50fe430c35c138e334cdae74eb4662d959
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-08 23:03:01 -0800

    Time: 61 ms (92.22%), Space: 42.5 MB (84.65%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(s) {
+  const ans = [];
+  if (s.length < 4 || s.length > 12) return ans;
+  if (s.length === 4) {
+    ans.push(s.split("").join("."));
+    return ans;
+  }
+  
+  const validOctet = s => {
+    if (s.length > 3 || s.length < 1) return false;
+    if (s.length > 1 && s[0] === '0') return false;
+    if (+s > 255) return false;
+    return true;
+  };
+  
+  const dfs = (s, octIdx = 3, addr = "") => {
+    // console.log([s, octIdx, addr]);
+    if (!s.length) return;
+    if (s.length > (octIdx + 1) * 3) return;
+    if (!octIdx && !validOctet(s)) return;
+    if (validOctet(s) && !octIdx) {
+      ans.push(s + addr);
+      // console.log(`ans: ${ans}`);
+      return;
+    }
+    for (let digits = 1; digits <= Math.min(s.length, 3); digits++) {
+      const oct = s.slice(s.length - digits);
+      if (validOctet(oct)) {
+        dfs(s.slice(0, s.length - digits), octIdx - 1, "." + oct + addr);
+      }
+    }
+  };
+  
+  dfs(s);
+  return ans;
+};
