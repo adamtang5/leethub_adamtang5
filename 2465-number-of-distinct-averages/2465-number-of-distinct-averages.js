4ed4a68ea38848b5939fc4c6f4a34b61d64183ea
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-11 22:54:36 -0800

    Time: 70 ms (32.11%), Space: 41.8 MB (69.47%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.js
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.js
@@ -5,11 +5,11 @@
 var distinctAverages = function(nums) {
   nums.sort((a, b) => a - b);
   const avgs = new Set();
-  let min, max;
-  while (nums.length) {
-    min = nums.shift();
-    max = nums.pop();
-    avgs.add((min + max) / 2);
+  let [l, r] = [0, nums.length - 1];
+  while (l < r) {
+    avgs.add((nums[l] + nums[r]) / 2);
+    l++;
+    r--;
   }
   return avgs.size;
 };
