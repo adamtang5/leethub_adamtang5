241ff0fa83f758d55b3fc373d06bf2c932457278
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-11 22:36:55 -0800

    Delete 1-two-sum directory

--- 1-two-sum/1-two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        data = {}
-        for i in range(len(nums)):
-            if nums[i] in data:
-                return [data[nums[i]], i]
-            else:
-                data[target - nums[i]] = i
