3e6b374a61bd2cf6d8fc3524a8659fc933617e20
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-11 23:30:39 -0800

    Time: 75 ms (36.88%), Space: 44.9 MB (11.93%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -6,13 +6,13 @@
 var countKDifference = function(nums, k) {
   const indices = {};
   let count = 0;
-  nums.forEach((num, i) => {
+  for (const num of nums) {
     indices[num] = indices[num] || 0;
     count += indices[num];
     indices[num + k] = indices[num + k] || 0;
     indices[num + k]++;
     indices[num - k] = indices[num - k] || 0;
     indices[num - k]++;
-  });
+  }
   return count;
 };
