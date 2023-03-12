3b947f7223eba891f0f51082a7066b421fb807cd
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-11 22:35:30 -0800

    Time: 63 ms (91.13%), Space: 43.1 MB (26.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,7 +5,6 @@
 */
 var twoSum = function(nums, target) {
   const indices = {};
-
   for (let i = 0; i < nums.length; i++) {
     if (indices[nums[i]] !== undefined) return [indices[nums[i]], i];
     indices[target - nums[i]] = i;
