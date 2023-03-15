de4cb689f54903f980260c854e94faa8efc98638
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-15 16:44:04 -0700

    Time: 79 ms (74.44%), Space: 211 MB (36.84%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.rb
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def inorder_traversal(root)
+  return [] if root == nil
+  return inorder_traversal(root.left) + [root.val] + inorder_traversal(root.right)
+end
