6c36cf624f13ae0b05cacf2aa4fce4962016d71b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-20 13:41:35 -0700

    Time: 102 ms (73.36%), Space: 46.9 MB (81.02%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -14,22 +14,12 @@ var addTwoNumbers = function(l1, l2) {
   const prev = new ListNode();
   let [curr1, curr2, curr, carry] = [l1, l2, prev, 0];
   while (curr1 || curr2) {
-    if (curr1 && curr2) {
-      curr.next = new ListNode(curr1.val + curr2.val + carry);
-    } else if (curr1 && !curr2) {
-      curr.next = new ListNode(curr1.val + carry);
-    } else if (!curr1 && curr2) {
-      curr.next = new ListNode(curr2.val + carry);
-    }
+    curr.next = new ListNode((curr1?.val || 0) + (curr2?.val || 0) + carry);
     if (curr1) curr1 = curr1.next;
     if (curr2) curr2 = curr2.next;
     curr = curr.next;
-    if (curr.val > 9) {
-      carry = 1;
-      curr.val %= 10;
-    } else {
-      carry = 0;
-    }
+    carry = Math.floor(curr.val / 10);
+    curr.val %= 10;
   }
   if (carry) curr.next = new ListNode(carry);
   return prev.next;
