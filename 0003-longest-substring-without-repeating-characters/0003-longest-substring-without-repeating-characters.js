d24a6eef5d30b849cbd4f2cf01426e37923c2efb
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-03-21 14:05:47 -0700

    Time: 76 ms (93.31%), Space: 47.8 MB (52.11%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if (!s.length) return 0;
+  let [sub, max] = ['', 0];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (!sub.includes(s[i])) {
+      sub += s[i];
+    } else {
+      sub = sub.slice(sub.indexOf(s[i]) + 1) + s[i];
+    }
+    if (sub.length > max) max = sub.length;
+  }
+  return max;
+};
