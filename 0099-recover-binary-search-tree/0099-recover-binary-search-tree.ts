56094aed479bc2cc94600d3063187b444ba81a4c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-04-01 22:18:17 -0700

    Time: 142 ms (80.65%), Space: 53.2 MB (54.84%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   val: number
+ *   left: TreeNode | null
+ *   right: TreeNode | null
+ *   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ *   }
+ * }
+ */
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function recoverTree(root: TreeNode | null): void {
+  const nodes: TreeNode[] = []
+  
+  function dfs(node: TreeNode | null): void {
+    if (!node) return
+    dfs(node.left)
+    nodes.push(node)
+    dfs(node.right)
+  }
+  dfs(root)
+  const vals: number[] = nodes.map(node => node.val)
+  vals.sort((a, b) => a - b)
+  nodes.forEach((node, i) => node.val = vals[i])
+}
