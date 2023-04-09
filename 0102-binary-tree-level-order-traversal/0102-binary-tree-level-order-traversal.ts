c2c19c6b95583cfe40b1bce8f8536c36a424a4f9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-04-08 22:55:28 -0700

    Time: 82 ms (15.49%), Space: 44 MB (99.57%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   val: number
+ *   left: TreeNode | null
+ *   right: TreeNode | null
+ *   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ *   }
+ * }
+ */
+function levelOrder(root: TreeNode | null): number[][] {
+  type AuxNode = [number, TreeNode]
+  const queue: AuxNode[] = root ? [[0, root]] : []
+  const ans: number[][] = []
+  while (queue.length) {
+    const [level, node] = queue.shift()
+    if (level > ans.length - 1) ans.push([])
+    ans[level].push(node.val)
+    if (node.left) queue.push([level + 1, node.left])
+    if (node.right) queue.push([level + 1, node.right])
+  }
+  return ans
+}
