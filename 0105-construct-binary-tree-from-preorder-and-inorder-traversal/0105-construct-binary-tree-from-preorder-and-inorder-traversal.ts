bad5405fc405efb45c2204d604d1fca6932890f1
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-04-17 12:07:55 -0700

    Time: 96 ms (69.63%), Space: 48.1 MB (68.69%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -1,52 +1,43 @@
 /**
  * Definition for a binary tree node.
  * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
+ *   val: number
+ *   left: TreeNode | null
+ *   right: TreeNode | null
+ *   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ *   }
  * }
  */
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-  interface AuxNode {
-    rootVal: number | null
-    leftInorder: number[] | null
-    rightInorder: number[] | null
-    leftPreorder: number[] | null
-    rightPreorder: number[] | null
-  }
-
-  function decompose(preorder: number[], inorder: number[]): AuxNode {
-    const ans: AuxNode = {
-      rootVal: null,
-      leftInorder: null,
-      rightInorder: null,
-      leftPreorder: null,
-      rightPreorder: null,
-    };
-    
-    if (preorder.length) {
-      ans.rootVal = preorder[0]
-      ans.leftInorder = inorder.slice(0, inorder.indexOf(ans.rootVal))
-      ans.rightInorder = inorder.slice(inorder.indexOf(ans.rootVal) + 1)
-      ans.leftPreorder = preorder.filter(el => ans.leftInorder.includes(el))
-      ans.rightPreorder = preorder.filter(el => ans.rightInorder.includes(el))
+  const inIdx = {}
+  inorder.forEach((el, i) => inIdx[el] = i)
+  
+  function helper(preLb: number, preUb: number): TreeNode | null {
+    if (preLb === preUb) return null
+    const rootInIdx: number = inIdx[preorder[preLb]]
+    if (preUb - preLb === 1) return new TreeNode(inorder[rootInIdx])
+    let [l, r] = [preLb + 1, preUb - 1]
+    if (inIdx[preorder[l]] > rootInIdx) {
+      return new TreeNode(preorder[preLb], null, helper(preLb + 1, preUb))
+    } else if (inIdx[preorder[r]] < rootInIdx) {
+      return new TreeNode(preorder[preLb], helper(preLb + 1, preUb), null)
     }
-    return ans
+    let mid: number
+    while (l <= r) {
+      mid = Math.floor((l + r) / 2)
+      if (inIdx[preorder[mid]] < rootInIdx && inIdx[preorder[mid + 1]] > rootInIdx) {
+        break
+      } else if (inIdx[preorder[mid]] < rootInIdx) {
+        l = mid + 1
+      } else {
+        r = mid - 1
+      }
+    }
+    return new TreeNode(preorder[preLb], helper(preLb + 1, mid + 1), helper(mid + 1, preUb))
   }
-
-  const parsed: AuxNode = decompose(preorder, inorder)
-  if (parsed.rootVal === null) {
-    return null
-  } else {
-    return new TreeNode(
-      parsed.rootVal,
-      buildTree(parsed.leftPreorder, parsed.leftInorder),
-      buildTree(parsed.rightPreorder, parsed.rightInorder),
-    )
-  }
+  
+  return helper(0, preorder.length)
 }
