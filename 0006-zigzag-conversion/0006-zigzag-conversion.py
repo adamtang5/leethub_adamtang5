88c71fedb60307053a2c4e85682c0553baa7314c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-04-30 23:06:38 -0700

    Time: 63 ms (51.47%), Space: 16.3 MB (13.95%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,12 @@
+class Solution:
+  def convert(self, s: str, numRows: int) -> str:
+    if numRows == 1: return s
+    rows = [''] * numRows
+    newBase = (numRows-1)*2
+    
+    for i, ch in enumerate(s):
+      iMod = i % newBase
+      newIdxMod = iMod if iMod <= newBase/2 else newBase-iMod
+      rows[newIdxMod] += ch
+    
+    return ''.join(rows)
