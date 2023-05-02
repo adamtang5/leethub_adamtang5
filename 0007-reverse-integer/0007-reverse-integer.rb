f13f9de54ad1bb446315fd2f480eb0cc00f186aa
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-02 14:41:42 -0700

    Time: 92 ms (32.09%), Space: 211 MB (79.68%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.rb
@@ -0,0 +1,13 @@
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+  sign = x >= 0 ? 1 : -1
+  x = x.abs
+  s = ''
+  while x > 0
+    s += (x % 10).to_s
+    x /= 10
+  end
+  ans = sign*s.to_i
+  return ans < -1*(2**31) || ans >= 2**31 ? 0 : ans
+end
