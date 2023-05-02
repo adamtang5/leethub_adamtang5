9b1d8ff1e0a4a8584c2020d8e1b0967b5ed299ab
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-02 14:36:14 -0700

    Time: 71 ms (61.36%), Space: 44.5 MB (15.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  const sign = x >= 0 ? 1 : -1;
+  const ans = sign * parseInt(Math.abs(x).toString(10).split('').reverse().join(''), 10);
+  return ans < -1 * (2 ** 31) || ans >= 2 ** 31 ? 0 : ans;
+};
