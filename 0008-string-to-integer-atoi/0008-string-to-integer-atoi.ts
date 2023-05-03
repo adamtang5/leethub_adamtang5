7b883f499a309c81f0bb797c8d2185f2a705557d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-03 09:17:32 -0700

    Time: 83 ms (44.10%), Space: 44.9 MB (79.92%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.ts
@@ -0,0 +1,12 @@
+function myAtoi(s: string): number {
+  s = s.trim()
+  let sign = 1
+  if (s[0] === '-') sign = -1
+  if ('+-'.includes(s[0])) s = s.slice(1)
+  
+  const digitsRe = /^\d+/
+  if (!s.match(digitsRe)) return 0
+  
+  const ans = sign * +s.match(digitsRe)[0]
+  return ans < 0 ? Math.max(ans, -1 * 2 **31) : Math.min(ans, 2 ** 31 - 1)
+}
