1c8633d64aa0ac9f5c4b1a1e8a2f4d8794009e31
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-03 21:02:24 -0700

    Time: 13 ms (5.35%), Space: 43.4 MB (5.18%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+  public int myAtoi(String s) {
+    s = s.trim();
+    if (s.length() == 0) return 0;
+    
+    int sign = 1;
+    if (s.charAt(0) == '-') sign = -1;
+    if (s.charAt(0) == '-' || s.charAt(0) == '+') s = s.substring(1);
+    
+    Pattern digits = Pattern.compile("^\\d+");
+    Matcher matcher = digits.matcher(s);
+    
+    List<String> list = new ArrayList<String>();
+    while (matcher.find()) {
+      list.add(matcher.group());
+      System.out.println(list.get(0));
+      break;
+    }
+
+    if (list.size() == 0) return 0;
+    try {
+      return sign * Integer.parseInt(list.get(0).toString());
+    } catch (NumberFormatException e) {
+      return sign < 0 ? -2147483648 : 2147483647;
+    }
+  }
+}
