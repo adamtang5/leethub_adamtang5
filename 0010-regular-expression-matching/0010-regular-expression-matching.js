07fedfcc447def7a61dc2547448eed79ffe43a6f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-06 12:13:28 -0700

    Time: 114 ms (53.85%), Space: 53.1 MB (5.27%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -1,55 +1,52 @@
 /**
- * @param {string} s
- * @param {string} p
- * @return {boolean}
- */
+* @param {string} s
+* @param {string} p
+* @return {boolean}
+*/
 var isMatch = function (s, p, dp={}) {
-    const parsed = [];
-    let i = 0;
-    while (i < p.length) {
-        if (p[i + 1] === '*') {
-            parsed.push(p[i] + p[i + 1]);
-            i += 2;
-        } else {
-            parsed.push(p[i]);
-            i++;
-        }
-    }
-
-    const charMatch = (ch1, ch2) => {
-        return ch2 === '.' || ch2 === ch1;
-    };
-
-    // base case
-    if (s.length && !parsed.length) return false;
-    if (!s.length && !parsed.length) return true;
-    if (!s.length && parsed.length) return parsed.every(el => el.length === 2);
-
-    // recursive step
-    const first = parsed.shift();
-    if (first.length === 1) {
-        return charMatch(s[0], first) && isMatch(s.slice(1), parsed, dp);
+  const parsed = [];
+  let i = 0;
+  while (i < p.length) {
+    if (p[i + 1] === '*') {
+      parsed.push(p[i] + p[i + 1]);
+      i += 2;
     } else {
-        if (!charMatch(s[0], first[0])) {
-            return isMatch(s, parsed, dp);
-        } else {
-            const key = JSON.stringify([s, [first, ...parsed]]);
-            if (dp[key] !== undefined) {
-                return dp[key];
-            } else {
-                let len = 1;
-                while (first[0] === '.' && len < s.length || s[len] === s[0]) {
-                    len++;
-                }
-
-                let bool = false;
-                for (let i = 0; i <= len; i++) {
-                    bool ||= isMatch(s.slice(i), parsed, dp);
-                }
-                dp[key] = bool;
-                // console.log(dp);
-                return bool;
-            }
-        }
+      parsed.push(p[i]);
+      i++;
     }
+  }
+
+  const charMatch = (ch1, ch2) => {
+    return ch2 === '.' || ch2 === ch1;
+  };
+
+  if (s.length && !parsed.length) return false;
+  if (!s.length && !parsed.length) return true;
+  if (!s.length && parsed.length) return parsed.every(el => el.length === 2);
+
+  const first = parsed.shift();
+  if (first.length === 1) {
+    return charMatch(s[0], first) && isMatch(s.slice(1), parsed, dp);
+  } else {
+    if (!charMatch(s[0], first[0])) {
+      return isMatch(s, parsed, dp);
+    } else {
+      const key = JSON.stringify([s, [first, ...parsed]]);
+      if (dp[key] !== undefined) {
+        return dp[key];
+      } else {
+        let len = 1;
+        while (first[0] === '.' && len < s.length || s[len] === s[0]) {
+          len++;
+        }
+
+        let ans = false;
+        for (let i = 0; i <= len; i++) {
+          ans ||= isMatch(s.slice(i), parsed, dp);
+        }
+        dp[key] = ans;
+        return ans;
+      }
+    }
+  }
 };
