d92374ed29663cba35acd840bae6fe967f95d6c6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-06 23:41:54 -0700

    Time: 83 ms (17.09%), Space: 16.6 MB (5.89%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,45 +1,52 @@
 import json
 
 class Solution:
-    def isMatch(self, s: str, p: str, dp={}) -> bool:
-        parsed = []
-        i = 0
-        while i < len(p):
-            if i+1 < len(p) and p[i+1] == '*':
-                parsed.append(p[i]+p[i+1])
-                i += 2
-            else:
-                parsed.append(p[i])
-                i += 1
+  def isMatch(self, s: str, p: str) -> bool:
+    parsed = []
+    i = 0
+    while i < len(p):
+      if i+1 < len(p) and p[i+1] == '*':
+        parsed.append(p[i]+p[i+1])
+        i += 2
+      else:
+        parsed.append(p[i])
+        i += 1
 
-        def charMatch(ch1, ch2):
-            return ch2 == '.' or ch2 == ch1
-        
-        # base cases
-        if len(s) > 0 and len(parsed) == 0:
-            return False
-        if len(s) == 0 and len(parsed) == 0:
-            return True
-        if len(s) == 0 and len(parsed) > 0:
-            return all([len(el) == 2 for el in parsed])
-        
-        # recursive steps
-        first = parsed.pop(0)
-        if len(first) == 1:
-            return charMatch(s[0], first) and self.isMatch(s[1:], parsed, dp)
+    def charMatch(ch1, ch2):
+      return ch2 == '.' or ch2 == ch1
+
+    def dfs(s, parsed, dp):
+      key = json.dumps([s, parsed])
+      if len(s) > 0 and len(parsed) == 0:
+        dp[key] = False
+        return dp[key]
+      if len(s) == 0 and len(parsed) == 0:
+        dp[key] = True
+        return dp[key]
+      if len(s) == 0 and len(parsed) > 0:
+        dp[key] = all([len(el) == 2 for el in parsed])
+        return dp[key]
+
+      first = parsed.pop(0)
+      if len(first) == 1:
+        dp[key] = charMatch(s[0], first) and dfs(s[1:], parsed, dp)
+        return dp[key]
+      else:
+        if not charMatch(s[0], first[0]):
+          dp[key] = dfs(s, parsed, dp)
+          return dp[key]
         else:
-            if not charMatch(s[0], first[0]):
-                return self.isMatch(s, parsed, dp)
-            else:
-                key = json.dumps([s, [first]+parsed])
-                if key in dp:
-                    return dp[key]
-                else:
-                    length = 1
-                    while length < len(s) and (first[0] == '.' or s[length] == s[0]):
-                        length += 1
-                    boo = False
-                    for i in range(length+1):
-                        boo = boo or self.isMatch(s[i:], parsed, dp)
-                    dp[key] = boo
-                    return boo
+          if key in dp:
+            return dp[key]
+          else:
+            length = 1
+            while length < len(s) and (first[0] == '.' or s[length] == s[0]):
+              length += 1
+            ans = False
+            for i in range(length+1):
+              copy = parsed.copy()
+              ans = ans or dfs(s[i:], copy, dp)
+            dp[key] = ans
+            return ans
+    
+    return dfs(s, parsed, {})
