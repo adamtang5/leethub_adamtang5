eecf05b527f10d3111e69e1e089327f3724d5a13
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-06 23:51:38 -0700

    Time: 181 ms (41.54%), Space: 211 MB (49.23%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.rb
+++ 0010-regular-expression-matching/0010-regular-expression-matching.rb
@@ -2,52 +2,65 @@
 # @param {String} p
 # @return {Boolean}
 def char_match(ch1, ch2)
-    return ch2 == '.' || ch2 == ch1
+  return ch2 == '.' || ch2 == ch1
 end
 
-def is_match(s, p, dp={})
-    parsed = []
-    i = 0
-    while i < p.length
-        if i+1 < p.length && p[i+1] == '*'
-            parsed << p[i]+p[i+1]
-            i += 2
-        else
-            parsed << p[i]
-            i += 1
-        end
-    end
-    
-    # base cases
-    return false if s.length > 0 && parsed.length == 0
-    return true if s.length == 0 && parsed.length == 0
-    if s.length == 0 && parsed.length > 0
-        return parsed.all?{ |el| el.length == 2 }
-    end
-    
-    # recursive steps
-    first = parsed.shift
-    if first.length == 1
-        return char_match(s[0], first) && is_match(s[1..-1], parsed, dp)
+def dfs(s, parsed, dp)
+  key = [s, parsed]
+  
+  if s.length > 0 && parsed.length == 0
+    dp[key] = false
+    return dp[key]
+  end
+  if s.length == 0 && parsed.length == 0
+    dp[key] = true
+    return dp[key]
+  end
+  if s.length == 0 && parsed.length > 0
+    dp[key] = parsed.all?{ |el| el.length == 2 }
+    return dp[key]
+  end
+
+  first = parsed.shift
+  if first.length == 1
+    dp[key] = char_match(s[0], first) && dfs(s[1..-1], parsed, dp)
+    return dp[key]
+  else
+    if !char_match(s[0], first[0])
+      dp[key] = dfs(s, parsed, dp)
+      return dp[key]
     else
-        if !char_match(s[0], first[0])
-            return is_match(s, parsed, dp)
-        else
-            key = [s, [first]+parsed]
-            if dp[key] != nil
-                return dp[key]
-            else
-                len = 1
-                while len < s.length && (first[0] == '.' || s[len] == s[0])
-                    len += 1
-                end
-                bool = false
-                (0..len).each do |i|
-                    bool ||= is_match(s[i..-1], parsed, dp)
-                end
-                dp[key] = bool
-                return bool
-            end
+      if dp.has_key?(key)
+        return dp[key]
+      else
+        len = 1
+        while len < s.length && (first[0] == '.' || s[len] == s[0])
+          len += 1
         end
+        ans = false
+        (0..len).each do |i|
+          copy = parsed.dup
+          ans ||= dfs(s[i..-1], copy, dp)
+        end
+        dp[key] = ans
+        return ans
+      end
     end
+  end  
+end
+
+def is_match(s, p)
+  parsed = []
+  i = 0
+  while i < p.length
+    if i+1 < p.length && p[i+1] == '*'
+      parsed << p[i]+p[i+1]
+      i += 2
+    else
+      parsed << p[i]
+      i += 1
+    end
+  end
+  
+  return dfs(s, parsed, {})
 end
