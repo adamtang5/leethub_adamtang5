55fcdd525b1563f24e4f64fee7f7d5929f00b258
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-06 22:58:04 -0700

    Time: 8552 ms (5.13%), Space: 70.2 MB (5.63%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -3,7 +3,7 @@
 * @param {string} p
 * @return {boolean}
 */
-var isMatch = function (s, p, dp={}) {
+var isMatch = function (s, p) {
   const parsed = [];
   let i = 0;
   while (i < p.length) {
@@ -20,33 +20,37 @@ var isMatch = function (s, p, dp={}) {
     return ch2 === '.' || ch2 === ch1;
   };
 
-  if (s.length && !parsed.length) return false;
-  if (!s.length && !parsed.length) return true;
-  if (!s.length && parsed.length) return parsed.every(el => el.length === 2);
+  const dfs = (s, parsed, dp) => {
+    if (s.length && !parsed.length) return false;
+    if (!s.length && !parsed.length) return true;
+    if (!s.length && parsed.length) return parsed.every(el => el.length === 2);
 
-  const first = parsed.shift();
-  if (first.length === 1) {
-    return charMatch(s[0], first) && isMatch(s.slice(1), parsed, dp);
-  } else {
-    if (!charMatch(s[0], first[0])) {
-      return isMatch(s, parsed, dp);
+    const first = parsed.shift();
+    if (first.length === 1) {
+      return charMatch(s[0], first) && isMatch(s.slice(1), parsed, dp);
     } else {
-      const key = JSON.stringify([s, [first, ...parsed]]);
-      if (dp[key] !== undefined) {
-        return dp[key];
+      if (!charMatch(s[0], first[0])) {
+        return isMatch(s, parsed, dp);
       } else {
-        let len = 1;
-        while (first[0] === '.' && len < s.length || s[len] === s[0]) {
-          len++;
-        }
+        const key = JSON.stringify([s, [first, ...parsed]]);
+        if (dp[key] !== undefined) {
+          return dp[key];
+        } else {
+          let len = 1;
+          while (first[0] === '.' && len < s.length || s[len] === s[0]) {
+            len++;
+          }
 
-        let ans = false;
-        for (let i = 0; i <= len; i++) {
-          ans ||= isMatch(s.slice(i), parsed, dp);
+          let ans = false;
+          for (let i = 0; i <= len; i++) {
+            ans ||= isMatch(s.slice(i), parsed, dp);
+          }
+          dp[key] = ans;
+          return ans;
         }
-        dp[key] = ans;
-        return ans;
       }
     }
-  }
+  };
+  
+  return dfs(s, parsed, {});
 };
