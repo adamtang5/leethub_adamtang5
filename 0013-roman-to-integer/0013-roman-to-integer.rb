392a86a6a1e94a877e784a99bfffc2574366c042
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-12 22:46:48 -0700

    Time: 101 ms (85.86%), Space: 211.1 MB (37.01%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.rb
@@ -0,0 +1,25 @@
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+  lookup = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000,
+  }
+
+  ans = i = 0
+  while i < s.length
+    if i+1 == s.length || lookup[s[i]] >= lookup[s[i+1]]
+      ans += lookup[s[i]]
+      i += 1
+    else
+      ans += lookup[s[i+1]] - lookup[s[i]]
+      i += 2
+    end
+  end
+  ans
+end
