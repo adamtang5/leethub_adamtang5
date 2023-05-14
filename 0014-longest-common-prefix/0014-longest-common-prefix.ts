0ba47411a0a8e0ba9fb93a38c9a0ff4fb6047579
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-13 23:40:06 -0700

    Time: 56 ms (94.98%), Space: 44.7 MB (56.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -1,11 +1,10 @@
 function longestCommonPrefix(strs: string[]): string {
-  let [ans, i] = ['', 0]
+  let ans = ''
   let minLength = Math.min(...strs.map(s => s.length));
   
-  while (i < minLength) {
+  for (let i = 0; i < minLength; i++) {
     if (strs.every(s => s[i] === strs[0][i])) {
       ans += strs[0][i]
-      i++
     } else {
       break
     }
