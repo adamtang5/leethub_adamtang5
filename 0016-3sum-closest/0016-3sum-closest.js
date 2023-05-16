e4e1b50001a46e2e57f71df626e0dfa40c55c75c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-15 23:47:07 -0700

    Time: 73 ms (89.34%), Space: 43.6 MB (78.61%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => a - b);
+  optSum = -Infinity;
+  let l, r;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    [l, r] = [i + 1, nums.length - 1];
+    while (l < r) {
+      if (Math.abs(nums[i] + nums[l] + nums[r] - target) < Math.abs(optSum - target)) {
+        optSum = nums[i] + nums[l] + nums[r];
+      }
+      if (nums[l] + nums[r] < target - nums[i]) {
+        l++;
+      } else if (nums[l] + nums[r] > target - nums[i]) {
+        r--;
+      } else {
+        return target;
+      }
+    }
+  }
+
+  return optSum;
+};
