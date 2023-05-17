608bc9a5ad6442415ab73e336c844d467eda0da9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-16 21:24:12 -0700

    Time: 47 ms (95.93%), Space: 41.7 MB (76.05%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const lookup = [
+  '',
+  '',
+  'abc',
+  'def',
+  'ghi',
+  'jkl',
+  'mno',
+  'pqrs',
+  'tuv',
+  'wxyz',
+];
+
+var letterCombinations = function(digits) {
+  if (!digits.length) return [];
+  if (digits.length === 1) return lookup[+digits].split('');
+
+  let d1Combos = letterCombinations(digits[0]);
+  return d1Combos.map(ch => {
+    return letterCombinations(digits.slice(1)).map(combo => ch + combo);
+  }).flat();
+};
