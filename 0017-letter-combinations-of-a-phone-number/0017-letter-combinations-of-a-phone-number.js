9075ab5dfd81923193d92a9bbad1db164c9ed6d0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-16 22:32:11 -0700

    Time: 52 ms (85.04%), Space: 41.9 MB (54.90%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -2,23 +2,21 @@
  * @param {string} digits
  * @return {string[]}
  */
-const lookup = [
-  '',
-  '',
-  'abc',
-  'def',
-  'ghi',
-  'jkl',
-  'mno',
-  'pqrs',
-  'tuv',
-  'wxyz',
-];
+const lookup = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+};
 
 var letterCombinations = function(digits) {
   if (!digits.length) return [];
-  if (digits.length === 1) return lookup[+digits].split('');
-
+  if (digits.length === 1) return lookup[digits[0]].split('');
+  
   const leadLetters = letterCombinations(digits[0]);
   return leadLetters.map(ch => letterCombinations(digits.slice(1)).map(combo => ch + combo)).flat();
 };
