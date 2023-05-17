2fc13baa9301115bb20d0bd2cb7ea0fc0b15f384
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-16 22:34:45 -0700

    Time: 65 ms (41.39%), Space: 44.4 MB (30.26%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,20 +1,18 @@
-const lookup: string[] = [
-  '',
-  '',
-  'abc',
-  'def',
-  'ghi',
-  'jkl',
-  'mno',
-  'pqrs',
-  'tuv',
-  'wxyz',
-]
+const lookup = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+}
 
 function letterCombinations(digits: string): string[] {
   if (!digits.length) return []
-  if (digits.length === 1) return lookup[+digits].split('')
+  if (digits.length === 1) return lookup[digits].split('')
   
-  let headCombos: string[] = letterCombinations(digits[0])
-  return headCombos.map(ch => letterCombinations(digits.slice(1)).map(combo => ch + combo)).flat()
+  const leadLetters: string[] = letterCombinations(digits[0])
+  return leadLetters.map(ch => letterCombinations(digits.slice(1)).map(combo => ch + combo)).flat()
 }
