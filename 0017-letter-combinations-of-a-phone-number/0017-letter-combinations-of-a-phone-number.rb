cbae683945a194190181d2640ddd9eec0975761e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-16 22:54:16 -0700

    Time: 83 ms (69.61%), Space: 211.1 MB (36.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.rb
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.rb
@@ -1,25 +1,20 @@
 # @param {String} digits
 # @return {String[]}
-LOOKUP = [
-  '',
-  '',
-  'abc',
-  'def',
-  'ghi',
-  'jkl',
-  'mno',
-  'pqrs',
-  'tuv',
-  'wxyz',
-]
+LOOKUP = {
+  '2'=>'abc',
+  '3'=>'def',
+  '4'=>'ghi',
+  '5'=>'jkl',
+  '6'=>'mno',
+  '7'=>'pqrs',
+  '8'=>'tuv',
+  '9'=>'wxyz',
+}
 
 def letter_combinations(digits)
-  if digits.length == 0
-    return []
-  elsif digits.length == 1
-    return LOOKUP[digits.to_i].split('')
-  else
-    d1Combos = letter_combinations(digits[0])
-    return d1Combos.map{ |ch| letter_combinations(digits[1..-1]).map{ |combo| ch + combo }}.flatten
-  end     
+  return [] if digits.length == 0
+  return LOOKUP[digits].split('') if digits.length == 1
+    
+  lead_letters = letter_combinations(digits[0])
+  lead_letters.map{ |ch| letter_combinations(digits[1..-1]).map{ |combo| ch + combo }}.flatten
 end
