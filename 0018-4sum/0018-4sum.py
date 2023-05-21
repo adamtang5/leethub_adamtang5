912fa0fc42f64cad8f8da58205d8afc141cf8477
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-20 22:10:36 -0700

    Time: 1038 ms (45.07%), Space: 16.5 MB (9.90%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,26 +1,26 @@
 import json
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        ans = set()
-        i = 0
-        while i < len(nums)-3:
-            j = i+1
-            while j < len(nums)-2:
-                l, r = j+1, len(nums)-1
-                while l < r:
-                    currSum = nums[i]+nums[j]+nums[l]+nums[r]
-                    if currSum == target:
-                        ans.add(json.dumps([nums[i], nums[j], nums[l], nums[r]]))
-                    if currSum <= target:
-                        l += 1
-                    else:
-                        r -= 1
-                        
-                while nums[j+1] == nums[j] and j < len(nums)-2:
-                    j += 1
-                j += 1
-            while nums[i+1] == nums[i] and i < len(nums)-3:
-                i += 1
-            i += 1
-        return [json.loads(s) for s in list(ans)]
+  def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    nums.sort()
+    ans = set()
+    i = 0
+    while i < len(nums)-3:
+      j = i+1
+      while j < len(nums)-2:
+        l, r = j+1, len(nums)-1
+        while l < r:
+          currSum = nums[i]+nums[j]+nums[l]+nums[r]
+          if currSum == target:
+            ans.add(json.dumps([nums[i], nums[j], nums[l], nums[r]]))
+          if currSum <= target:
+            l += 1
+          else:
+            r -= 1
+
+        while nums[j+1] == nums[j] and j < len(nums)-2:
+          j += 1
+        j += 1
+      while nums[i+1] == nums[i] and i < len(nums)-3:
+        i += 1
+      i += 1
+    return [json.loads(s) for s in list(ans)]
