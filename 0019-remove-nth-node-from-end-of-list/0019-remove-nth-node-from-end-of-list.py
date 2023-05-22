676f38851e8fbd49d42ed243d47eb47ba0526d21
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-21 23:29:22 -0700

    Time: 46 ms (30.60%), Space: 16.2 MB (27.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,23 +1,23 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#   def __init__(self, val=0, next=None):
+#     self.val = val
+#     self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length, curr = 0, head
-        while curr:
-            length += 1
-            curr = curr.next
-        
-        i, dummyHead = -1, ListNode()
-        dummyHead.next = head
-        curr = dummyHead
-        
-        while i < length - n - 1:
-            curr = curr.next
-            i += 1
-        prev = curr
-        curr = curr.next
-        prev.next = curr.next
-        return dummyHead.next
+  def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    length, curr = 0, head
+    while curr:
+      length += 1
+      curr = curr.next
+
+    i, dh = -1, ListNode()
+    dh.next = head
+    curr = dh
+
+    while i < length-n-1:
+      curr = curr.next
+      i += 1
+    prev = curr
+    curr = curr.next
+    prev.next = curr.next
+    return dh.next
