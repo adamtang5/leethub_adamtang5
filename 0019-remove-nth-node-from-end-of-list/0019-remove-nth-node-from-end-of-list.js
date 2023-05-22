b18cf1e3f55bac87cf1937a71d387ab43c91d1a4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-21 23:18:57 -0700

    Time: 62 ms (57.99%), Space: 43.4 MB (40.06%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,25 +10,23 @@
  * @param {number} n
  * @return {ListNode}
  */
-
 var removeNthFromEnd = function(head, n) {
-    let [len, curr] = [0, head];
-    
-    while (curr) {
-        len++;
-        curr = curr.next;
-    }
-    
-    let [i, dummyHead] = [-1, new ListNode()];
-    dummyHead.next = head;
-    curr = dummyHead;
-    
-    while (i < len - n - 1) {
-        curr = curr.next;
-        i++;
-    }
-    let prev = curr;
-    curr = curr.next;
-    prev.next = curr.next;
-    return dummyHead.next;
+  let [len, curr] = [0, head];
+  while (curr) {
+      len++;
+      curr = curr.next;
+  }
+
+  let [i, dh] = [-1, new ListNode()];
+  dh.next = head;
+  curr = dh;
+
+  while (i < len - n - 1) {
+      curr = curr.next;
+      i++;
+  }
+  let prev = curr;
+  curr = curr.next;
+  prev.next = curr.next;
+  return dh.next;
 };
