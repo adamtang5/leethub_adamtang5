ea8a9e111d3773ea05b39bc1409b73db86b7af36
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-22 23:25:10 -0700

    Time: 80 ms (74.68%), Space: 210.9 MB (87.10%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.rb
+++ 0020-valid-parentheses/0020-valid-parentheses.rb
@@ -1,18 +1,18 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
-    stack = []
-    open = {
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-    }
-    s.each_char do |paren|
-        if '([{'.include?(paren)
-            stack << paren
-        elsif stack.pop != open[paren]
-            return false 
-        end
+  stack = []
+  open = {
+    ')'=>'(',
+    ']'=>'[',
+    '}'=>'{',
+  }
+  s.each_char do |paren|
+    if open.has_value?(paren)
+      stack << paren
+    elsif stack.pop != open[paren]
+      return false 
     end
-    return stack.length == 0
+  end
+  stack.length == 0
 end
