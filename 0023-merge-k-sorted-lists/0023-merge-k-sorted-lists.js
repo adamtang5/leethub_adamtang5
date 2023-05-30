1612c7a7092a34a1718e7bbc2ba792b93869f06c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-29 22:46:12 -0700

    Time: 98 ms (57.59%), Space: 47.4 MB (71.23%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+const merge2Lists = (list1, list2) => {
+  const dh = new ListNode();
+  let [curr1, curr2, currDH] = [list1, list2, dh];
+  while (curr1 && curr2) {
+    if (curr1.val <= curr2.val) {
+      currDH.next = curr1;
+      curr1 = curr1.next;
+    } else {
+      currDH.next = curr2;
+      curr2 = curr2.next;
+    }
+    currDH = currDH.next;
+  }
+  currDH.next = curr1 ? curr1 : curr2;
+  return dh.next; 
+};
+    
+var mergeKLists = function(lists) {
+  if (!lists.length) return null;
+  if (lists.length === 1) return lists[0];
+  
+  let first, second;
+  while (lists.length > 1) {
+    first = lists.shift();
+    second = lists.shift();
+    lists.push(merge2Lists(first, second));
+  }
+  return lists[0];
+};
