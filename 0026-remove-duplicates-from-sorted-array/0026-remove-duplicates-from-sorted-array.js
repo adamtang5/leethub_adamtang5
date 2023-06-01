66eee047072d5ac60d779d921817560d5a63ea58
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-31 22:38:15 -0700

    Time: 63 ms (91.23%), Space: 44.8 MB (41.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -6,10 +6,8 @@ var removeDuplicates = function(nums) {
   const len = nums.length;
   let [l, r] = [0, 0];
   while (r < len) {
-    while (nums[r] === nums[l] && r < len) {
-      r++;
-    }
-    nums[l + 1] = nums[r];
+    while (nums[r] === nums[l] && r < len) r++;
+    if (r < len) nums[l + 1] = nums[r];
     l++;
   }
   return l;
