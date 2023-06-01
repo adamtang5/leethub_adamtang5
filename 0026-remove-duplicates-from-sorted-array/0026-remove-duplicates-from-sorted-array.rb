426b3bb05c7ff67d958235a32cd33d1612447438
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-31 22:33:53 -0700

    Time: 89 ms (77.08%), Space: 211.8 MB (39.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
@@ -1,15 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def remove_duplicates(nums)
-    l, r, len = 0, 0, nums.length
-    while r < len
-        while r < len && nums[r] == nums[l]
-            r += 1
-        end
-        if r < len
-            nums[l+1] = nums[r]
-        end
-        l += 1
+  l, r, len = 0, 0, nums.length
+  while r < len
+    while r < len && nums[r] == nums[l]
+      r += 1
     end
-    return l
+    if r < len
+      nums[l+1] = nums[r]
+    end
+    l += 1
+  end
+  l
 end
