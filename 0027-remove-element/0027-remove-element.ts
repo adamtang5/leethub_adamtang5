f0dabf5599d79ba99ed654ed6dd79903cc3652ad
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-31 22:58:08 -0700

    Time: 60 ms (76.38%), Space: 43.4 MB (86.02%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,9 +1,12 @@
 function removeElement(nums: number[], val: number): number {
-  let [l, r] = [0, nums.length - 1]
-  while (l <= r) {
-    while (l < nums.length && nums[l] !== val) l++
-    while (r >= 0 && nums[r] === val) r--
-    if (l < r) [nums[l], nums[r]] = [nums[r], nums[l]]
+  let [ahead, behind] = [0, 0]
+  while (ahead < nums.length) {
+    while (ahead < nums.length && nums[ahead] === val) ahead++
+    while (ahead < nums.length && nums[ahead] !== val) {
+      nums[behind] = nums[ahead]
+      behind++
+      ahead++
+    }
   }
-  return l
+  return behind
 }
