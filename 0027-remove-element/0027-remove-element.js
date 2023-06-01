c8d52667fad6bce99a64253a033aad50cd9a997d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-05-31 22:44:48 -0700

    Time: 67 ms (18.66%), Space: 42.1 MB (50.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,15 +4,11 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let [l, r] = [0, nums.length - 1];
-    while (l <= r) {
-        while (l < nums.length && nums[l] !== val) {
-            l++;
-        }
-        while (r >= 0 && nums[r] === val) {
-            r--;
-        }
-        if (l < r) [nums[l], nums[r]] = [nums[r], nums[l]];
-    }
-    return l;
+  let [l, r] = [0, nums.length - 1];
+  while (l <= r) {
+    while (l < nums.length && nums[l] !== val) l++;
+    while (r >= 0 && nums[r] === val) r--;
+    if (l < r) [nums[l], nums[r]] = [nums[r], nums[l]];
+  }
+  return l;
 };
