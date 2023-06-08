b453713fd8f398dfadead12fabfc93d6acb39e1f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-07 22:29:01 -0700

    Time: 556 ms (30.00%), Space: 213.2 MB (45.00%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.rb
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.rb
@@ -2,36 +2,32 @@
 # @param {String[]} words
 # @return {Integer[]}
 def find_substring(s, words)
-    ans = []
-    tally = Hash.new(0)
-    words.each { |word| tally[word] += 1 }
-    
-    (0...words[0].length).each do |offset|
-        start = offset
-        copy = tally.dup
-        curr_words = []
-        (offset..s.length-words[0].length).step(words[0].length).each do |i|
-            curr_word, j = '', 0
-            while j < words[0].length
-                curr_word += s[i+j]
-                j += 1
-            end
-            start = [i+j-words.length*words[0].length, start].max
-            curr_words << curr_word
-            if curr_words.length-words.length-1 >= 0
-                popped = curr_words[-words.length-1]
-                if copy[popped]
-                    copy[popped] += 1
-                end
-            end
-            if copy[curr_word]
-                copy[curr_word] -= 1
-                if copy.values.all?{ |val| val == 0 }
-                    ans << start
-                end
-            end
-        end
+  ans = []
+  tally = Hash.new(0)
+  words.each { |word| tally[word] += 1 }
+  start, curr_word, j = 0, '', 0
+
+  (0...words[0].length).each do |offset|
+    start = offset
+    copy = tally.dup
+    curr_words = []
+    (offset..s.length-words[0].length).step(words[0].length).each do |i|
+      curr_word, j = '', 0
+      while j < words[0].length
+        curr_word += s[i+j]
+        j += 1
+      end
+      start = [i+j-words.length*words[0].length, start].max
+      curr_words << curr_word
+      if curr_words.length-words.length-1 >= 0
+        popped = curr_words[-words.length-1]
+        copy[popped] += 1 if copy[popped]
+      end
+      if copy[curr_word]
+        copy[curr_word] -= 1
+        ans << start if copy.values.all?{ |val| val == 0 }
+      end
     end
-    ans.sort!
-    return ans
+  end
+  ans
 end
