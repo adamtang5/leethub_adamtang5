c3f25f82800cfa3bae718b33a6dd0950462f9a2f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-10 23:38:49 -0700

    Time: 75 ms (55.64%), Space: 44.8 MB (81.45%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.ts
@@ -0,0 +1,33 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+  function bubbleSort(start: number): void {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = start; j < nums.length - 1; j++) {
+        if (nums[j] > nums[j + 1]) [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+  }
+
+  if (nums.length === 1) return
+  let [l, r] = [nums.length - 2, nums.length - 1]
+  if (nums[l] < nums[r]) {
+    [nums[l], nums[r]] = [nums[r], nums[l]]
+    return
+  } else {
+    let end = nums.length - 1
+    while (nums[l] >= nums[r]) {
+      l--
+      r--
+    }
+    if (l < 0) {
+      bubbleSort(0)
+      return
+    }
+    while (nums[end] <= nums[l]) end--
+    [nums[l], nums[end]] = [nums[end], nums[l]]
+    bubbleSort(l + 1)
+    return
+  }
+};
