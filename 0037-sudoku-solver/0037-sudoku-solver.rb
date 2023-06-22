66910fdaab8e4743911026087534ebbc91f8c82c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-21 23:45:37 -0700

    Time: 228 ms (90.00%), Space: 210.9 MB (60.00%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.rb
+++ 0037-sudoku-solver/0037-sudoku-solver.rb
@@ -1,54 +1,30 @@
 # @param {Character[][]} board
 # @return {Void} Do not return anything, modify board in-place instead.
-class State
-  attr_accessor :solved, :rows, :cols, :subs
-  
-  def initialize(board)
-    @solved = false
-    @rows = Array.new(9){ Set.new() }
-    @cols = Array.new(9){ Set.new() }
-    @subs = Array.new(9){ Set.new() }
-    (0...9).each do |r|
-      (0...9).each do |c|
-        if board[r][c] != '.'
-          d = board[r][c].to_i
-          sub_idx = 3*(r/3) + (c/3)
-          @rows[r].add(d)
-          @cols[c].add(d)
-          @subs[sub_idx].add(d)
-        end
-      end
-    end
+def backtrack(r, c, board, solved, rows, cols, subs)
+  if r == 9
+    solved[0] = true
+    return
   end
-  
-  def backtrack(r, c, board)
-    if r == 9
-      @solved = true
-      return
-    end
-    
-    next_r, next_c = (c+1)/9+r, (c+1)%9
-    if board[r][c] != '.'
-      backtrack(next_r, next_c, board)
-    else
-      (1..9).each do |d|
-        sub_idx = 3*(r/3) + c/3
-        if !@rows[r].include?(d) && !@cols[c].include?(d) && !@subs[sub_idx].include?(d)
-          @rows[r].add(d)
-          @cols[c].add(d)
-          @subs[sub_idx].add(d)
-          board[r][c] = d.to_s
 
-          backtrack(next_r, next_c, board)
+  next_r, next_c = r+(c+1)/9, (c+1)%9
+  if board[r][c] != '.'
+    backtrack(next_r, next_c, board, solved, rows, cols, subs)
+  else
+    (1..9).each do |d|
+      sub_idx = r/3*3 + c/3
+      if !rows[r].include?(d) && !cols[c].include?(d) && !subs[sub_idx].include?(d)
+        rows[r].add(d)
+        cols[c].add(d)
+        subs[sub_idx].add(d)
+        board[r][c] = d.to_s
 
-          if !@solved
-            @rows[r].delete(d)
-            @cols[c].delete(d)
-            @subs[sub_idx].delete(d)
-            board[r][c] = '.'
-          else
-            break
-          end
+        backtrack(next_r, next_c, board, solved, rows, cols, subs)
+
+        if !solved[0]
+          rows[r].delete(d)
+          cols[c].delete(d)
+          subs[sub_idx].delete(d)
+          board[r][c] = '.'
         end
       end
     end
@@ -56,6 +32,21 @@ class State
 end
 
 def solve_sudoku(board)
-  state = State.new(board)
-  state.backtrack(0, 0, board)
+  solved = [false]
+  rows = Array.new(9){ Set.new() }
+  cols = Array.new(9){ Set.new() }
+  subs = Array.new(9){ Set.new() }
+  (0...9).each do |r|
+    (0...9).each do |c|
+      if board[r][c] != '.'
+        d = board[r][c].to_i
+        sub_idx = (r/3*3) + (c/3)
+        rows[r].add(d)
+        cols[c].add(d)
+        subs[sub_idx].add(d)
+      end
+    end
+  end
+  
+  backtrack(0, 0, board, solved, rows, cols, subs)
 end
