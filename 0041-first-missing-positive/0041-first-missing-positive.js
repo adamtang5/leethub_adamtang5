96e31e9c23cb603b47268a1cd3845d73878c319a
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-24 23:55:05 -0700

    Time: 87 ms (61.31%), Space: 57.3 MB (31.33%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-  const positives = {};
+  const positives = new Set();
   nums.forEach(n => {
-    if (n > 0) positives[n] = true;
+    if (n > 0) positives.add(n);
   });
   let missing = 1;
-  while (positives[missing] !== undefined) missing++;
+  while (positives.has(missing)) missing++;
   return missing;
 };
