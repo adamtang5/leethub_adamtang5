71e0d0dad7769808cec60a833627572912ead24f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-24 22:54:15 -0700

    Time: 55 ms (96.00%), Space: 16.6 MB (17.14%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,24 +1,23 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates = [c for c in candidates if c <= target]
-        if candidates == []:
-            return []
-        candidates.sort(reverse=True)
-        ans = []
-        
-        def dfs(c, t, suffix=[]):
-            if t == 0:
-                ans.insert(0, suffix)
-                return
-            elif c == []:
-                return
-            elif t > 0:
-                currMax = c[0]
-                n = 1
-                while n < len(c) and c[n] == c[n-1]:
-                    n += 1
-                for i in range(min(t // currMax, n), -1, -1):
-                    dfs(c[n:], t-i*currMax, [currMax]*i + suffix)
-        
-        dfs(candidates, target)
-        return ans
+  def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    candidates = [c for c in candidates if c <= target]
+    if candidates == []: return []
+    candidates.sort(reverse=True)
+    ans = []
+
+    def dfs(c, t, suffix=[]):
+      if t == 0:
+        ans.insert(0, suffix)
+        return
+      elif c == []:
+        return
+      elif t > 0:
+        currMax = c[0]
+        n = 1
+        while n < len(c) and c[n] == c[n-1]:
+          n += 1
+        for i in range(min(t//currMax, n), -1, -1):
+          dfs(c[n:], t-i*currMax, [currMax]*i+suffix)
+
+    dfs(candidates, target)
+    return ans
