6df73737d7e3d8449b274b49fedb8952593f037c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-24 23:58:27 -0700

    Time: 163 ms (56.10%), Space: 223.2 MB (43.90%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.rb
+++ 0041-first-missing-positive/0041-first-missing-positive.rb
@@ -1,9 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def first_missing_positive(nums)
-  positives = Hash.new
-  nums.each { |n| positives[n] = true if n > 0 }
+  positives = Set.new
+  nums.each { |n| positives.add(n) if n > 0 }
   missing = 1
-  missing += 1 while positives.has_key?(missing)
+  missing += 1 while positives.include?(missing)
   missing
 end
