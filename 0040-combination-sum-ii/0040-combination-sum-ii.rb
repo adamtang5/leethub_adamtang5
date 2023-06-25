7ef81b9a78134f7bedf95ce6be682c063fe69f52
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-24 22:56:00 -0700

    Time: 127 ms (48.00%), Space: 211 MB (72.00%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.rb
+++ 0040-combination-sum-ii/0040-combination-sum-ii.rb
@@ -2,27 +2,26 @@
 # @param {Integer} target
 # @return {Integer[][]}
 def dfs(c, t, ans, suffix=[])
-    if t == 0
-        ans.unshift(suffix)
-        return
-    elsif c == []
-        return
-    elsif t > 0
-        curr_max = c[0]
-        n = 1
-        while n < c.length && c[n] == c[n-1]
-            n += 1
-        end
-        ([t / curr_max, n].min).downto(0).each do |i|
-            dfs(c[n..-1], t-i*curr_max, ans, [curr_max]*i + suffix)
-        end
+  if t == 0
+    ans.unshift(suffix)
+    return
+  elsif c == []
+    return
+  elsif t > 0
+    curr_max = c[0]
+    n = 1
+    n += 1 while n < c.length && c[n] == c[n-1]
+    ([t / curr_max, n].min).downto(0).each do |i|
+      dfs(c[n..-1], t-i*curr_max, ans, [curr_max]*i + suffix)
     end
+  end
 end
 
 def combination_sum2(candidates, target)
-    candidates.delete_if { |n| n > target }
-    candidates.sort!.reverse!
-    ans = []
-    dfs(candidates, target, ans)
-    return ans
+  candidates.delete_if { |n| n > target }
+  return [] if candidates.length == 0
+  candidates.sort!.reverse!
+  ans = []
+  dfs(candidates, target, ans)
+  ans
 end
