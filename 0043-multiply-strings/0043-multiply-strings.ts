f770f28d2f86fc12a46fee9e89e1b172e776b90e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-27 16:46:28 -0700

    Time: 81 ms (57.25%), Space: 45.7 MB (59.54%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.ts
+++ 0043-multiply-strings/0043-multiply-strings.ts
@@ -1,18 +1,20 @@
 function multiply(num1: string, num2: string): string {
   if (num1 === '0' || num2 === '0') return '0'
-  const rev1: string[] = num1.split('').reverse()
-  const rev2: string[] = num2.split('').reverse()
   const products: number[] = new Array(num1.length + num2.length).fill(0)
+  let p1: number
+  let p2: number
   let prod: number
   let place: number
-  rev1.forEach((digit1, place1) => {
-    rev2.forEach((digit2, place2) => {
-      place = place1 + place2
-      prod = (+digit1) * (+digit2)
+  for (let i = num1.length - 1; i >= 0; i--) {
+    p1 = num1.length - i - 1
+    for (let j = num2.length - 1; j >= 0; j--) {
+      p2 = num2.length - j - 1
+      place = p1 + p2
+      prod = (+num1[i]) * (+num2[j])
       products[place] += prod % 10
       products[place + 1] += Math.floor(prod / 10)
-    })
-  })
+    }
+  }
   for (let i = 0; i < products.length - 1; i++) {
     if (products[i] > 9) {
       products[i + 1] += Math.floor(products[i] / 10)
