33702ee024de84111467308319179c05014348c3
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-27 16:49:43 -0700

    Time: 176 ms (11.68%), Space: 16.5 MB (9.95%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,14 +1,14 @@
 class Solution:
   def multiply(self, num1: str, num2: str) -> str:
     if num1 == '0' or num2 == '0': return '0'
-    rev1, rev2 = list(num1), list(num2)
-    rev1.reverse()
-    rev2.reverse()
-    products = [0 for i in range(len(rev1)+len(rev2))]
-    for place1, digit1 in enumerate(rev1):
-      for place2, digit2 in enumerate(rev2):
-        place = place1+place2
-        prod = int(digit1)*int(digit2)
+    products = [0 for i in range(len(num1)+len(num2))]
+    p1 = p2 = prod = place = None
+    for i in range(len(num1)-1, -1, -1):
+      p1 = len(num1)-i-1
+      for j in range(len(num2)-1, -1, -1):
+        p2 = len(num2)-j-1
+        place = p1+p2
+        prod = int(num1[i])*int(num2[j])
         products[place] += prod%10
         products[place+1] += floor(prod/10)
     for i in range(len(products)-1):
