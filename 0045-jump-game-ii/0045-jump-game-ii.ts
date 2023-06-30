6a1fd2fadc802df3be184d8385e4412200ee66f2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-06-29 23:44:49 -0700

    Time: 77 ms (68.76%), Space: 44.6 MB (86.89%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -0,0 +1,12 @@
+function jump(nums: number[]): number {
+  let [ans, l, r, ub] = [0, 0, 0, 0]
+  while (r < nums.length - 1) {
+    for (let i = l; i <= r; i++) {
+      ub = Math.max(ub, i + nums[i])
+    }
+    l = r + 1
+    r = ub
+    ans++
+  }
+  return ans
+}
