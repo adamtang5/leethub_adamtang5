9fd5be07b2ae786c2b9bd74f81d261fb07c1fbae
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-07 23:11:47 -0700

    Time: 118 ms (77.02%), Space: 46.1 MB (82.66%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.js
+++ 0044-wildcard-matching/0044-wildcard-matching.js
@@ -4,55 +4,50 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  p = p.replaceAll(/\*+/g, '*');
-  
   const charMatch = (ch1, ch2) => {
+    if (!ch1 || !ch2) return false;
     return ch2 === '?' || ch2 === ch1;
   };
   
-  const dfs = (sIdx, pIdx, dp) => {
-    const key = `${sIdx}-${pIdx}`;
-    
-    if (sIdx < s.length && pIdx === p.length) {
-      dp[key] = false;
-      return dp[key];
-    }
-    if (sIdx === s.length && pIdx === p.length) {
-      dp[key] = true;
-      return dp[key];
-    }
-    if (sIdx === s.length && pIdx < p.length) {
-      dp[key] = true;
-      for (let i = pIdx; i < p.length; i++) {
-        if (p[i] !== '*') {
-          dp[key] = false;
-          break;
-        }
-      }
-      return dp[key];
-    }
-    
-    if (p[pIdx] !== '*') {
-      if (!charMatch(s[sIdx], p[pIdx])) {
-        dp[key] = false;
-        return dp[key];
+  const parse = (s, chunks) => {
+    if (!chunks.length) return true;
+    let [start, cIdx, j] = [0, 0, 1];
+    while (cIdx < chunks.length && start < s.length) {
+      while (start < s.length && !charMatch(s[start], chunks[cIdx][0])) start++;
+      if (start === s.length) break;
+      j = 1;
+      while (j < chunks[cIdx].length && start + j < s.length && charMatch(s[start + j], chunks[cIdx][j])) j++;
+      if (j === chunks[cIdx].length) {
+        cIdx++;
+        start += j;
       } else {
-        dp[key] = dfs(sIdx + 1, pIdx + 1, dp);
-        return dp[key];
-      }
-    } else {
-      if (dp[key] !== undefined) {
-        return dp[key];
-      } else {
-        let ans = false;
-        for (let i = sIdx; i <= s.length; i++) {
-          ans ||= dfs(i, pIdx + 1, dp);
-        }
-        dp[key] = ans;
-        return dp[key];
+        start++;
       }
     }
+    return cIdx === chunks.length;
   };
   
-  return dfs(0, 0, {});
+  p = p.replaceAll(/\*+/g, '*');
+  let i = 0;
+  if (p[i] !== '*') {
+    while (p[i] !== '*' && i < p.length) {
+      if (!charMatch(s[i], p[i])) return false;
+      i++;
+    }
+    s = s.slice(i);
+    p = p.slice(i);
+  }
+  if (p === '') return s === '';
+  i = 0;
+  if (p[p.length - 1 - i] !== '*') {
+    while (p[p.length - 1 - i] !== '*') {
+      if (!charMatch(s[s.length - 1 - i], p[p.length - 1 - i])) return false;
+      i++;
+    }
+    s = s.slice(0, s.length - i);
+    p = p.slice(0, p.length - i);
+  }
+  if (p === '*') return true;
+  
+  return parse(s, p.slice(1, p.length - 1).split('*'));
 };
