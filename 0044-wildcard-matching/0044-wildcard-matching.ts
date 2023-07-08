5d4ebe585dc2cf7a9e03d1b6aefc91af0163b3c9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-07 23:24:17 -0700

    Time: 132 ms (66.67%), Space: 50.5 MB (65.15%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.ts
+++ 0044-wildcard-matching/0044-wildcard-matching.ts
@@ -1,4 +1,27 @@
 function isMatch(s: string, p: string): boolean {
+  function charMatch(ch1: string | undefined, ch2: string | undefined): boolean {
+    if (!ch1 || !ch2) return false
+    return ch2 === '?' || ch2 === ch1
+  }
+  
+  function parse(s: string, chunks: string[]): boolean {
+    if (!chunks.length) return true
+    let [start, cIdx, j] = [0, 0, 1]
+    while (cIdx < chunks.length && start < s.length) {
+      while (start < s.length && !charMatch(s[start], chunks[cIdx][0])) start++
+      if (start === s.length) break
+      j = 1
+      while (j < chunks[cIdx].length && start + j < s.length && charMatch(s[start + j], chunks[cIdx][j])) j++
+      if (j === chunks[cIdx].length) {
+        cIdx++
+        start += j
+      } else {
+        start++
+      }
+    }
+    return cIdx === chunks.length
+  }
+  
   let t: string = ''
   for (let i = 0; i < p.length; i++) {
     if (p[i] === '*') {
@@ -7,54 +30,26 @@ function isMatch(s: string, p: string): boolean {
     t += p[i]
   }
   p = t
-  
-  function charMatch(ch1: string, ch2: string): boolean {
-    return ch2 === '?' || ch2 === ch1
+  let i = 0
+  if (p[i] !== '*') {
+    while (p[i] !== '*' && i < p.length) {
+      if (!charMatch(s[i], p[i])) return false
+      i++
+    }
+    s = s.slice(i)
+    p = p.slice(i)
   }
-  
-  function dfs(sIdx: number, pIdx: number, dp: {}): boolean {
-    const key: string = `${sIdx}-${pIdx}`
-    
-    if (sIdx < s.length && pIdx === p.length) {
-      dp[key] = false
-      return dp[key]
+  if (p === '') return s === ''
+  i = 0
+  if (p[p.length - 1 - i] !== '*') {
+    while (p[p.length - 1 - i] !== '*') {
+      if (!charMatch(s[s.length - 1 - i], p[p.length - 1 - i])) return false
+      i++
     }
-    if (sIdx === s.length && pIdx === p.length) {
-      dp[key] = true
-      return dp[key]
-    }
-    if (sIdx === s.length && pIdx < p.length) {
-      dp[key] = true
-      for (let i = pIdx; i < p.length; i++) {
-        if (p[i] !== '*') {
-          dp[key] = false
-          break
-        }
-      }
-      return dp[key]
-    }
-    
-    if (p[pIdx] !== '*') {
-      if (!charMatch(s[sIdx], p[pIdx])) {
-        dp[key] = false
-        return dp[key]
-      } else {
-        dp[key] = dfs(sIdx + 1, pIdx + 1, dp)
-        return dp[key]
-      }
-    } else {
-      if (dp[key] !== undefined) {
-        return dp[key]
-      } else {
-        let ans = false
-        for (let i = sIdx; i <= s.length; i++) {
-          ans ||= dfs(i, pIdx + 1, dp)
-        }
-        dp[key] = ans
-        return dp[key]
-      }
-    }
+    s = s.slice(0, s.length - i)
+    p = p.slice(0, p.length - i)
   }
+  if (p === '*') return true
   
-  return dfs(0, 0, {})
+  return parse(s, p.slice(1, p.length - 1).split('*'))
 }
