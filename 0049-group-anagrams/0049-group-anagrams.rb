f3eddb6cd2c9e96658421ac1888f854486df6254
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-09 23:21:00 -0700

    Time: 216 ms (42.12%), Space: 216 MB (73.48%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.rb
@@ -0,0 +1,12 @@
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+  groups = Hash.new { |h, k| h[k] = [] }
+  strs.each do |s|
+    keyStr = s.bytes
+    keyStr.sort!
+    keyStr = keyStr.inspect
+    groups[keyStr] << s
+  end
+  groups.values
+end
