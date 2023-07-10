7c9214384609b3a33e585ee74d0f954d6e9005e9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-09 23:20:06 -0700

    Time: 138 ms (15.53%), Space: 20.8 MB (42.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    groups = collections.defaultdict(list)
+    for s in strs:
+      key = [0]*26
+      for ch in s:
+        key[ord(ch)-ord('a')] += 1
+      key = str(key)
+      groups[key].append(s)
+    return groups.values()
