652d6cfb734e6413ff7010ea0d5dc04027624e73
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-11 12:52:48 -0700

    Time: 73 ms (31.89%), Space: 43.7 MB (92.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.ts
+++ 0050-powx-n/0050-powx-n.ts
@@ -1,20 +1,9 @@
 function myPow(x: number, n: number): number {
-  if (n === 0) return 1
-  if (n > 0) {
-    while (n > 0) {
-      if (n % 2) {
-        return x * myPow(x, n - 1)
-      } else {
-        return myPow(x * x, n / 2)
-      }
-    }
-  } else {
-    while (n < 0) {
-      if (n % 2 < 0) {
-        return myPow(x, n + 1) / x
-      } else {
-        return myPow(x * x, n / 2)
-      }
-    }
+  if (n === 0) {
+    return 1
+  } else if (n > 0) {
+    return n % 2 ? x * myPow(x, n - 1) : myPow(x * x, n / 2)
+  } else if (n < 0) {
+    return n % 2 < 0 ? myPow(x, n + 1) / x : myPow(x * x, n / 2)
   }
 }
