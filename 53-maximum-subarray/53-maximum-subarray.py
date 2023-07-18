6eaf728901e2060b172911a682abf80d300b9fe0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-17 22:28:44 -0700

    Delete 53-maximum-subarray directory

--- 53-maximum-subarray/53-maximum-subarray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        prevMax, currMax, ans = float('-Inf'), float('-Inf'), float('-Inf')
-        for n in nums:
-            prevMax = currMax
-            currMax = max(n, n + prevMax)
-            ans = max(ans, currMax)
-        return ans
