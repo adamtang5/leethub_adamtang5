23dfaced9353545b82e6beb8c69f281c732e2f9b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-17 22:49:14 -0700

    Time: 75 ms (5.84%), Space: 42.7 MB (74.54%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.ts
@@ -0,0 +1,39 @@
+function spiralOrder(matrix: number[][]): number[] {
+  let [rlb, rub, clb, cub] = [0, matrix.length - 1, 0, matrix[0].length - 1]
+  const ans: number[] = []
+  
+  function goRight(): void {
+    for (let i = clb; i <= cub; i++) {
+      ans.push(matrix[rlb][i])
+    }
+    rlb++
+  }
+  function goDown(): void {
+    for (let i = rlb; i <= rub; i++) {
+      ans.push(matrix[i][cub])
+    }
+    cub--
+  }
+  function goLeft(): void {
+    for (let i = cub; i >= clb; i--) {
+      ans.push(matrix[rub][i])
+    }
+    rub--
+  }
+  function goUp(): void {
+    for (let i = rub; i >= rlb; i--) {
+      ans.push(matrix[i][clb])
+    }
+    clb++
+  }
+  
+  const moveSeq: Function[] = [goRight, goDown, goLeft, goUp]
+  
+  let moveIdx = 0;
+  while (rub >= rlb && cub >= clb) {
+    moveSeq[moveIdx % moveSeq.length]()
+    moveIdx++
+  }
+  
+  return ans
+}
