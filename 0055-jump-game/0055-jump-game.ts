683cce8024607f88fee1cb209bb71adc3c1560e0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-20 22:44:51 -0700

    Time: 79 ms (65.01%), Space: 47.6 MB (70.44%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.ts
@@ -0,0 +1,12 @@
+function canJump(nums: number[]): boolean {
+  let [i, reach] = [0, nums[0]]
+  while (i < reach) {
+    if (reach >= nums.length - 1) {
+      return true;
+    } else {
+      i++;
+      reach = Math.max(i + nums[i], reach);
+    }
+  }
+  return reach >= nums.length - 1;
+}
