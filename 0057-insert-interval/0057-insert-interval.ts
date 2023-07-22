63a36c3c4410d40650a7d961e9c6880d212bdab1
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-22 00:06:35 -0700

    Time: 62 ms (98.77%), Space: 44.9 MB (72.92%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.ts
@@ -0,0 +1,25 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  let [i, len, idx] = [0, 0, -Infinity]
+  while (i < intervals.length) {
+    if (newInterval[1] < intervals[i][0]) {
+      idx = i
+      break
+    } else if (newInterval[0] <= intervals[i][1]) {
+      idx = i
+      newInterval[0] = Math.min(intervals[i][0], newInterval[0])
+      newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+      len++
+      i++
+      while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+        len++
+        i++
+      }
+      break
+    }
+    i++
+  }
+  if (idx < 0) idx = i
+  intervals.splice(idx, len, newInterval)
+  return intervals
+}
