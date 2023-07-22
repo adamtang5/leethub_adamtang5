fe3dacf0f4eafe460b883dbafd71538facc3cf55
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-22 13:48:19 -0700

    Time: 63 ms (54.54%), Space: 42.7 MB (75.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,10 +1,3 @@
 function lengthOfLastWord(s: string): number {
-  let i: number = s.length - 1
-  while (s[i] === ' ') i--
-  let ans: number = 0
-  while (i >= 0 && s[i] !== ' ') {
-    ans++
-    i--
-  }
-  return ans
+  return s.trim().split(' ').at(-1).length
 }
