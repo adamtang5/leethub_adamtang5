e29db7dfa7f974cd83bcabeb088d139964bda145
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-23 22:45:39 -0700

    Time: 62 ms (69.40%), Space: 42.6 MB (35.82%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.js
+++ 0060-permutation-sequence/0060-permutation-sequence.js
@@ -9,17 +9,17 @@ var getPermutation = function(n, k) {
     return n * fact(n - 1);
   };
   
-  const dfs = (q, s, ans=[]) => {
+  const dfs = (q, s, ans='') => {
     if (s.length === 0) return ans;
     
     const subSize = fact(s.length - 1);
     const subIdx = Math.min(q / subSize);
     const ext = s.splice(subIdx, 1);
-    return dfs(q % subSize, s, [...ans, ...ext]);
+    return dfs(q % subSize, s, ans + ext[0]);
   }
   
   // const seq = _.range(1, n + 1);   * using lodash
   const seq = Array.from({ length: n }, (v, i) => (i + 1).toString());
   
-  return dfs(k - 1, seq).join("");
+  return dfs(k - 1, seq);
 };
