ca36291f7d8b1a4e8de89ea13deb9e8cf3262626
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-27 16:58:40 -0700

    Time: 60 ms (100.00%), Space: 211 MB (38.10%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.rb
@@ -0,0 +1,25 @@
+# @param {Integer[][]} obstacle_grid
+# @return {Integer}
+
+def in_bounds(r, c, grid)
+  r >= 0 && r < grid.length && c >= 0 && c < grid[0].length
+end
+
+def dp_value(r, c, grid, dp)
+  in_bounds(r, c, grid) ? dp[r][c] : 0
+end
+
+def unique_paths_with_obstacles(obstacle_grid)
+  return 0 if obstacle_grid[-1][-1] == 1
+  dp = Array.new(obstacle_grid.length) { Array.new(obstacle_grid[0].length, nil) }
+  dp[-1][-1] = obstacle_grid[-1][-1] == 1 ? 0 : 1
+
+  (0...obstacle_grid.length).reverse_each do |r|
+    (0...obstacle_grid[0].length).reverse_each do |c|
+      if r != obstacle_grid.length-1 || c != obstacle_grid[0].length-1
+        dp[r][c] = obstacle_grid[r][c] == 0 ? dp_value(r+1, c, obstacle_grid, dp) + dp_value(r, c+1, obstacle_grid, dp) : 0
+      end
+    end
+  end
+  dp[0][0]
+end
