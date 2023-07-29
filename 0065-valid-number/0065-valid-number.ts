4dba0b6daf2cad7dfea6b8804f96c94d79eeccbf
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-29 13:43:13 -0700

    Time: 68 ms (92.31%), Space: 44.7 MB (74.36%) - LeetHub

--- 0065-valid-number/0065-valid-number.ts
+++ 0065-valid-number/0065-valid-number.ts
@@ -1,35 +1,32 @@
 function isNumber(s: string): boolean {
   const digitsRe: RegExp = /[0-9]/
   
-  function validDec(s: string): boolean {
+  function validNum(s: string, numType: string): boolean {
     if (s.length === 0) return false
-    if ("+-".includes(s[0])) return validDec(s.slice(1))
-    let dotIdx: number = -1
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === ".") {
-        if (dotIdx >= 0 || s.length === 1) return false
-        dotIdx = i
-      } else if (!digitsRe.test(s[i])) {
-        return false
+    if ("+-".includes(s[0])) return validNum(s.slice(1), numType)
+    if (numType === "dec") {
+      let dotIdx: number = -1
+      for (let i = 0; i < s.length; i++) {
+        if (s[i] === ".") {
+          if (dotIdx >= 0 || s.length === 1) return false
+          dotIdx = i
+        } else if (!digitsRe.test(s[i])) {
+          return false
+        }
       }
-    }
-    return true
-  }
-  
-  function validInt(s: string): boolean {
-    if (s.length === 0) return false
-    if ("+-".includes(s[0])) return validDec(s.slice(1))
-    for (let i = 0; i < s.length; i++) {
-      if (!digitsRe.test(s[i])) return false
+    } else if (numType === "int") {
+      for (let i = 0; i < s.length; i++) {
+        if (!digitsRe.test(s[i])) return false
+      }
     }
     return true
   }
   
   const parts: string[] = s.toLowerCase().split("e")
   if (parts.length === 1) {
-    return validDec(parts[0])
+    return validNum(parts[0], "dec")
   } else if (parts.length === 2) {
-    return validDec(parts[0]) && validInt(parts[1])
+    return validNum(parts[0], "dec") && validNum(parts[1], "int")
   } else {
     return false
   }
