df7697cf048740b22b70cf20f718c43484887a76
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-07-29 13:50:42 -0700

    Time: 75 ms (70.76%), Space: 44.3 MB (56.36%) - LeetHub

--- 0065-valid-number/0065-valid-number.js
+++ 0065-valid-number/0065-valid-number.js
@@ -5,35 +5,32 @@
 var isNumber = function(s) {
   const digitsRe = /[0-9]/;
 
-  const validDec = s => {
+  const validNum = (s, numType) => {
     if (s.length === 0) return false;
-    if ("+-".includes(s[0])) return validDec(s.slice(1));
-    let dotIdx = -1;
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === ".") {
-        if (dotIdx >= 0 || s.length === 1) return false;
-        dotIdx = i;
-      } else if (!digitsRe.test(s[i])) {
-        return false;
+    if ("+-".includes(s[0])) return validNum(s.slice(1), numType);
+    if (numType === "dec") {
+      let dotIdx = -1;
+      for (let i = 0; i < s.length; i++) {
+        if (s[i] === ".") {
+          if (dotIdx >= 0 || s.length === 1) return false;
+          dotIdx = i;
+        } else if (!digitsRe.test(s[i])) {
+          return false;
+        }
       }
-    }
-    return true;
-  };
-  
-  const validInt = s => {
-    if (s.length === 0) return false;
-    if ("+-".includes(s[0])) return validDec(s.slice(1));
-    for (let i = 0; i < s.length; i++) {
-      if (!digitsRe.test(s[i])) return false;
+    } else if (numType === "int") {
+      for (let i = 0; i < s.length; i++) {
+        if (!digitsRe.test(s[i])) return false;
+      }
     }
     return true;
   };
   
   const parts = s.toLowerCase().split("e");
   if (parts.length === 1) {
-    return validDec(parts[0]);
+    return validNum(parts[0], "dec");
   } else if (parts.length === 2) {
-    return validDec(parts[0]) && validInt(parts[1]);
+    return validNum(parts[0], "dec") && validNum(parts[1], "int");
   } else {
     return false;
   }
