5b1bc254f392cb6b38ebfc75e92743618d6292eb
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-03 21:04:18 -0700

    Time: 53 ms (83.38%), Space: 41.6 MB (85.29%) - LeetHub

--- 0068-text-justification/0068-text-justification.js
+++ 0068-text-justification/0068-text-justification.js
@@ -37,7 +37,7 @@ var fullJustify = function(words, maxWidth) {
       minWidth = l < words.length ? words[l].length : 0;
     } else if (minWidth > maxWidth) {
       minWidth -= words[r].length;
-      minWidth -= 1;
+      minWidth--;
       r--;
       lines.push(justifyLine(words.slice(l, r + 1), maxWidth));
       l = r + 1;
@@ -47,7 +47,7 @@ var fullJustify = function(words, maxWidth) {
       r++;
       if (r < words.length) {
         minWidth += words[r].length;
-        minWidth += 1;
+        minWidth++;
       } else {
         lines.push(leftJustify(words.slice(l), maxWidth));
         break;
