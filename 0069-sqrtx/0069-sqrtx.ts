01c1b54cbf4d068e8b378e83911d8329f2421dcf
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-06 11:01:05 -0700

    Time: 71 ms (76.03%), Space: 44.8 MB (56.63%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -0,0 +1,15 @@
+function mySqrt(x: number): number {
+  if (x < 2) return x
+  let [l, r] = [0, x]
+  let pivot: number = Math.floor((l + r) / 2)
+  while (l < r) {
+    if (pivot * pivot <= x && (pivot + 1) * (pivot + 1) > x) {
+      return pivot
+    } else if (pivot * pivot > x) {
+      r = pivot
+    } else {
+      l = pivot
+    }
+    pivot = Math.floor((l + r) / 2)
+  }
+}
