44ac7004582c084e2946b6e427ae60a08de29f73
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-07 16:57:08 -0700

    Time: 1 ms (99.60%), Space: 40.1 MB (13.10%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,13 +1,12 @@
 class Solution {
   public int mySqrt(int x) {
-    // if (x == Integer.MAX_VALUE) return mySqrt(x - 1);
     int pivot = 46340;
     if (x >= pivot * pivot) return pivot;
     if (x < 2) return x;
     int l = 0;
     int r = x;
     while (l < r) {
-      if (pivot * pivot <= x && (pivot + 1) * (pivot + 1) > 0 && (pivot + 1) * (pivot + 1) > x) {
+      if (pivot * pivot <= x && (pivot + 1) * (pivot + 1) > x) {
         break;
       } else if (pivot * pivot > x) {
         r = pivot;
