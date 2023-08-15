6603740d962921a39862581d706e9058b07c01ae
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-15 14:30:43 -0700

    Time: 654 ms (5.07%), Space: 219.3 MB (5.07%) - LeetHub

---
+++ 0077-combinations/0077-combinations.ts
@@ -0,0 +1,25 @@
+function combine(n: number, k: number): number[][] {
+  function invert(combo: number[], n: number): number[] {
+    const ans: number[] = []
+    for (let i = 1; i <= n; i++) {
+      if (!combo.includes(i)) ans.push(i)
+    }
+    return ans
+  }
+  
+  if (k === 1) {
+    return new Array(n).fill([]).map((combo, i) => [i + 1])
+  } else if (k === n) {
+    return [new Array(n).fill([]).map((el, i) => i + 1)]
+  } else if (k <= n - k) {
+    const ans: number[][] = []
+    combine(n, k - 1).forEach(combo => {
+      for (let s = combo.at(-1) + 1; s <= n; s++) {
+        ans.push([...combo, s])
+      }
+    })
+    return ans
+  } else {
+    return combine(n, n - k).map(combo => invert(combo, n))
+  }
+}
