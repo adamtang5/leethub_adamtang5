666aca6eba2f5c91e10614546f6b8703bd518b7e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-16 00:12:29 -0700

    Time: 65 ms (5.04%), Space: 111.8 MB (5.34%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,43 @@
 class Solution {
-  public void backtrack(int start, List<Integer> combo, List<List<Integer>> ans, int n, int k) {
-    if (combo.size() == k) {
-      ans.add(new ArrayList<Integer>(combo));
-      return;
+  public List<Integer> invert(List<Integer> combo, int n) {
+    List<Integer> ans = new ArrayList<Integer>();
+    for (int i = 1; i <= n; i++) {
+      if (!combo.contains(i)) ans.add(i);
     }
-    
-    for (int i = start; i <= n; i++) {
-      combo.add(i);
-      backtrack(i + 1, combo, ans, n, k);
-      combo.remove(combo.size() - 1);
-    }
+    return ans;
   }
   
   public List<List<Integer>> combine(int n, int k) {
     List<List<Integer>> ans = new ArrayList();
-    backtrack(1, new ArrayList<Integer>(), ans, n, k);
+    if (k == 1) {
+      for (int i = 1; i <= n; i++) {
+        List<Integer> single = new ArrayList<Integer>();
+        single.add(i);
+        ans.add(single);
+      }
+    } else if (k == n) {
+      List<Integer> all = new ArrayList<Integer>();
+      for (int i = 1; i <= n; i++) {
+        all.add(i);
+      }
+      ans.add(all);
+    } else if (k <= n - k) {
+      List<List<Integer>> reduced = combine(n, k - 1);
+      for (List<Integer> combo : reduced) {
+        for (int s = combo.get(combo.size() - 1) + 1; s <= n; s++) {
+          List<Integer> newCombo = new ArrayList<Integer>();
+          newCombo.addAll(combo);
+          newCombo.add(s);
+          ans.add(newCombo);
+        }
+      }
+    } else {
+      List<List<Integer>> reduced = combine(n, n - k);
+      return reduced.stream()
+        .map(combo -> invert(combo, n))
+        .collect(Collectors.toList());
+    }
+    
     return ans;
   }
 }
