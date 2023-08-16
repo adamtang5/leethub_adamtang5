09b0e6b854b200219eefef8f6438ed1f7536c73a
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-16 16:18:19 -0700

    Time: 72 ms (73.85%), Space: 211.2 MB (18.46%) - LeetHub

--- 0078-subsets/0078-subsets.rb
+++ 0078-subsets/0078-subsets.rb
@@ -1,15 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def subsets(nums)
-    ans = []
-    (0...2**nums.length).each do |i|
-        sub = []
-        (0...nums.length).each do |j|
-            if i & (1<<j) > 0
-                sub << nums[j]
-            end
-        end
-        ans << sub
+  ans = []
+  (0...2**nums.length).each do |i|
+    sub = []
+    (0...nums.length).each do |j|
+      sub << nums[j] if i & (1<<j) > 0
     end
-    return ans
+    ans << sub
+  end
+  ans
 end
