31aad26f4df995f9632e271e27fabf0748eaac53
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-16 15:20:38 -0700

    Time: 52 ms (90.41%), Space: 44.4 MB (16.17%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,13 +3,13 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    const ans = [];
-    for (let i = 0; i < 2 ** nums.length; i++) {
-        const sub = [];
-        for (let j = 0; j < nums.length; j++) {
-            if (i & 1 << j) sub.push(nums[j]);
-        }
-        ans.push(sub);
+  const ans = [];
+  for (let i = 0; i < 2 ** nums.length; i++) {
+    const sub = [];
+    for (let j = 0; j < nums.length; j++) {
+      if (i & 1 << j) sub.push(nums[j]);
     }
-    return ans;
+    ans.push(sub);
+  }
+  return ans;
 };
