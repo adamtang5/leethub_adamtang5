f23eafc948a0ee1b6a1b32e5030b3ef714c8f962
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-19 22:58:17 -0700

    Time: 2111 ms (24.30%), Space: 50.7 MB (8.86%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,46 +4,46 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const chTally = {};
-    for (let r = 0; r < board.length; r++) {
-        for (let c = 0; c < board[0].length; c++) {
-            chTally[board[r][c]] = chTally[board[r][c]] || 0;
-            chTally[board[r][c]]++;
-        }
+  const chTally = {};
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length; c++) {
+      chTally[board[r][c]] = chTally[board[r][c]] || 0;
+      chTally[board[r][c]]++;
     }
-    for (const ch of word) {
-        if (chTally[ch] === 0) return false;
-        chTally[ch]--;
-    }
-    
-    const path = new Set();
-    
-    const inBound = (r, c) => {
-        return r >= 0 && r < board.length && c >= 0 && c < board[0].length;
-    };
-    
-    const valid = (r, c, i) => {
-        return inBound(r, c) && word[i] === board[r][c] && !path.has(`${r}-${c}`);
-    };
-    
-    const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    
-    const dfs = (r, c, i) => {
-        if (i === word.length) return true;
-        if (!valid(r, c, i)) return false;
-        path.add(`${r}-${c}`);
-        const ans = dirs.reduce((res, [rd, cd]) => {
-            const [nr, nc] = [r + rd, c + cd];
-            return res || dfs(nr, nc, i + 1);
-        }, false);
-        path.delete(`${r}-${c}`);
-        return ans;
-    };
+  }
+  for (const ch of word) {
+    if (chTally[ch] === 0) return false;
+    chTally[ch]--;
+  }
 
-    for (let r = 0; r < board.length; r++) {
-        for (let c = 0; c < board[0].length; c++) {
-            if (dfs(r, c, 0)) return true;
-        }
+  const path = new Set();
+
+  const inBound = (r, c) => {
+    return r >= 0 && r < board.length && c >= 0 && c < board[0].length;
+  };
+
+  const valid = (r, c, i) => {
+    return inBound(r, c) && word[i] === board[r][c] && !path.has(`${r}-${c}`);
+  };
+
+  const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+  const dfs = (r, c, i) => {
+    if (i === word.length) return true;
+    if (!valid(r, c, i)) return false;
+    path.add(`${r}-${c}`);
+    const ans = dirs.reduce((res, [rd, cd]) => {
+      const [nr, nc] = [r + rd, c + cd];
+      return res || dfs(nr, nc, i + 1);
+    }, false);
+    path.delete(`${r}-${c}`);
+    return ans;
+  };
+
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length; c++) {
+      if (dfs(r, c, 0)) return true;
     }
-    return false;
+  }
+  return false;
 };
