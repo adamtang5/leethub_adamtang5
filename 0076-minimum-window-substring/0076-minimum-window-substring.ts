733c32505eb4ab3ee7ee142043fb9bbd998e75e3
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-30 00:30:11 -0700

    Time: 109 ms (53.83%), Space: 48.9 MB (51.86%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.ts
@@ -0,0 +1,27 @@
+function minWindow(s: string, t: string): string {
+  const tTally = {}
+  const window = {}
+  for (let i = 0; i < t.length; i++) {
+    tTally[t[i]] = tTally[t[i]] || 0
+    tTally[t[i]]++
+  }
+  let [have, lb, ub, l] = [0, -1, s.length + 1, 0]
+  const need: number = Object.keys(tTally).length
+  
+  for (let r = 0; r < s.length; r++) {
+    if (tTally[s[r]] !== undefined) {
+      window[s[r]] = window[s[r]] || 0
+      window[s[r]]++
+      if (window[s[r]] === tTally[s[r]]) have++
+      while (have === need) {
+        if (r + 1 - l < ub - lb) [lb, ub] = [l, r + 1]
+        if (tTally[s[l]] !== undefined) {
+          window[s[l]]--
+          if (window[s[l]] < tTally[s[l]]) have--
+        }
+        l++
+      }
+    }
+  }
+  return lb < 0 ? "" : s.slice(lb, ub)
+}
