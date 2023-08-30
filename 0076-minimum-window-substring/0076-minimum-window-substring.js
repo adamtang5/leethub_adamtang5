bc31168a5639bb045d4de5711bb536a80c14e61c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-08-30 00:20:29 -0700

    Time: 105 ms (34.24%), Space: 48.1 MB (33.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -6,15 +6,12 @@
 var minWindow = function(s, t) {
   const tTally = {};
   const window = {};
-  let have = 0;
   for (let i = 0; i < t.length; i++) {
     tTally[t[i]] = tTally[t[i]] || 0;
     tTally[t[i]]++;
   }
+  let [have, lb, ub, l] = [0, -1, s.length + 1, 0];
   const need = Object.keys(tTally).length;
-  
-  let [lb, ub] = [-1, s.length + 1];
-  let l = 0;
   
   for (let r = 0; r < s.length; r++) {
     if (tTally[s[r]] !== undefined) {
