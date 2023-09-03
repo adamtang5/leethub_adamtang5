e67ab6307d15852de16a58dcb3140751a2e2b44f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-09-02 22:46:26 -0700

    Time: 143 ms (100.00%), Space: 219.5 MB (16.67%) - LeetHub

--- 0089-gray-code/0089-gray-code.rb
+++ 0089-gray-code/0089-gray-code.rb
@@ -1,15 +1,15 @@
 # @param {Integer} n
 # @return {Integer[]}
 def bit_seq(n)
-  return [0] * 2 if n == 1
+  return [0]*2 if n == 1
   last_seq = bit_seq(n-1)
   last_seq[-1] = n-1
-  return last_seq*2
+  last_seq*2
 end
 
 def gray_code(n)
   seq = bit_seq(n)
-  ans = [0] * (2**n)
+  ans = [0]*(2**n)
   (0...ans.length-1).each{ |i| ans[i+1] = ans[i] ^ (1 << (n-1-seq[i])) }
-  return ans
+  ans
 end
