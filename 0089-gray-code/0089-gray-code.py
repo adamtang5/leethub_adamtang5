7db088b29e3005d1e3d20399475be64abe09a2a4
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-09-02 22:45:31 -0700

    Time: 92 ms (66.13%), Space: 23.6 MB (84.92%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,14 +1,13 @@
 class Solution:
   def grayCode(self, n: int) -> List[int]:
-    def bitSeq(n):
-      if n == 1:
-        return [0] * 2
+    def bitSeq(n: int) -> List[int]:
+      if n == 1: return [0]*2
       lastSeq = bitSeq(n-1)
       lastSeq[-1] = n-1
       return lastSeq*2
     
     seq = bitSeq(n)
-    ans = [0] * (2**n)
+    ans = [0]*(2**n)
     for i in range(len(ans)-1):
       ans[i+1] = ans[i] ^ (1 << (n-1-seq[i]))
     return ans
