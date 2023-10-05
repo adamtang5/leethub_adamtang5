897029ad152505a377029a891e40cf38dd0b7127
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-04 20:15:14 -0700

    Time: 40 ms (93.97%), Space: 17.9 MB (20.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,13 @@
 import re
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-z0-9]', '', s.lower())
-        l, r = 0, len(s)-1
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            else:
-                l += 1
-                r -= 1
-        return True
+  def isPalindrome(self, s: str) -> bool:
+    s = re.sub('[^a-z0-9]', '', s.lower())
+    l, r = 0, len(s)-1
+    while l < r:
+      if s[l] != s[r]:
+        return False
+      else:
+        l += 1
+        r -= 1
+    return True
