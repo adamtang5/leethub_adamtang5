6c7202a5fea90ebad3767a66408eeca0f925adff
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-04 20:10:11 -0700

    Time: 52 ms (94.21%), Space: 44.9 MB (67.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const re = /[^a-z0-9]/g;
-    s = s.toLowerCase().replace(re, '');
-    let [l, r] = [0, s.length - 1];
-    while (l < r) {
-        if (s[l] !== s[r]) {
-            return false;
-        } else {
-            l++;
-            r--;
-        }
+  const re = /[^a-z0-9]/g;
+  s = s.toLowerCase().replace(re, '');
+  let [l, r] = [0, s.length - 1];
+  while (l < r) {
+    if (s[l] !== s[r]) {
+      return false;
+    } else {
+      l++;
+      r--;
     }
-    return true;
+  }
+  return true;
 };
