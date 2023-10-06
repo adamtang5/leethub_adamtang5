3f05bfafad53a7b4dc80ec7b1f76c04fe7b1bb79
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-05 22:54:25 -0700

    Time: 4 ms (5.51%), Space: 44 MB (51.28%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -14,21 +14,36 @@
  * }
  */
 class Solution {
-  public void dfs(TreeNode node, int runningSum, List<Integer> runningVals, int targetSum, List<List<Integer>> ans) {
-    if (node == null) return;
-    List<Integer> next = new ArrayList<Integer>();
-    next.addAll(runningVals);
-    next.add(node.val);
-    if (node.left == null && node.right == null && runningSum + node.val == targetSum) {
-      ans.add(next);
-    }
-    dfs(node.left, runningSum + node.val, next, targetSum, ans);
-    dfs(node.right, runningSum + node.val, next, targetSum, ans);
-  }
-  
   public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
     List<List<Integer>> ans = new ArrayList();
-    dfs(root, 0, new ArrayList<Integer>(), targetSum, ans);
+    int runningSum = 0;
+    List<TreeNode> stack1 = new ArrayList<TreeNode>();
+    if (root != null) stack1.add(root);
+    List<TreeNode> stack2 = new ArrayList<TreeNode>();
+    TreeNode curr;
+    while (!stack1.isEmpty()) {
+      curr = stack1.remove(stack1.size() - 1);
+      stack2.add(curr);
+      runningSum += curr.val;
+      if (curr.right != null) stack1.add(curr.right);
+      if (curr.left != null) stack1.add(curr.left);
+      if (stack2.get(stack2.size() - 1).left == null && stack2.get(stack2.size() - 1).right == null) {
+        if (runningSum == targetSum) {
+          List<Integer> seq = stack2.stream()
+            .map(node -> node.val)
+            .collect(Collectors.toList());
+          ans.add(seq);
+        }
+        while (
+          !stack1.isEmpty() && 
+          !stack2.isEmpty() && 
+          stack1.get(stack1.size() - 1) != stack2.get(stack2.size() - 1).right
+        ) {
+          curr = stack2.remove(stack2.size() - 1);
+          runningSum -= curr.val;
+        }
+      }
+    }
     return ans;
   }
 }
