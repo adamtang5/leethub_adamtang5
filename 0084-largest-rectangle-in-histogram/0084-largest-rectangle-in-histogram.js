e317fcf3818eee1e47ac70121558ade4bd7202da
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-10 20:45:48 -0700

    Time: 123 ms (33.14%), Space: 58.8 MB (60.48%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+  let ans = 0;
+  const stack = [
+    {
+      idx: 0,
+      start: 0,
+      height: heights[0],
+    }
+  ];
+  let i = 1;
+  let popped;
+  while (i < heights.length) {
+    if (heights[i] <= stack.at(-1).height) {
+      popped = stack.pop();
+      ans = Math.max(ans, (popped.idx - popped.start + 1) * popped.height);
+      while (stack.length && stack.at(-1).height >= heights[i]) {
+        stack.at(-1).idx = popped.idx;
+        popped = stack.pop();
+        ans = Math.max(ans, (popped.idx - popped.start + 1) * popped.height);
+      }
+      stack.push({
+        idx: i,
+        start: popped.start,
+        height: heights[i],
+      });
+    } else {
+      stack.push({
+        idx: i,
+        start: i,
+        height: heights[i],
+      });
+    }
+    i++;
+  }
+  while (stack.length) {
+    popped = stack.pop();
+    ans = Math.max(ans, (popped.idx - popped.start + 1) * popped.height);
+    if (stack.length) stack.at(-1).idx = popped.idx;
+  }
+  return ans;
+};
