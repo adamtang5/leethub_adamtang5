14d79a7a05a9ccc7320254b8c4fdda0f3c5ed6f9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-19 13:37:13 -0700

    Time: 981 ms (6.38%), Space: 58.7 MB (68.08%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.ts
+++ 0115-distinct-subsequences/0115-distinct-subsequences.ts
@@ -5,11 +5,13 @@ function numDistinct(s: string, t: string): number {
   for (let i = 0; i < s.length; i++) {
     if (tChars.has(s[i])) newS += s[i]
   }
-  let [l, r] = [0, s.length]
-  while (newS[l] !== t[0] && l < newS.length) l++
-  while (newS[r - 1] !== t.at(-1) && r > 0) r--
+  if (!newS.length) return 0
+  let [l, r] = [0, newS.length]
+  while (l < newS.length && newS[l] !== t[0]) l++
+  while (r > 0 && newS[r - 1] !== t.at(-1)) r--
   s = newS.slice(l, r)
   if (!s.length) return 0
+  if (s === t) return 1
   
   const dp: number[][] = new Array(s.length).fill([]).map(_ => new Array(t.length).fill(0))
   for (let l = 0; l < s.length; l++) {
