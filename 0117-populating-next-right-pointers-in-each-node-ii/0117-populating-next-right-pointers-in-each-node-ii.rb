0eeefbc9ca113c2e8b3e6a249d23597f3e531341
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-26 21:51:41 -0700

    Time: 83 ms (72.97%), Space: 240.6 MB (91.89%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.rb
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.rb
@@ -9,25 +9,34 @@
 
 # @param {Node} root
 # @return {Node}
+def is_leaf(node)
+  !node.left && !node.right
+end
+
+def next_child(node)
+  !node ? nil : node.left ? node.left : node.right
+end
+
+def last_child(node)
+  !node ? nil : node.right ? node.right : node.left
+end
+
 def connect(root)
-  queue = root ? [[0, root]] : []
-  levels = []
-  while !queue.empty?
-    level, node = queue.shift
-    levels << [] if level > levels.length-1
-    levels[level] << node
-    queue << [level+1, node.left] if node.left
-    queue << [level+1, node.right] if node.right
-  end
-  curr = nil
-  while !levels.empty?
-    level = levels.shift
-    curr = nil
-    last = nil
-    while !level.empty?
-      last = level.pop
-      last.next = curr
-      curr = last
+  this_cur, this_nxt, nxt_lvl = root, nil, next_child(root)
+  while this_cur && nxt_lvl
+    this_cur.left.next = this_cur.right if this_cur.left && this_cur.right
+    if last_child(this_cur) && this_nxt
+      this_nxt = this_nxt.next while this_nxt && is_leaf(this_nxt)
+      last_child(this_cur).next = next_child(this_nxt)
+    end
+    this_cur = this_nxt
+    this_nxt = this_nxt.next if this_nxt
+    if !this_cur
+      this_cur = nxt_lvl
+      this_nxt = this_cur
+      this_nxt = this_nxt.next while this_nxt && is_leaf(this_nxt)
+      nxt_lvl = !this_nxt || is_leaf(this_nxt) ? nil : next_child(this_nxt)
+      this_nxt = this_cur.next
     end
   end
   root
