bf5b097ffdbd9fd63c6f57a6020a0a410352656d
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-26 21:44:14 -0700

    Time: 44 ms (92.11%), Space: 17.7 MB (68.50%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,24 +10,32 @@ class Node:
 
 class Solution:
   def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-    queue = [[0, root]] if root else []
-    levels = []
-    while queue:
-      level, node = queue.pop(0)
-      if level > len(levels)-1:
-        levels.append([])
-      levels[level].append(node)
-      if node.left:
-        queue.append([level+1, node.left])
-      if node.right:
-        queue.append([level+1, node.right])
-    curr = None
-    while levels:
-      level = levels.pop(0)
-      curr = None
-      last = None
-      while level:
-        last = level.pop()
-        last.next = curr
-        curr = last
+    def isLeaf(node: 'Node') -> bool:
+      return not node.left and not node.right
+    
+    def nextChild(node: 'Optional[Node]') -> 'Optional[Node]':
+      return None if not node else node.left if node.left else node.right
+    
+    def lastChild(node: 'Optional[Node]') -> 'Optional[Node]':
+      return None if not node else node.right if node.right else node.left
+    
+    thisCur, thisNxt, nxtLvl = root, None, nextChild(root)
+    
+    while thisCur and nxtLvl:
+      if thisCur.left and thisCur.right:
+        thisCur.left.next = thisCur.right
+      if lastChild(thisCur) and thisNxt:
+        while thisNxt and isLeaf(thisNxt):
+          thisNxt = thisNxt.next
+        lastChild(thisCur).next = nextChild(thisNxt)
+      thisCur = thisNxt
+      if thisNxt:
+        thisNxt = thisNxt.next
+      if not thisCur:
+        thisCur = nxtLvl
+        thisNxt = thisCur
+        while thisNxt and isLeaf(thisNxt):
+          thisNxt = thisNxt.next
+        nxtLvl = None if not thisNxt or isLeaf(thisNxt) else nextChild(thisNxt)
+        thisNxt = thisCur.next
     return root
