bb93b799c687b58e0d2d7383367de8abbba8a8fc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-30 12:18:51 -0700

    Time: 54 ms (82.00%), Space: 211 MB (90.00%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.rb
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val = 0, left = nil, right = nil)
+#     @val = val
+#     @left = left
+#     @right = right
+#   end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def dfs(node, ans)
+  return if !node
+  dfs(node.left, ans)
+  dfs(node.right, ans)
+  ans << node.val
+  nil
+end
+
+def postorder_traversal(root)
+  ans = []
+  dfs(root, ans)
+  ans
+end
