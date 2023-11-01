ed6dbe9c4f1d3710c9f94350c501fd04e857bf94
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-10-31 23:15:39 -0700

    Time: 53 ms (76.07%), Space: 42.6 MB (53.40%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+  let ans = Math.max(...nums);
+  let [currMin, currMax] = [1, 1];
+
+  nums.forEach(num => {
+    let temp = currMax;
+    currMax = Math.max(currMin * num, temp * num, num);
+    currMin = Math.min(currMin * num, temp * num, num);
+    ans = Math.max(currMax, currMin, ans);
+  });
+
+  return ans;
+};
