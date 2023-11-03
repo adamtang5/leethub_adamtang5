f12d218c2d9cc439680c07e2b7d84000120abaca
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-03 16:09:24 -0700

    Time: 82 ms (35.91%), Space: 50.5 MB (25.37%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,16 +12,27 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-  const nodes = new Set();
-  let curr = headA;
-  while (curr) {
-    nodes.add(curr);
-    curr = curr.next;
+  let [lenA, lenB] = [0, 0];
+  let [currA, currB] = [headA, headB];
+  while (currA) {
+    lenA++;
+    currA = currA.next;
   }
-  curr = headB;
-  while (curr) {
-    if (nodes.has(curr)) return curr;
-    curr = curr.next;
+  while (currB) {
+    lenB++;
+    currB = currB.next;
   }
-  return null;
+  console.log(lenA, lenB);
+  [currA, currB] = [headA, headB];
+  if (lenA > lenB) {
+    for (let i = lenA - lenB; i > 0; i--) currA = currA.next;
+  } else if (lenB > lenA) {
+    for (let i = lenB - lenA; i > 0; i--) currB = currB.next;
+  }
+  while (currA) {
+    if (currA === currB) break;
+    currA = currA.next;
+    currB = currB.next;
+  }
+  return currA;
 };
