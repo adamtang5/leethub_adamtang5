5975847c5f36de1c26c55d0c914c78ed1f3de6c7
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-02 18:46:20 -0700

    Time: 83 ms (32.72%), Space: 212.6 MB (19.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.rb
+++ 0169-majority-element/0169-majority-element.rb
@@ -2,8 +2,8 @@
 # @return {Integer}
 def majority_element(nums)
   tally = Hash.new(0)
-  nums.each{ |num| tally[num] += 1 }
-  tally.each do |num, count|
-    return num if count >= nums.length / 2.0
+  nums.each do |num|
+    tally[num] += 1
+    return num if tally[num] >= nums.length / 2.0
   end
 end
