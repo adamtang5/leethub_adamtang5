fe8544cd8edd942d715eab0fdf35de008c4e3da0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-02 18:44:50 -0700

    Time: 151 ms (55.91%), Space: 17.8 MB (37.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 class Solution:
   def majorityElement(self, nums: List[int]) -> int:
     tally = defaultdict(int)
+    threshold = len(nums)/2
     for num in nums:
       tally[num] += 1
-    for num in tally:
-      if tally[num] >= len(nums)/2:
+      if tally[num] >= threshold:
         return num
