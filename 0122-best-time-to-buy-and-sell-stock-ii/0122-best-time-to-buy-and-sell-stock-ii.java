6012a9726b9b09a435e138c141b7b8cc47a454d2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-06 14:08:21 -0800

    Time: 1 ms (85.91%), Space: 44.4 MB (28.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,8 @@
 class Solution {
   public int maxProfit(int[] prices) {
     int ans = 0;
-    int l = 0;
-    int r = 1;
-    while (r < prices.length) {
-      if (prices[r] >= prices[l]) {
-        while (r < prices.length - 1 && prices[r + 1] >= prices[r]) r++;
-        ans += prices[r]-prices[l];
-      }
-      l = r;
-      r++;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] > prices[i - 1]) ans += prices[i] - prices[i - 1];
     }
     return ans;
   }
