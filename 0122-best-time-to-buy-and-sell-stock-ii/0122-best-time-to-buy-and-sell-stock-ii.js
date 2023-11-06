63a32d1b32c75358ab4f10e032b2d5e326696d1b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-06 14:01:36 -0800

    Time: 60 ms (26.23%), Space: 42.5 MB (30.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,14 +3,9 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let [ans, l, r] = [0, 0, 1];
-  while (r < prices.length) {
-    if (prices[r] >= prices[l]) {
-      while (r < prices.length - 1 && prices[r + 1] >= prices[r]) r++;
-      ans += prices[r] - prices[l];
-    }
-    l = r;
-    r++;
+  let ans = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) ans += prices[i] - prices[i - 1];
   }
   return ans;
 };
