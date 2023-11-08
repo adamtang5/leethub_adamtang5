ba037b9ad160385c9c1b6e5d15cce3e35eb26077
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-07 21:29:00 -0800

    Time: 57 ms (5.24%), Space: 43 MB (7.58%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,14 @@
+public class Solution {
+  // you need treat n as an unsigned value
+  public int reverseBits(int n) {
+    int rev = 0;
+    for (int i = 0; i < 32; i++) {
+      System.out.println(n);
+      System.out.println(rev);
+      rev <<= 1;
+      if ((int) (n & 1) == 1) rev ^= 1;
+      n >>= 1;
+    }
+    return rev;
+  }
+}
