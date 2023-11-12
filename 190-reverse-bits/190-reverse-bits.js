b9bdd87c31e4b589c7fad3e96b67ad32ac1abe0e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-11 23:04:58 -0800

    Delete 190-reverse-bits directory

--- 190-reverse-bits/190-reverse-bits.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} n - a positive integer
- * @return {number} - a positive integer
- */
-var reverseBits = function(n) {
-    const reversedArr = n.toString(2).split('').reverse();
-    const right = new Array(32 - reversedArr.length).fill('0');
-    return parseInt([...reversedArr, ...right].join(''), 2);
-};
--- 190-reverse-bits/190-reverse-bits.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        revStr = format(n, 'b')[::-1]
-        right = '0' * (32-len(revStr))
-        return int(revStr+right, 2)
-        
