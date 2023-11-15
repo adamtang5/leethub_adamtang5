303d5048c4111fbf8e4b1063f30a08852cd81224
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-14 23:27:17 -0800

    Time: 712 ms (98.30%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,3 @@
-/* Write your T-SQL query statement below */
-SELECT email AS Email FROM Person
+/* Write your PL/SQL query statement below */
+SELECT email AS "Email" FROM Person
 GROUP BY email HAVING COUNT(email) > 1;
