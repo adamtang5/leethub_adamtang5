05ed12c0005bd57a6a07ec4880cfcc2588043012
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-16 22:57:46 -0800

    Time: 148 ms (13.45%), Space: 65.2 MB (10.30%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -1,4 +1,5 @@
 function numIslands(grid: string[][]): number {
+  if (!grid.length) return 0
   let ans = 0
   let visited: Set<string> = new Set()
   const dirs: number[][] = [[0, 1], [1, 0], [0, -1], [-1, 0]]
@@ -11,21 +12,27 @@ function numIslands(grid: string[][]): number {
     return inBounds(row, col) && !visited.has(`${row}-${col}`) && grid[row][col] === '1'
   }
   
-  function dfs(row: number, col: number): number {
-    if (!valid(row, col)) return 0
-    
+  function bfs(row: number, col: number): void {
+    const queue: number[][] = [[row, col]]
     visited.add(`${row}-${col}`)
-    for (let [rowDiff, colDiff] of dirs) {
-      const newRow = row + rowDiff
-      const newCol = col + colDiff
-      dfs(newRow, newCol)
+    while (queue.length) {
+      const [currRow, currCol] = queue.shift()
+      dirs.forEach(([rowDiff, colDiff]) => {
+        const [newRow, newCol] = [currRow + rowDiff, currCol + colDiff]
+        if (valid(newRow, newCol)) {
+          queue.push([newRow, newCol])
+          visited.add(`${newRow}-${newCol}`)
+        }
+      })
     }
-    return 1
   }
   
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[0].length; col++) {
-      if (!visited.has(`${row}-${col}`)) ans += dfs(row, col)
+      if (valid(row, col)) {
+        bfs(row, col)
+        ans++
+      }
     }
   }
   return ans
