b5f5dc50bdcf377e85c935cb4fda12d050db7cbd
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-16 21:37:26 -0800

    Time: 145 ms (13.97%), Space: 64.7 MB (12.19%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -0,0 +1,32 @@
+function numIslands(grid: string[][]): number {
+  let ans = 0
+  let visited: Set<string> = new Set()
+  
+  function inBounds(row: number, col: number): boolean {
+    return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length
+  }
+  
+  function valid(row: number, col: number): boolean {
+    return inBounds(row, col) && !visited.has(`${row}-${col}`) && grid[row][col] === '1'
+  }
+  
+  function dfs(row: number, col: number): number {
+    if (!valid(row, col)) return 0
+    
+    visited.add(`${row}-${col}`)
+    const dirs: number[][] = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    for (let [rowDiff, colDiff] of dirs) {
+      const newRow = row + rowDiff
+      const newCol = col + colDiff
+      dfs(newRow, newCol)
+    }
+    return 1
+  }
+  
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (!visited.has(`${row}-${col}`)) ans += dfs(row, col)
+    }
+  }
+  return ans
+}
