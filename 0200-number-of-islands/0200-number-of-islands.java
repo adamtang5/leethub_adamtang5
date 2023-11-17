9718829024116fd75c3f58d5c3e406f6cad3bada
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-16 23:21:48 -0800

    Time: 74 ms (5.05%), Space: 54.2 MB (5.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -8,18 +8,37 @@ class Solution {
     return inBounds(row, col, grid) && !visited.contains(key) && grid[row][col] == '1';
   }
   
-  public int dfs(int row, int col, char[][] grid, Set<String> visited, int[][] dirs) {
-    if (!valid(row, col, grid, visited)) return 0;
+  public void bfs(int row, int col, char[][] grid, Set<String> visited, int[][] dirs) {
+    List<List<Integer>> q = new ArrayList();
+    List<Integer> init = new ArrayList<Integer>();
+    init.add(row);
+    init.add(col);
+    q.add(init);
     
     String key = String.valueOf(row) + "-" + String.valueOf(col);
     visited.add(key);
-    for (int i = 0; i < dirs.length; i++) {
-      dfs(row + dirs[i][0], col + dirs[i][1], grid, visited, dirs);
+    while (!q.isEmpty()) {
+      List<Integer> first = q.remove(0);
+      for (int i = 0; i < dirs.length; i++) {
+        int newRow = first.get(0) + dirs[i][0];
+        int newCol = first.get(1) + dirs[i][1];
+        if (valid(newRow, newCol, grid, visited)) {
+          List<Integer> next = new ArrayList<Integer>();
+          next.add(newRow);
+          next.add(newCol);
+          q.add(next);
+          String nextKey = String.valueOf(newRow) + "-" + String.valueOf(newCol);
+          visited.add(nextKey);
+        }
+      }
     }
-    return 1;
   }
   
   public int numIslands(char[][] grid) {
+    if (grid.length == 0) return 0;
+    
+    int ans = 0;
+    Set<String> visited = new HashSet<String>();
     int[][] dirs = new int[4][2];
     dirs[0][0] = 0;
     dirs[0][1] = 1;
@@ -30,12 +49,12 @@ class Solution {
     dirs[3][0] = -1;
     dirs[3][1] = 0;
 
-    int ans = 0;
-    Set<String> visited = new HashSet<String>();
     for (int row = 0; row < grid.length; row++) {
       for (int col = 0; col < grid[0].length; col++) {
-        String key = String.valueOf(row) + "-" + String.valueOf(col);
-        if (!visited.contains(key)) ans += dfs(row, col, grid, visited, dirs);
+        if (valid(row, col, grid, visited)) {
+          bfs(row, col, grid, visited, dirs);
+          ans++;
+        }
       }
     }
     return ans;
