ab3fbd4ed59684c380bad49a8d91690a67380810
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-16 22:44:06 -0800

    Time: 450 ms (5.47%), Space: 223.4 MB (22.40%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.rb
+++ 0200-number-of-islands/0200-number-of-islands.rb
@@ -8,20 +8,20 @@ def valid(row, col, grid, visited)
   in_bounds(row, col, grid) && !visited.include?([row, col]) && grid[row][col] == '1'
 end
 
-def dfs(row, col, grid, visited)
+def dfs(row, col, grid, visited, dirs)
   return 0 if !valid(row, col, grid, visited)
   visited.add([row, col])
-  dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-  dirs.each{ |row_diff, col_diff| dfs(row+row_diff, col+col_diff, grid, visited) }
+  dirs.each{ |row_diff, col_diff| dfs(row+row_diff, col+col_diff, grid, visited, dirs) }
   1
 end
 
 def num_islands(grid)
   return 0 if grid.length == 0
   ans, visited = 0, Set.new
+  dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
   (0...grid.length).each do |row|
     (0...grid[row].length).each do |col|
-      ans += dfs(row, col, grid, visited) if !visited.include?([row, col])
+      ans += dfs(row, col, grid, visited, dirs) if !visited.include?([row, col])
     end
   end
   ans
