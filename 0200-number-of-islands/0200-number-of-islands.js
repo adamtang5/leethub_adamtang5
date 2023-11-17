e0d2f425f4f740200f59c7cc0fdcfc02c596f300
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-16 22:34:39 -0800

    Time: 147 ms (8.62%), Space: 65.2 MB (5.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,8 +3,10 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+  if (!grid.length) return 0;
   let ans = 0;
   let visited = new Set();
+  const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
 
   const inBounds = (row, col) => {
     return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
@@ -14,21 +16,27 @@ var numIslands = function(grid) {
     return inBounds(row, col) && !visited.has(`${row}-${col}`) && grid[row][col] === "1";
   };
 
-  const dfs = (row, col) => {
-    if (!valid(row, col)) return 0;
-
+  const bfs = (row, col, dirs) => {
+    const queue = [[row, col]];
     visited.add(`${row}-${col}`);
-    const fundDirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    for (let [rowDiff, colDiff] of fundDirs) {
-      const [newRow, newCol] = [row + rowDiff, col + colDiff];
-      dfs(newRow, newCol);
+    while (queue.length) {
+      const [currRow, currCol] = queue.shift();
+      dirs.forEach(([rowDiff, colDiff]) => {
+      const [newRow, newCol] = [currRow + rowDiff, currCol + colDiff];
+        if (valid(newRow, newCol)) {
+          queue.push([newRow, newCol]);
+          visited.add(`${newRow}-${newCol}`);
+        }
+      });
     }
-    return 1;
   };
 
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[row].length; col++) {
-      if (!visited.has(`${row}-${col}`)) ans += dfs(row, col);
+      if (valid(row, col)) {
+        bfs(row, col, dirs);
+        ans++;
+      }
     }
   }
   return ans;
