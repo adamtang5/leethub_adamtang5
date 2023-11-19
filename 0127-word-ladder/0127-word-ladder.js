fe1e1c5a3972f22eb26a1e4d980412f9d86645e7
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-19 00:18:28 -0800

    Time: 1311 ms (10.82%), Space: 52.9 MB (28.65%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+  const distance = (word1, word2) => {
+    let ans = 0;
+    for (let i = 0; i < word1.length; i++) {
+      if (word1[i] !== word2[i]) ans++;
+    }
+    return ans;
+  };
+  
+  const wordSet = new Set(wordList);
+  const adjList = {};
+  adjList[beginWord] = new Set();
+  const queue = [beginWord];
+  const visited = new Set();
+  let first;
+  while (queue.length) {
+    first = queue.shift();
+    visited.add(first);
+    wordList.forEach(word => {
+      if (distance(first, word) === 1) {
+        if (!visited.has(word) && !queue.includes(word)) queue.push(word);
+        adjList[first] = adjList[first] || new Set();
+        adjList[word] = adjList[word] || new Set();
+        adjList[first].add(word);
+        adjList[word].add(first);
+      }
+    });
+  }
+  // console.log(adjList);
+  if (!adjList[endWord]) return 0;
+  
+  const pathQ = [[beginWord]];
+  visited.clear();
+  visited.add(beginWord);
+  let currPath, currNode;
+  while (pathQ.length) {
+    currPath = pathQ.shift();
+    currNode = currPath.at(-1);
+    if (currNode === endWord) return currPath.length;
+    adjList[currNode].forEach(nb => {
+      if (!visited.has(nb)) {
+        visited.add(nb);
+        pathQ.push([...currPath, nb]);
+      }
+    });
+  }
+};
