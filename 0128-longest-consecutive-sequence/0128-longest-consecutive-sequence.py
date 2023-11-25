8365fdcfb7a6adb870a522d7c98ee203a2fd8ab9
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-24 23:38:00 -0800

    Time: 574 ms (38.84%), Space: 55.9 MB (5.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,36 +1,32 @@
 class Solution:
   def longestConsecutive(self, nums: List[int]) -> int:
-    tally, ans = dict(), 0
+    edge, occurred, ans = set(), dict(), 0
     for num in nums:
-      if not tally.get(num):
-        if (not tally.get(num+1) or tally.get(num+1)['state'] == 'Neighbor') and \
-           (not tally.get(num-1) or tally.get(num-1)['state'] == 'Neighbor'):
-          tally[num] = {
-            'state': 'Present',
-            'range': {
-              'lb': num,
-              'ub': num,
-            }
+      if num not in edge and not occurred.get(num):
+        if not occurred.get(num+1) and not occurred.get(num-1):
+          occurred[num] = {
+            'lb': num,
+            'ub': num,
           }
           ans = max(ans, 1)
-          tally[num+1] = { 'state': 'Neighbor' }
-          tally[num-1] = { 'state': 'Neighbor' }
-      elif tally[num] and tally[num]['state'] == 'Neighbor':
-        tally[num+1] = tally.get(num+1, { 'state': 'Neighbor' })
-        tally[num-1] = tally.get(num-1, { 'state': 'Neighbor' })
+          edge.add(num+1)
+          edge.add(num-1)
+      elif num in edge:
+        if not occurred.get(num+1):
+          edge.add(num+1)
+        if not occurred.get(num-1):
+          edge.add(num-1)
         lMin = rMax = num
-        if tally[num+1] and tally[num+1]['state'] == 'Present':
-          rMax = tally[num+1]['range']['ub']
-        if tally[num-1] and tally[num-1]['state'] == 'Present':
-          lMin = tally[num-1]['range']['lb']
-        newNumberState = {
-          'state': 'Present',
-          'range': {
-            'lb': lMin,
-            'ub': rMax,
-          }
+        if occurred.get(num+1):
+          rMax = occurred[num+1]['ub']
+        if occurred.get(num-1):
+          lMin = occurred[num-1]['lb']
+        newState = {
+          'lb': lMin,
+          'ub': rMax,
         }
-        tally[num] = tally[lMin] = tally[rMax] = newNumberState
+        occurred[num] = occurred[lMin] = occurred[rMax] = newState
+        edge.remove(num)
         ans = max(ans, rMax-lMin+1)
     return ans
     
