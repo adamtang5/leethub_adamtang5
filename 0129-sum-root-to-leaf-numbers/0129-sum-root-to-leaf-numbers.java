8f4017ae005f44c1098077d2460e0d639e4f5eb2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-25 21:49:43 -0800

    Time: 0 ms (100.00%), Space: 40.5 MB (13.19%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,20 +14,16 @@
  * }
  */
 class Solution {
-  public void dfs(TreeNode node, int curr, List<Integer> ans) {
-    if (node.left == null && node.right == null) {
-      int temp = ans.get(0);
-      temp += curr + node.val;
-      ans.set(0, temp);
-    }
+  public void dfs(TreeNode node, int curr, int[] ans) {
+    if (node.left == null && node.right == null) ans[0] += curr + node.val;
     if (node.left != null) dfs(node.left, 10 * (curr + node.val), ans);
     if (node.right != null) dfs(node.right, 10 * (curr + node.val), ans);
   }
   
   public int sumNumbers(TreeNode root) {
-    List<Integer> ans = new ArrayList<Integer>();
-    ans.add(0);
+    int[] ans = new int[1];
+    ans[0] = 0;
     dfs(root, 0, ans);
-    return ans.get(0);
+    return ans[0];
   }
 }
