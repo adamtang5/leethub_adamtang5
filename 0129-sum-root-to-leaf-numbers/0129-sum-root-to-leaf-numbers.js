ef55eaaaa657ca9c6ecbad720198db6a5d7519e5
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-25 21:02:30 -0800

    Time: 51 ms (63.48%), Space: 42.4 MB (52.67%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -1,9 +1,9 @@
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.left = (left===undefined ? null : left)
+ *   this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -14,7 +14,6 @@ var sumNumbers = function(root) {
   let ans = 0;
   
   const dfs = (node, curr) => {
-    console.log(node.val, curr);
     if (!node.left && !node.right) ans += curr + node.val;
     if (node.left) dfs(node.left, 10 * (curr + node.val));
     if (node.right) dfs(node.right, 10 * (curr + node.val));
