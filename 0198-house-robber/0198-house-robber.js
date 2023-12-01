6ded5cd3866248a44207ebce053747ef293560f2
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-11-30 21:23:58 -0800

    Time: 56 ms (25.21%), Space: 42.3 MB (10.48%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  const dp = new Array(nums.length).fill(0);
+  dp[dp.length - 1] = nums.at(-1);
+  if (nums.length > 1) dp[dp.length - 2] = Math.max(nums.at(-2), nums.at(-1));
+  for (let i = nums.length - 3; i >= 0; i--) {
+    dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+  }
+  return dp[0];
+};
