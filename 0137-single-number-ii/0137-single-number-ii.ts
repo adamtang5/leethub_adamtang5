171689c2d42670a13f8e179dcd4fd4cd752b7b0e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-12-10 00:10:08 -0800

    Time: 51 ms (90.84%), Space: 42.4 MB (100.00%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.ts
+++ 0137-single-number-ii/0137-single-number-ii.ts
@@ -2,8 +2,8 @@ function singleNumber(nums: number[]): number {
   let ones = 0
   let twos = 0
   nums.forEach(num => {
-    ones = (ones ^ num) & (~twos)
-    twos = (twos ^ num) & (~ones)
+    ones = ones ^ num & ~twos
+    twos = twos ^ num & ~ones
   })
   return ones
 }
