7103956ce230cea1b47d36a794f03f5696072393
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2023-12-27 20:57:22 -0800

    Time: 112 ms (5.00%), Space: 44.8 MB (78.24%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.ts
@@ -0,0 +1,44 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *   val: number
+ *   next: Node | null
+ *   random: Node | null
+ *   constructor(val?: number, next?: Node, random?: Node) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ *     this.random = (random===undefined ? null : random)
+ *   }
+ * }
+ */
+
+function copyRandomList(head: Node | null): Node | null {
+  if (!head) return head
+  
+  const orig: Node[] = []
+  let curr: Node | null = head
+  while (curr) {
+    orig.push(curr)
+    curr = curr.next
+  }
+  const randoms: number[] = new Array(orig.length).fill(0)
+  for (let i = 0; i < orig.length; i++) {
+    randoms[i] = orig.indexOf(orig[i].random)
+  }
+  
+  const ans: Node = new Node(head.val)
+  const ansArr: Node[] = []
+  curr = ans
+  for (let i = 1; i < orig.length; i++) {
+    ansArr.push(curr)
+    curr.next = new Node(orig[i].val)
+    curr = curr.next
+  }
+  ansArr.push(curr)
+  
+  randoms.forEach((r, i) => {
+    if (r >= 0) ansArr[i].random = ansArr[r]
+  })
+  
+  return ans
+}
