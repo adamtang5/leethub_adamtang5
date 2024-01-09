d326290eba8f3a16ef1d33772471dd8f473bd564
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-01-08 17:02:17 -0800

    Time: 83 ms (43.31%), Space: 53.2 MB (5.16%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,21 +10,27 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-  const stack = [];
-  let curr = head;
-  while (curr) {
-    stack.push(curr);
-    curr = curr.next;
+  let [slow, fast] = [head, head.next];
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
-  curr = head;
-  stack.shift();
-  while (stack.length) {
-    curr.next = stack.pop();
-    curr = curr.next;
-    if (stack.length) {
-      curr.next = stack.shift();
-      curr = curr.next;
-    }
+  let second = slow.next;
+  slow.next = null;
+  let prev, nextNode;
+  while (second) {
+    nextNode = second.next;
+    second.next = prev;
+    prev = second;
+    second = nextNode;
   }
-  curr.next = null;
+  let first = head;
+  second = prev;
+  let next1, next2;
+  while (second) {
+    [next1, next2] = [first.next, second.next];
+    first.next = second;
+    second.next = next1;
+    [first, second] = [next1, next2];
+  }
 };
