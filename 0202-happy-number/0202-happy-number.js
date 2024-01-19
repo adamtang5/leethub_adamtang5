267a5192c6480466ea1ded3aafecad9b8602b7dc
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-01-18 21:39:45 -0800

    Time: 64 ms (34.93%), Space: 44.2 MB (40.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  const reduce = num => {
+    let [ans, d] = [0, 0];
+    while (num > 0) {
+      d = num % 10;
+      ans += d * d;
+      num = Math.floor(num / 10);
+    }
+    return ans;
+  };
+  
+  let iter = 100;
+  while (n !== 1 && iter > 0) {
+    n = reduce(n);
+    iter--;
+  }
+  return n === 1;
+};
