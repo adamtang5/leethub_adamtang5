baeb0c865cadeecc1ea8c12f6af41b4450746c3b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-02 14:12:02 -0800

    Time: 183 ms (19.52%), Space: 71.1 MB (50.00%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+  if (!head || !head.next) return head;
+  let [slow, fast] = [head, head];
+  while (fast && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  let [prev1, prev2] = [new ListNode(), new ListNode()];
+  prev1.next = head;
+  prev2.next = slow.next;
+  slow.next = null;
+  
+  prev1.next = sortList(prev1.next);
+  prev2.next = sortList(prev2.next);
+
+  const merge = (head1, head2) => {
+    let [prev, curr1, curr2] = [new ListNode(), head1, head2];
+    let curr = prev;
+    while (curr1 && curr2) {
+      if (curr1.val <= curr2.val) {
+        curr.next = curr1;
+        curr1 = curr1.next;
+      } else {
+        curr.next = curr2;
+        curr2 = curr2.next;
+      }
+      curr = curr.next;
+    }
+    if (curr1) {
+      curr.next = curr1;
+    } else {
+      curr.next = curr2;
+    }
+    return prev.next;
+  };
+  
+  return merge(prev1.next, prev2.next);
+};
