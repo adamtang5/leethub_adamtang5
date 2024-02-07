4c4c2de7c260d14f784f60d762b7f8a13a2ef468
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-07 13:40:07 -0800

    Time: 354 ms (34.22%), Space: 112.8 MB (39.42%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  if (n < 3) return 0;
+  const comps = new Array(n).fill(false);
+  for (let m = 2; m <= Math.floor(Math.sqrt(n)); m++) {
+    if (!comps[m]) {
+      for (let t = m * m; t < n; t += m) {
+        comps[t] = true;
+      }
+    }
+  }
+  return comps.filter(x => !x).length - 2;
+};
