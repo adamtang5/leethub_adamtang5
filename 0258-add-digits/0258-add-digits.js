b8e8782890b6383b57168851e704712ca32a77e8
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-22 21:12:33 -0800

    Time: 77 ms (45.21%), Space: 53.2 MB (38.04%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,15 +3,8 @@
  * @return {number}
  */
 var addDigits = function(num) {
-  const reduce = n => {
-    let ans = 0;
-    while (n > 0) {
-      ans += n % 10;
-      n = Math.floor(n / 10);
-    }
-    return ans;
-  };
-  
-  while (num >= 10) num = reduce(num);
+  if (num === 0) return 0;
+  num %= 9;
+  if (num === 0) return 9;
   return num;
 };
