3bdf7bc2ef723062d060def5b5c8bd170d406004
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-22 21:15:20 -0800

    Time: 41 ms (30.45%), Space: 16.6 MB (73.23%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,6 @@
 class Solution:
   def addDigits(self, num: int) -> int:
-    def reduce(n: int) -> int:
-      ans = 0
-      while n > 0:
-        ans += n%10
-        n //= 10
-      return ans
-    
-    while num >= 10:
-      num = reduce(num)
+    if num == 0: return num
+    num %= 9
+    if num == 0: return 9
     return num
