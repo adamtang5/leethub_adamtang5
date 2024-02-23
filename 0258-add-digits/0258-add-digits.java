7a053f11964e0a7508f936b2fb03da6f2154148c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-22 21:17:47 -0800

    Time: 1 ms (52.52%), Space: 40.3 MB (98.60%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,15 +1,8 @@
 class Solution {
-  public int reduce(int n) {
-    int ans = 0;
-    while (n > 0) {
-      ans += n % 10;
-      n /= 10;
-    }
-    return ans;
-  }
-  
   public int addDigits(int num) {
-    while (num >= 10) num = reduce(num);
+    if (num == 0) return 0;
+    num %= 9;
+    if (num == 0) return 9;
     return num;
   }
 }
