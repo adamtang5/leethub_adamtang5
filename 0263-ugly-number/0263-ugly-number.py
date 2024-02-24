d431cee042037eb243c09e26816937bfc6f8cc20
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-23 21:44:40 -0800

    Time: 32 ms (83.25%), Space: 16.6 MB (53.54%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -2,8 +2,7 @@ class Solution:
   def isUgly(self, n: int) -> bool:
     if n <= 0: return False
     if n < 7: return True
-    factors = [2, 3, 5]
-    for f in factors:
+    for f in [2, 3, 5]:
       while n%f == 0:
         n //= f
     return n == 1
