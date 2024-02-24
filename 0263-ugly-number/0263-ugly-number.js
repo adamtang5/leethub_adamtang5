355557c677bd694e1370b126300e1bceeac89e28
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-02-23 21:42:55 -0800

    Time: 59 ms (84.77%), Space: 52.6 MB (18.52%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -5,8 +5,7 @@
 var isUgly = function(n) {
   if (n <= 0) return false;
   if (n < 7) return true;
-  const factors = [2, 3, 5];
-  for (const f of factors) {
+  for (const f of [2, 3, 5]) {
     while (n % f === 0) n /= f;
   }
   return n === 1;
