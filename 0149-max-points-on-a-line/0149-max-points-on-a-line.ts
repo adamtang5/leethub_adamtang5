7b67df81e6cc840e664c7e34b2404977321529a3
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-01 10:56:00 -0800

    Time: 206 ms (6.38%), Space: 77.3 MB (6.38%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.ts
@@ -0,0 +1,27 @@
+function maxPoints(points: number[][]): number {
+  if (points.length < 3) return points.length
+  
+  const groups = {}
+  function group(pointA: number[], pointB: number[]): void {
+    const slope = ((pointB[1] - pointA[1]) / (pointB[0] - pointA[0]))
+    const invSlope = ((pointB[0] - pointA[0]) / (pointB[1] - pointA[1]))
+    const yInt = invSlope === 0 ? Infinity : pointA[1] - slope * pointA[0]
+    const xInt = slope === 0 ? Infinity : pointA[0] - invSlope * pointA[1]
+    const key = `${slope},${xInt},${yInt}`
+    groups[key] ||= new Set()
+    groups[key].add(`${pointA[0]},${pointA[1]}`)
+    groups[key].add(`${pointB[0]},${pointB[1]}`)
+  }
+
+  for (let i = 0; i < points.length - 1; i++) {
+    for (let j = i + 1; j < points.length; j++) {
+      group(points[i], points[j])
+    }
+  }
+
+  let ans = 0
+  for (const key in groups) {
+    ans = Math.max(ans, groups[key].size)
+  }
+  return ans
+}
