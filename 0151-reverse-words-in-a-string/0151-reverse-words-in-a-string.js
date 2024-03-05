305613f6394c932e2199f2dc0dd4fe635c551056
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-05 10:28:45 -0800

    Time: 64 ms (16.43%), Space: 50.4 MB (66.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,25 +3,6 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-  let ans = "";
-  let curr = "";
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === " ") {
-      if (curr.length && ans.length) {
-        ans = curr + " " + ans;
-      } else if (curr.length && !ans.length) {
-        ans = curr;
-      }
-      curr = "";
-    } else {
-      curr += s[i];
-    }
-  }
-  if (curr.length && ans.length) {
-    ans = curr + " " + ans;
-  } else if (curr.length && !ans.length) {
-    ans = curr;
-  }
-  
-  return ans;
+  const words = s.split(" ").filter(word => word.length > 0);
+  return words.reverse().join(" ");
 };
