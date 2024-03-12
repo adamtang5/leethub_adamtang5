68fa802f263aa7d39b2a823dd165e78b7003b24e
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-11 23:17:05 -0700

    Time: 217 ms (29.11%), Space: 54.6 MB (50.06%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.ts
+++ 0279-perfect-squares/0279-perfect-squares.ts
@@ -1,26 +1,16 @@
 function numSquares(n: number): number {
   const squares: number[] = []
-  const dp = {}
-  let sq: number
-  for (let b = 1; b <= Math.floor(Math.sqrt(n)); b++) {
-    sq = b * b
-    squares.unshift(sq)
-    dp[sq] = 1
+  let b = 1
+  while (b * b <= n) {
+    squares.push(b * b)
+    b++
   }
-  
-  function populate(num: number): void {
-    if (num in dp) return
-    let val = num
-    squares.forEach(sq => {
-      if (sq <= num) {
-        if (num - sq in dp) {
-          val = Math.min(val, dp[sq] + dp[num - sq])
-          dp[num] = val
-        }
-      }
-    })
+  const dp: number[] = new Array(n + 1).fill(Infinity)
+  dp[0] = 0
+  for (let i = 1; i <= n; i++) {
+    for (const sq of squares) {
+      if (i >= sq) dp[i] = Math.min(dp[i], dp[i - sq] + 1)
+    }
   }
-  
-  for (let b = 1; b <= n; b++) populate(b)
   return dp[n]
 }
