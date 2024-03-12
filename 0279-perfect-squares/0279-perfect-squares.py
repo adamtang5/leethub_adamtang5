7b0c5c42ad3900b97ee67fbcae5e2091817484eb
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-11 23:19:50 -0700

    Time: 2403 ms (53.76%), Space: 16.5 MB (86.09%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,21 +1,13 @@
 class Solution:
   def numSquares(self, n: int) -> int:
-    squares, dp, sq = list(), dict(), -1
-    for b in range(1, math.floor(math.sqrt(n))+1):
-      sq = b*b
-      squares.insert(0, sq)
-      dp[sq] = 1
-    
-    def populate(num: int) -> None:
-      if num in dp: return
-      val = num
+    squares, b = list(), 1
+    while b*b <= n:
+      squares.append(b*b)
+      b += 1
+    dp = [float('inf')]*(n+1)
+    dp[0] = 0
+    for i in range(1, n+1):
       for sq in squares:
-        if sq <= num:
-          if num-sq in dp:
-            val = min(val, dp[sq]+dp[num-sq])
-            dp[num] = val
-    
-    for b in range(1, n+1):
-      populate(b)
-      
+        if i >= sq:
+          dp[i] = min(dp[i], dp[i-sq]+1)
     return dp[n]
