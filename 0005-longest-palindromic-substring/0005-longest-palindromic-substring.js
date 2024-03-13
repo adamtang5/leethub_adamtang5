6fffdb8c8228811f70a34d0554a23e4029470fb0
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 13:59:45 -0700

    Time: 134 ms (34.47%), Space: 52.7 MB (52.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -5,31 +5,21 @@
 var longestPalindrome = function(s) {
   if (s.length < 2) return s;
   
-  let [maxLength, maxSub] = [1, s[0]];
-  let l, r;
+  s = `$${s}@`;
+  s = s.split('').join('#');
+  const pExt = new Array(s.length).fill(0);
   
-  for (let i = 0; i < s.length; i++) {
-    // odd
-    [l, r] = [i, i];
-    while (l >= 0 && r < s.length && s[l] === s[r]) {
-      if (r - l + 1 > maxLength) {
-        maxLength = r - l + 1;
-        maxSub = s.slice(l, r + 1);
-      }
-      l--;
-      r++;
-    }
-    
-    // even
-    [l, r] = [i, i + 1];
-    while (l >= 0 && r < s.length && s[l] === s[r]) {
-      if (r - l + 1 > maxLength) {
-        maxLength = r - l + 1;
-        maxSub = s.slice(l, r + 1);
-      }
-      l--;
-      r++;
+  let [ctr, r, mir] = [0, 0, 0];
+  for (let i = 0; i < s.length - 1; i++) {
+    mir = 2 * ctr - i;
+    if (i < r) pExt[i] = Math.min(r - i, pExt[mir]);
+    while (s[i - 1 -  pExt[i]] === s[i + 1 + pExt[i]]) pExt[i]++;
+    if (pExt[i] + 1 > r) {
+      ctr = i;
+      r = pExt[i] + 1;
     }
   }
-  return maxSub;
+  const maxExt = Math.max(...pExt);
+  ctr = pExt.indexOf(maxExt);
+  return s.slice(ctr - maxExt, ctr + maxExt + 1).replaceAll('#', '');
 };
