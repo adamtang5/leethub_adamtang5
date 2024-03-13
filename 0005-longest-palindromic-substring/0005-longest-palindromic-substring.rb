dfb543029eecfc997b91d47dfa6e499588349440
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 15:45:42 -0700

    Time: 1008 ms (21.21%), Space: 212 MB (31.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.rb
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.rb
@@ -2,29 +2,19 @@
 # @return {String}
 def longest_palindrome(s)
   return s if s.length < 2
-  max_len, max_sub = 1, s[0]
-  (0...s.length).each do |i|
-    # odd
-    l = r = i
-    while l >= 0 && r < s.length && s[l] == s[r]
-      if r-l+1 > max_len
-        max_len = r-l+1
-        max_sub = s[l..r]
-      end
-      l -= 1
-      r += 1
-    end
-
-    # even
-    l, r = i, i+1
-    while l >= 0 && r < s.length && s[l] == s[r]
-      if r-l+1 > max_len
-        max_len = r-l+1
-        max_sub = s[l..r]
-      end
-      l -= 1
-      r += 1
+  s = "$#{s}@".split('').join('-')
+  p_ext = [0]*s.length
+  ctr = r = mir = 0
+  (0...s.length-1).each do |i|
+    mir = 2*ctr-i
+    p_ext[i] = [r-i, p_ext[mir]].min if i < r
+    p_ext[i] += 1 while s[i-1-p_ext[i]] == s[i+1+p_ext[i]]
+    if p_ext[i]+1 > r
+      ctr = i
+      r = p_ext[i]+i
     end
   end
-  max_sub
+  max_ext = p_ext.max
+  ctr = p_ext.index(max_ext)
+  s[ctr-max_ext..ctr+max_ext].gsub('-', '')
 end
