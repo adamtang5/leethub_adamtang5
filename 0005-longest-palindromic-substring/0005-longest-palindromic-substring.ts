a8fa29639c25b005ae601c3a1ecf669bef13dcdb
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 10:05:29 -0700

    Time: 90 ms (64.72%), Space: 53.9 MB (60.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -3,11 +3,10 @@ function longestPalindrome(s: string): string {
   
   let maxLength = 1
   let maxSub: string = s[0]
-  let ub = s.length % 2 ? s.length : s.length - 1
   let l: number
   let r: number
   
-  for (let i = 0; i < ub; i++) {
+  for (let i = 0; i < s.length; i++) {
     // odd
     l = i
     r = i
