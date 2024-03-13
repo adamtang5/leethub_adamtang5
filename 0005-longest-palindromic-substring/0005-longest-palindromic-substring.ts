3851cacde4d660b38a1f4a364383190bb03b37e6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 14:34:43 -0700

    Time: 143 ms (37.86%), Space: 53.6 MB (67.70%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -1,35 +1,23 @@
 function longestPalindrome(s: string): string {
   if (s.length < 2) return s
   
-  let maxLength = 1
-  let maxSub: string = s[0]
-  let l: number
-  let r: number
+  s = `$${s}@`
+  s = s.split('').join('#')
+  const pExt: number[] = new Array(s.length).fill(0)
   
-  for (let i = 0; i < s.length; i++) {
-    // odd
-    l = i
-    r = i
-    while (l >= 0 && r < s.length && s[l] === s[r]) {
-      if (r - l + 1 > maxLength) {
-        maxLength = r - l + 1
-        maxSub = s.slice(l, r + 1)
-      }
-      l--
-      r++
-    }
-    
-    // even
-    l = i
-    r = i + 1
-    while (l >= 0 && r < s.length && s[l] === s[r]) {
-      if (r - l + 1 > maxLength) {
-        maxLength = r - l + 1
-        maxSub = s.slice(l, r + 1)
-      }
-      l--
-      r++
+  let ctr = 0
+  let r = 0
+  let mir = 0
+  for (let i = 0; i < s.length - 1; i++) {
+    mir = 2 * ctr - i
+    if (i < r) pExt[i] = Math.min(r - i, pExt[mir])
+    while (s[i - 1 - pExt[i]] === s[i + 1 + pExt[i]]) pExt[i]++;
+    if (pExt[i] + 1 > r) {
+      ctr = i
+      r = pExt[i] + 1
     }
   }
-  return maxSub
+  const maxExt: number = Math.max(...pExt)
+  ctr = pExt.indexOf(maxExt)
+  return s.slice(ctr - maxExt, ctr + maxExt + 1).replaceAll('#', '')
 };
