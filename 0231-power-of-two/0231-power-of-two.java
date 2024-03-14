e8cabcd8ad0bd41d275eb3248b75289ff3624a76
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 22:54:40 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (80.11%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,6 @@
 class Solution {
   public boolean isPowerOfTwo(int n) {
-    while (n > 1) {
-      if (n % 2 > 0) return false;
-      n /= 2;
-    }
-    return n == 1;
+    if (n <= 0) return false;
+    return (n & -n) == n;
   }
 }
