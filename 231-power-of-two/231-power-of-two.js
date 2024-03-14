3da33d32d4561ff755f478ab03ec23fb94dae485
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-03-13 22:56:14 -0700

    Delete 231-power-of-two directory

--- 231-power-of-two/231-power-of-two.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfTwo = function(n) {
-    if (n <= 0) return false;
-    return (n & (-n >>> 0)) === n;
-};
--- 231-power-of-two/231-power-of-two.rb
+++
@@ -1,6 +0,0 @@
-# @param {Integer} n
-# @return {Boolean}
-def is_power_of_two(n)
-    return false if n <= 0
-    return n & (-n) == n
-end
