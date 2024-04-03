f00ef5ea467377c24a9ab07aa600dc2b5fad212f
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-04-03 00:04:35 -0700

    Time: 45 ms (99.33%), Space: 51.6 MB (45.33%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.ts
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.ts
@@ -1,9 +1,9 @@
 function trailingZeroes(n: number): number {
-  const powers: number[] = [];
-  let b = 5;
+  const powers: number[] = []
+  let b = 5
   while (b < 10000) {
-    powers.push(b);
-    b *= 5;
+    powers.push(b)
+    b *= 5
   }
-  return powers.reduce((sum, base) => sum + Math.floor(n / base), 0);
-};
+  return powers.reduce((sum, base) => sum + Math.floor(n / base), 0)
+}
