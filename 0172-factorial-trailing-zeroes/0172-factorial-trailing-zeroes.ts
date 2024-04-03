73299cea71dcc60db0d03378e979296e9279850c
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-04-03 00:03:30 -0700

    Time: 72 ms (18.00%), Space: 51.7 MB (32.00%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.ts
@@ -0,0 +1,9 @@
+function trailingZeroes(n: number): number {
+  const powers = [];
+  let b = 5;
+  while (b < 10000) {
+    powers.push(b);
+    b *= 5;
+  }
+  return powers.reduce((sum, base) => sum + Math.floor(n / base), 0);
+};
