3940155ab07fbd6e3a21951be605f5e8acc4832b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-04-09 00:40:41 -0700

    Time: 67 ms (52.00%), Space: 53.3 MB (44.00%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.ts
@@ -0,0 +1,21 @@
+function calculateMinimumHP(dungeon: number[][]): number {
+  const m = dungeon.length
+  const n = dungeon[0].length
+  const dp: number[][] = new Array(m).fill([]).map(_ => new Array(n).fill(0))
+  dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1])
+  
+  for (let i = n - 2; i >= 0; i--) {
+    dp[m - 1][i] = Math.max(1, dp[m - 1][i + 1] - dungeon[m - 1][i])
+  }
+  
+  for (let i = m - 2; i >= 0; i--) {
+    dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1])
+  }
+  
+  for (let r = m - 2; r >= 0; r--) {
+    for (let c = n - 2; c >= 0; c--) {
+      dp[r][c] = Math.max(1, Math.min(Math.max(1, dp[r][c + 1] - dungeon[r][c]), Math.max(1, dp[r + 1][c] - dungeon[r][c])))
+    }
+  }
+  return dp[0][0]
+}
