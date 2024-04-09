d94a63039cf96ed0c5e2135b7300c34d62581cf6
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-04-08 23:31:02 -0700

    Time: 68 ms (17.65%), Space: 51.5 MB (42.16%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.js
+++ 0174-dungeon-game/0174-dungeon-game.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var calculateMinimumHP = function(dungeon) {
-  const dp = new Array(dungeon.length).fill().map(_ => new Array(dungeon[0].length).fill(0));
-  dp[dungeon.length - 1][dungeon[0].length - 1] = Math.max(1, 1 - dungeon.at(-1).at(-1));
+  const [m, n] = [dungeon.length, dungeon[0].length];
+  const dp = new Array(m).fill().map(_ => new Array(n));
+  dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
   
-  for (let i = dungeon[0].length - 2; i >= 0; i--) {
-    dp.at(-1)[i] = Math.max(1, dp.at(-1)[i + 1] - dungeon.at(-1)[i]);
+  for (let i = n - 2; i >= 0; i--) {
+    dp[m - 1][i] = Math.max(1, dp[m - 1][i + 1] - dungeon[m - 1][i]);
   }
   
-  for (let i = dungeon.length - 2; i >= 0; i--) {
-    dp[i][dungeon[0].length - 1] = Math.max(1, dp[i + 1].at(-1) - dungeon[i].at(-1));
+  for (let i = m - 2; i >= 0; i--) {
+    dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
   }
   
-  for (let r = dp.length - 2; r >= 0; r--) {
-    for (let c = dp[0].length - 2; c >= 0; c--) {
+  for (let r = m - 2; r >= 0; r--) {
+    for (let c = n - 2; c >= 0; c--) {
       dp[r][c] = Math.max(1, Math.min(Math.max(1, dp[r][c + 1] - dungeon[r][c]), Math.max(1, dp[r + 1][c] - dungeon[r][c])));
     }
   }
