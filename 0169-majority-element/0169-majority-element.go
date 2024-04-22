13eb77e04b8f26503e694cb01dfc75c71e7eceef
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-04-21 23:58:34 -0700

    Time: 21 ms (38.52%), Space: 7 MB (12.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,11 +1,16 @@
 func majorityElement(nums []int) int {
-  ans := 0
-  tally := make(map[int]int)
+  ans := 1000000001
+  count := 0
   for _, num := range nums {
-    tally[num]++
-    if 2*tally[num] >= len(nums) {
+    if count == 0 {
       ans = num
-      break
+      count++
+    } else {
+      if ans == num {
+        count++
+      } else {
+        count--
+      }
     }
   }
   return ans
