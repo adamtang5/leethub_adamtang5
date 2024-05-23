f64abf845fe17229485f66dfa97721629184ca5b
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-05-23 10:35:32 -0700

    Time: 12 ms (37.28%), Space: 6.1 MB (23.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -5,8 +5,7 @@ func lengthOfLongestSubstring(s string) int {
   sub, max := "", 0
   for i := 0; i < len(s); i++ {
     if strings.Contains(sub, string(s[i])) {
-      idx := strings.Index(sub, string(s[i]))
-      sub = sub[idx+1:] + string(s[i])
+      sub = sub[strings.Index(sub, string(s[i]))+1:] + string(s[i])
     } else {
       sub += string(s[i])
     }
