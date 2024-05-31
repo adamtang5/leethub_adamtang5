b82cab4d4b821257ea7d63450e4a12b7f058ebfe
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-05-31 14:11:07 -0700

    Time: 13 ms (51.97%), Space: 5.7 MB (26.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -2,30 +2,24 @@ func longestPalindrome(s string) string {
   if len(s) < 2 {
     return s
   }
-  maxLen, maxSub, l, r := 1, s[:1], 0, 0
-  
-  for i := 0; i < len(s); i++ {
-    // odd
-    l, r = i, i
-    for l >= 0 && r < len(s) && s[l:l+1] == s[r:r+1] {
-      if r-l+1 > maxLen {
-        maxLen = r-l+1
-        maxSub = s[l:r+1]
-      }
-      l--
-      r++
+  s = "$" + s + "@"
+  s = strings.Join(strings.Split(s, ""), "#")
+  var pExt = make([]int, len(s))
+  ctr, r, mir := 0, 0, 0
+  for i := 0; i < len(s)-1; i++ {
+    mir = 2*ctr-i
+    if i < r {
+      pExt[i] = min(r-i, pExt[mir])
     }
-    
-    //even
-    l, r = i, i+1
-    for l >= 0 && r < len(s) && s[l:l+1] == s[r:r+1] {
-      if r-l+1 > maxLen {
-        maxLen = r-l+1
-        maxSub = s[l:r+1]
-      }
-      l--
-      r++
+    for i-pExt[i]-1 >= 0 && i+pExt[i]+1 < len(s) && s[i-pExt[i]-1] == s[i+pExt[i]+1] {
+      pExt[i]++
+    }
+    if pExt[i]+1 > r {
+      ctr = i
+      r = pExt[i]+1
     }
   }
-  return maxSub
+  maxExt := slices.Max(pExt)
+  ctr = slices.Index(pExt, maxExt)
+  return strings.ReplaceAll(s[ctr-maxExt:ctr+maxExt+1], "#", "")
 }
