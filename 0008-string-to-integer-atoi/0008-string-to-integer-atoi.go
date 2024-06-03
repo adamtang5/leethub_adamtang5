0260fa1551fecef8818dc94362240bd538a0b730
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-06-03 00:40:18 -0700

    Time: 4 ms (22.16%), Space: 4.4 MB (5.75%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.go
@@ -0,0 +1,41 @@
+import (
+  "fmt"
+  "strconv"
+  "strings"
+  "regexp"
+)
+
+func myAtoi(s string) int {
+  s = strings.TrimSpace(s)
+  sign := 1
+  if len(s) < 1 {
+    return 0
+  }
+  if s[0:1] == "-" {
+    sign = -1
+  }
+  if strings.Contains("+-", s[0:1]) {
+    s = s[1:]
+  }
+  
+  r, _ := regexp.Compile("^\\d+")
+  fmt.Println(r.FindString(s))
+  if !r.MatchString(s) {
+    return 0
+  }
+  ans, err := strconv.Atoi(r.FindString(s))
+  if err != nil {
+    if sign > 0 {
+      return 2147483647
+    } else {
+      return -2147483648
+    }
+  } else {
+    ans *= sign
+    if ans < 0 {
+      return max(ans, -2147483648)
+    } else {
+      return min(ans, 2147483647)
+    }
+  }
+}
