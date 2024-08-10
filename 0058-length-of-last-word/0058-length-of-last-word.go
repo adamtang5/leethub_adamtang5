c62b81b855eebb782732d6d098f210a46ed3f317
Adam Tang <95706331+adamtang5@users.noreply.github.com>
2024-08-10 08:04:41 -0700

    Time: 1 ms (76.29%), Space: 2.3 MB (78.99%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -1,4 +1,10 @@
 func lengthOfLastWord(s string) int {
-  sSlice := strings.Split(strings.Trim(s, " "), " ")
-  return len(sSlice[len(sSlice)-1])
+  i, ans := len(s)-1, 0
+  for s[i] == []byte(" ")[0] {
+    i--
+  }
+  for i >= 0 && s[i] != []byte(" ")[0] {
+    ans, i = ans+1, i-1
+  }
+  return ans
 }
