27514ae8a0363cfaffc488ebec6402bb0f5290c7
Adam Tang <adamtang@gmail.com>
2025-01-15 22:37:03 -0800

    Time: 73 ms (18.18%), Space: 71.3 MB (10.57%) - LeetHub

--- 0077-combinations/0077-combinations.go
+++ 0077-combinations/0077-combinations.go
@@ -9,20 +9,19 @@ func combine(n int, k int) [][]int {
     return ans
   }
 
+  ans := [][]int{}
   if k == 1 {
-    ans := make([][]int, n)
-    for i := range ans {
-      ans[i] = []int{i + 1}
+    for i:=1;i<=n;i++ {
+      ans = append(ans, []int{i})
     }
-    return ans
   } else if k == n {
-    ans := make([][]int, 1)
-    for i:=0;i<n;i++ {
-      ans[0] = append(ans[0], i + 1)
+    el := []int{}
+    for i:=1;i<=n;i++ {
+      el = append(el, i)
     }
-    return ans
+    ans = append(ans, el)
   } else if k <= n - k {
-    ans, reduced := [][]int{}, combine(n, k - 1)
+    reduced := combine(n, k - 1)
     for _, combo := range reduced {
       for s:=combo[len(combo)-1]+1;s<=n;s++ {
         newCombo := make([]int, len(combo)+1)
@@ -31,13 +30,11 @@ func combine(n int, k int) [][]int {
         ans = append(ans, newCombo)
       }
     }
-    return ans
   } else {
-    ans, reduced := [][]int{}, combine(n, n - k)
+    reduced := combine(n, n - k)
     for _, combo := range reduced {
       ans = append(ans, invert(combo, n))
     }
-    return ans
   }
-  return [][]int{}
+  return ans
 }
