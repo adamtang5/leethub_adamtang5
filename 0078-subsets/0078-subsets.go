df4358cd63a59a5dfd2d686e89458450f669b3ef
Adam Tang <adamtang@gmail.com>
2025-01-16 20:56:08 -0800

    Time: 0 ms (100%), Space: 4.5 MB (8.17%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,13 +1,22 @@
 func subsets(nums []int) [][]int {
-  ans := [][]int{}
-  for i := 0; i < 1 << len(nums); i++ {
-    sub := []int{}
-    for j := 0; j < len(nums); j++ {
-      if i & (1 << j) > 0 {
-        sub = append(sub, nums[j])
-      }
-    }
-    ans = append(ans, sub)
+  ans := &[][]int{}
+  dfs(0, nums, ans, &[]int{})
+  return *ans
+}
+
+func dfs(i int, nums []int, ans *[][]int, subset *[]int)  {
+  if i >= len(nums) {
+    cp1 := make([]int, len(*subset))
+    copy(cp1, *subset)
+    *ans = append(*ans, cp1)
+    return
   }
-  return ans
+
+  *subset = append(*subset, nums[i])
+  dfs(i + 1, nums, ans, subset)
+
+  cp2 := make([]int, len(*subset) - 1)
+  copy(cp2, *subset)
+  *subset = cp2
+  dfs(i + 1, nums, ans, subset)
 }
