33992cd83ab7f2ed5979981986617700578def00
Adam Tang <adamtang@gmail.com>
2025-02-23 00:07:30 -0800

    Time: 0 ms (100%), Space: 40.6 MB (26.28%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,8 +1,8 @@
 class Solution {
-  public void helper(int seg, HashMap<Integer, Integer> dp) {
+  public void helper(int seg, Map<Integer, Integer> dp) {
     int total = 0;
     for (int n = 1; n <= seg; n++) {
-      total += (int) dp.get(n - 1) * (int) dp.get(seg - n);
+      total += dp.get(n - 1) * dp.get(seg - n);
     }
     dp.put(seg, total);
   }
@@ -10,7 +10,7 @@ class Solution {
   public int numTrees(int n) {
     Map dp = new HashMap<Integer, Integer>();
     dp.put(0, 1);
-    for (int i = 1; i <= n; i++) helper(i, (HashMap<Integer, Integer>) dp);
+    for (int i = 1; i <= n; i++) helper(i, dp);
     return (int) dp.get(n);
   }
 }
