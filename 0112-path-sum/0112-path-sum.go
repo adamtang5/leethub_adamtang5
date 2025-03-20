2a5d68c32ea50ee2ba46eaefdbc548a43079fc2c
Adam Tang <adamtang@gmail.com>
2025-03-19 23:02:05 -0700

    Time: 0 ms (100%), Space: 6.7 MB (82.72%) - LeetHub

--- 0112-path-sum/0112-path-sum.go
+++ 0112-path-sum/0112-path-sum.go
@@ -6,38 +6,16 @@
  *   Right *TreeNode
  * }
  */
-type AuxNode struct {
-  Sum int
-  Node *TreeNode
+func hasPathSum(root *TreeNode, targetSum int) bool {
+  return dfs(root, 0, targetSum)
 }
 
-func hasPathSum(root *TreeNode, targetSum int) bool {
-  queue := []*AuxNode{}
-  if root != nil {
-    queue = append(queue, &AuxNode{
-      Sum: (*root).Val,
-      Node: root,
-    })
+func dfs(node *TreeNode, runningSum int, targetSum int) bool {
+  if node == nil {
+    return false
   }
-  for len(queue) > 0 {
-    curr := queue[0]
-    queue = queue[1:]
-    sum, node := (*curr).Sum, (*curr).Node
-    if (*node).Left == nil && (*node).Right == nil && sum == targetSum {
-      return true
-    }
-    if (*node).Left != nil {
-      queue = append(queue, &AuxNode{
-        Sum: sum + (*(*node).Left).Val,
-        Node: (*node).Left,
-      })
-    }
-    if (*node).Right != nil {
-      queue = append(queue, &AuxNode{
-        Sum: sum + (*(*node).Right).Val,
-        Node: (*node).Right,
-      })
-    }
+  if (*node).Left == nil && (*node).Right == nil {
+    return runningSum + (*node).Val == targetSum
   }
-  return false
+  return (*node).Left != nil && dfs((*node).Left, runningSum + (*node).Val, targetSum) || (*node).Right != nil && dfs((*node).Right, runningSum + (*node).Val, targetSum)
 }
