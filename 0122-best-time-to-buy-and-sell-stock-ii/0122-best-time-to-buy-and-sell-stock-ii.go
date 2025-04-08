9882967ac896be241e53ce5a5c4b2de6a51d34df
Adam Tang <adamtang@gmail.com>
2025-04-07 22:46:07 -0700

    Time: 0 ms (100%), Space: 5.1 MB (81.63%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,14 +1,9 @@
 func maxProfit(prices []int) int {
-  ans, l, r := 0, 0, 1
-  for r < len(prices) {
-    if prices[r] >= prices[l] {
-      for r < len(prices) - 1 && prices[r + 1] >= prices[r] {
-        r++
-      }
-      ans += prices[r] - prices[l]
+  ans := 0
+  for i := 1; i < len(prices); i++ {
+    if prices[i] > prices[i - 1] {
+      ans += prices[i] - prices[i - 1]
     }
-    l = r
-    r++
   }
   return ans
 }
