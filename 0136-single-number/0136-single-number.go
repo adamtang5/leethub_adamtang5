3fbd949c535d5eda53aea89f19512886ffab2e9c
Adam Tang <adamtang@gmail.com>
2025-04-24 14:37:49 -0700

    Time: 0 ms (100%), Space: 7.9 MB (69.4%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,15 +1,7 @@
 func singleNumber(nums []int) int {
-  tally, ans := make(map[int]bool), 0
-  for _, num := range nums {
-    if _, ok := tally[num]; ok {
-      delete(tally, num)
-    } else {
-      tally[num] = true
-    }
-  }
-  for num := range tally {
-    ans = num
-    break
+  ans := nums[0]
+  for i := 1; i < len(nums); i++ {
+    ans ^= nums[i]
   }
   return ans
 }
